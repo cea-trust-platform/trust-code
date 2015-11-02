32d4624c26f81de4e9dcfd7eb2c87b6124c26df2
fauchet <gauthier.fauchet@cea.fr>
2015-11-02 20:20:34 +0100

    tools non compiles si make debug

--- Outils/ICoCo/Test_ICoCo.sh
+++ Outils/ICoCo/Test_ICoCo.sh
@@ -2,7 +2,9 @@ cd ICoCo_src
 [ ! -f ./configure ] && baltik_build_configure
 ./configure
 echo "Compiling ICoCo test"
-make module_optim module_debug  
+debug=""
+[ -f $exec_debug ]  && debug="module_debug"
+make module_optim $debug
 status=$?
 [ $status -ne 0 ]  && echo Error
 exit $status
