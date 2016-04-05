e4db38c12d317490f73e940c06e6457ae027d001
triou <triou@cea.fr>
2016-04-05 17:25:02 +0200

    reprise_auto script adapted with keyword translated into english

--- bin/admin/test_reprise_auto.sh
+++ bin/admin/test_reprise_auto.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-source $TRUST_ROOT/env_TRUST.sh
-trust -copy Obstacle
-cd Obstacle
-trust -clean
 log=test_reprise_auto.log
+source $TRUST_ROOT/env_TRUST.sh 1>$log 2>&1
+trust -copy Obstacle 1>$log 2>&1
+cd Obstacle
+trust -clean 1>$log 2>&1
 make_PAR.data Obstacle 1>$log 2>&1
 echo $ECHO_OPTS "1,$ s?tmax 5.0?tmax 100.0?g\nw" | ed PAR_Obstacle.data 1>>$log 2>&1
 trust -create_sub_file PAR_Obstacle 2 1>>$log 2>&1
@@ -15,6 +15,7 @@ then
    echo "Script reprise_auto KO"
    echo "final time reached no detecte in PAR_Obstacle.err:" >> $log
    cat PAR_Obstacle.err >> $log
+   cat $log | mail_ -s\"[mise_a_jour_TRUST_arch] KO reprise_auto script on $HOST_BUILD\" $TRUST_MAIL
 else
    echo "Script reprise_auto OK"
 fi
