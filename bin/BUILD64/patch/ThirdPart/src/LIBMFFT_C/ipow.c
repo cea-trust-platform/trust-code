27c81c92b8e1463c0677762e656d96d4cea6c843
fauchet <gauthier.fauchet@cea.fr>
2016-04-05 18:31:59 +0200

    Ajout des scripts pour compiler en INT64

---
+++ bin/BUILD64/patch/ThirdPart/src/LIBMFFT_C/ipow.c
@@ -0,0 +1,31 @@
+#define INTEGER long
+#define NBITS_INTEGER 32
+#define REAL8 double
+unsigned INTEGER ipow(
+unsigned INTEGER mult,
+unsigned INTEGER N
+);
+/* Returns mult raised to the Nth power
+ * Uses a cyclic reduction method
+ */
+unsigned INTEGER ipow(unsigned INTEGER mult, unsigned INTEGER N)
+{
+  unsigned INTEGER n = N, power = mult;
+  unsigned INTEGER multv = 1;
+  unsigned INTEGER j;
+
+  for(j=0; j<NBITS_INTEGER; j++) {
+
+    if (n == 0) break;
+
+    if ( (1&n) != 0) {
+      multv *= power;
+    }
+
+    power *= power;
+    n >>= 1;
+
+  }
+
+  return multv;
+}
