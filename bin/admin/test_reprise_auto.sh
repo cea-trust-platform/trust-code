ab423215ca5d3664382619a2fc76869e2acad627
triou <triou@cea.fr>
2017-09-15 15:22:03 +0200

    Second update in TRUST ATELIER to launch mise_a_jour_TRUST_arch with SLURM on CCRT clusters

--- bin/admin/test_reprise_auto.sh
+++ bin/admin/test_reprise_auto.sh
@@ -10,7 +10,7 @@ echo $ECHO_OPTS "1,$ s?tmax 5.0?tmax 100.0?g\nw" | ed PAR_Obstacle.data 1>>$log 
 USE_MPIRUN=1 trust -create_sub_file PAR_Obstacle 2 1>>$log 2>&1
 echo "Launch step..."
 reprise_auto -test 1>>$log 2>&1
-echo "Wainting step..."
+echo "Waiting step..."
 sleep 1800 # Attente de 30mn que le calcul/reprise se finisse
 echo "Verification step..."
 cd `pwd`/test_reprise_auto 1>>$log 2>&1
