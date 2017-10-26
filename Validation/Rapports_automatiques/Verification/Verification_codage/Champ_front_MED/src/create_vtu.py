c78b4f693cb02c41ca1c5b1dbb0f3e90376e0ea3
triou <triou@cea.fr>
2017-10-26 13:38:08 +0200

    Revert "Merge remote-tracking branch 'shared/abn/chp_f_med' into Livraison"
    This reverts commit a42aba5bd691953b27fddcdaebe41f62e20f38d3, reversing
    changes made to 4743643cbaffe19338f0177134a4e74225e77d58.
    
    Conflicts:
    	RELEASE_NOTES

--- Validation/Rapports_automatiques/Verification/Verification_codage/Champ_front_MED/src/create_vtu.py
+++
@@ -1,7 +0,0 @@
-import MEDLoader as mc
-
-file_name = "etat_interm_3D_cas_test.med"
-
-f = mc.ReadFieldCell(file_name, "maillage_2D", 0, "VITESSE_ELEM_dom_0_3D", 0, -1)
-f.writeVTK("etat_interm.vtu")
-
