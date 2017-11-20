fd7c3049aab1f0d48e818ff68f031d8e973459ac
triou <triou@cea.fr>
2017-11-20 17:36:19 +0100

    First automatic commit

--- src/MAIN/MAIN.cpp
+++ src/MAIN/MAIN.cpp
@@ -337,7 +337,7 @@ int main_TRUST(int argc, char** argv,mon_main*& main_process,int force_mpi)
         Cerr<<"Localisation etude: " << ::pwd() << finl;
         Cerr<<"Nom du cas " << data_file << finl;
         Cerr<<" code : "<< argv[0] << finl;
-        Cerr<<" version : 1.7.6 " << finl;
+        Cerr<<" version : 1.7.7_beta " << finl;
       }
 
     main_process->dowork(data_file);
