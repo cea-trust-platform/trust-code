bd627ff2bb04407e8af602869db094223670b958
triou <triou@cea.fr>
2018-03-28 15:35:16 +0200

    Cerr message adjustment at the request of a user

--- src/Kernel/Champs_dis/Champ_Q4_implementation.h
+++ src/Kernel/Champs_dis/Champ_Q4_implementation.h
@@ -1,5 +1,5 @@
 /****************************************************************************
-* Copyright (c) 2015 - 2016, CEA
+* Copyright (c) 2017, CEA
 * All rights reserved.
 *
 * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
@@ -236,7 +236,7 @@ inline double coord_barycentrique(const IntTab& polys,
       return coord_bary;
     }
 
-  Cerr<<"The number of node by element " <<nb_som_elem<<" does not correspond has a treated situation"<<finl;
+  Cerr<<"The number of nodes by element " <<nb_som_elem<<" does not correspond to a treated situation."<<finl;
   Process::exit();
   return 0.;
 }
@@ -491,7 +491,7 @@ inline double coord_barycentrique(const IntTab& polys,
       return coord_bary;
     }
 
-  Cerr<<"The number of node by element " <<nb_som_elem<<" does not correspond has a treated situation"<<finl;
+  Cerr<<"The number of nodes by element " <<nb_som_elem<<" does not correspond to a treated situation."<<finl;
   Process::exit();
   return 0.;
 }
