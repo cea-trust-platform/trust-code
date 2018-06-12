ffd5bd214e5eafe4d4cf36bba7f60fa550ea080f
triou <triou@cea.fr>
2018-06-12 16:17:17 +0200

    Fix for XData comments

--- src/Kernel/MEDimpl/EcrMEDfile.cpp
+++ src/Kernel/MEDimpl/EcrMEDfile.cpp
@@ -1,5 +1,5 @@
 /****************************************************************************
-* Copyright (c) 2017, CEA
+* Copyright (c) 2018, CEA
 * All rights reserved.
 *
 * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
@@ -23,7 +23,7 @@
 #include <EcrMEDfile.h>
 
 Implemente_instanciable_sans_constructeur(EcrMEDfile,"Ecrire_MEDfile",EcrMED);
-// XD writemedfile interprete ecrire_medfile -1 Obsolete keyword to write a mesh with MED file API
+// XD writemedfile writemed ecrire_medfile -1 Obsolete keyword to write a mesh with MED file API
 
 /**
  * Set flag use_medcoupling
--- src/Kernel/MEDimpl/LireMEDfile.cpp
+++ src/Kernel/MEDimpl/LireMEDfile.cpp
@@ -1,5 +1,5 @@
 /****************************************************************************
-* Copyright (c) 2017, CEA
+* Copyright (c) 2018, CEA
 * All rights reserved.
 *
 * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
@@ -23,7 +23,7 @@
 #include <LireMEDfile.h>
 
 Implemente_instanciable_sans_constructeur(LireMEDfile,"Lire_MEDfile",LireMED);
-// XD Read_MEDfile interprete lire_medfile -1 Obsolete keyword to read a mesh with MED file API
+// XD Read_MEDfile Read_MED lire_medfile -1 Obsolete keyword to read a mesh with MED file API
 
 LireMEDfile::LireMEDfile()
 {
