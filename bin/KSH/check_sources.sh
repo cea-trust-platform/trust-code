22b9b3f9ae104320a1dc7b2fd2efb8cc7fe9405b
Pierre LEDAC <pierre.ledac@cea.fr>
2019-02-14 12:27:21 +0100

    Better indent

--- bin/KSH/check_sources.sh
+++ bin/KSH/check_sources.sh
@@ -70,7 +70,8 @@ check_src_in_gc()
 check_recent_src()
 {
     file=$1
-	indent_file.sh $file
+    indent_file.sh $file
+    
     # pas daccumulation de <<  sinon cela met des heures avec gcc 6.6.1 #
     #   gros_pipe=`awk -F\<\< '{if (NF>19) {print NF $0}}' $file`
     #gros_pipe=`awk -F\<\< '{if (NF>1) { te=$0;a=gsub(";","ok",te); if (((NF+old)>19)&&(a!=0))  {print ("line:",FNR, "number",NF+old, $0)}  ; if ((NF>1)&&(a==0)) { old=NF+old } else { old =0 } }}' $file`
@@ -95,7 +96,7 @@ check_recent_src()
     # Interdiction du francais dans les messages d'erreur du Kernel
     ###############################################################
     if [ "`echo $PWD | grep /TRUST/src/Kernel/`" != "" ]
-	then
+    then
 	$TRUST_ROOT/bin/KSH/forbid_french.ct $file
 	erreur $?
     fi
@@ -105,7 +106,7 @@ check_recent_src()
     #################################################
     ok=`grep include $file | grep "#" | grep "\"*\"" 2>/dev/null`
     if [ "$ok" != "" ]
-	then
+    then
 	echo "**************************************************************"
 	echo "An include file is written under the form: #include \"toto.h\""
 	echo "It should be written like: #include <toto.h>"
@@ -116,7 +117,7 @@ check_recent_src()
     ####################################################################
     # Check source header: (Baltik rep="" so not checked yet for Baltik)
     ####################################################################
-    [ "$rep" != "" ] && check_en_tete $rep/$file         
+    [ "$rep" != "" ] && check_en_tete $rep/$file     
 }
 
 check_update_P()
@@ -251,14 +252,12 @@ check_src()
 check_all()
 {
   # Used by Baltik projects
-  if [ 1 -eq 1 ]
-  then
-    for file in `ls *.cpp *.h *.c 2>/dev/null` 
-    do
-       # ls 1>/dev/null
-       check_recent_src $file
-    done
-  fi
+  for file in `ls *.cpp *.h *.c 2>/dev/null` 
+  do
+     # ls 1>/dev/null
+     check_recent_src $file
+  done
+  
   check_src_in_gc
     
   check_update_P -check `\ls *.P 2>/dev/null`
@@ -296,8 +295,8 @@ then
 	 newer_files=`find * -type f  \( -name make.include -o -name '*'.cpp -o -name '*'.h -o -name '*'.c \) -newer ${reffile} | grep -v / | grep -v "\.o$"`
 	 new_newer_files=`find . -maxdepth 1 -type f -newer ${reffile} | grep -v CMakeLists.txt` 
       fi
-	[ "$new_newer_files" = "" ] && exit 0
-     mk_Instancie
+      [ "$new_newer_files" = "" ] && exit 0
+      mk_Instancie
       #############
       # Git check #
       #############
@@ -353,6 +352,7 @@ then
       # Fin des verifications
       #######################
       cd $org
+
       if [ $err = 1 ]
       then
 	rm -f ${reffile}
