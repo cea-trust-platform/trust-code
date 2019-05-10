b3c164d7550caf6c58a0f0da0a8fa8a46e78ed98
Nabil DJATI <nabil.djati@cea.fr>
2019-05-10 15:19:46 +0200

    trust uses conda python

--- env_src/test.py
+++ env_src/test.py
@@ -6,4 +6,12 @@ print(hashlib.__file__)
 print(hashlib.md5)
 import md5
 print(md5.__file__) 
+import matplotlib
+print(matplotlib.__file__)
+import numpy
+print(numpy.__file__)
+import jsonschema
+print(jsonschema.__file__)
+import PyQt4
+PyQt4.__file__
 print "Python is OK"
