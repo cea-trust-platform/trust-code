63b45ec2f4d4c0dea6ba6a028e5f886985eb417d
Nabil DJATI <nabil.djati@cea.fr>
2019-05-21 16:03:28 +0200

    fix of the PATH in gmsh.sh

--- Outils/Gmsh/gmsh.sh
+++ Outils/Gmsh/gmsh.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-env LD_LIBRARY_PATH=/volatile/ndjati/Workdir_TRUST/TRUST179_miniconda/exec/python/lib:$LD_LIBRARY_PATH gmsh_exec $@ 
+# This script has to be removed and the gmsh_exec renamed into gmsh in $TRUST_ROOT/Outils/Gmsh/install_gmsh once MED will be compiled with cmake+hdf5 from conda
+
+direc=$(dirname $0)
+
+LD_LIBRARY_PATH=$TRUST_ROOT/exec/python/lib:$LD_LIBRARY_PATH 
+${direc}/gmsh_exec $@ 
+
