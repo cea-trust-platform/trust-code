5926e829603c71be818c17848a17e36562f0f4a5
triou <triou@cea.fr>
2019-07-11 13:50:11 +0200

    Fix for clang compilers

--- src/Kernel/Framework/Convection_tools.cpp
+++ src/Kernel/Framework/Convection_tools.cpp
@@ -14,26 +14,75 @@
 *****************************************************************************/
 //////////////////////////////////////////////////////////////////////////////
 //
-// File:        Convection_tools.h
+// File:        Convection_tools.cpp
 // Directory:   $TRUST_ROOT/src/Kernel/Framework
 // Version:     1
 //
 //////////////////////////////////////////////////////////////////////////////
 #include <ArrOfInt.h>
 #include <Double.h>
+#include <Convection_tools.h>
 
 //////////////////////////////////////////////////////////////
 //   Fonctions limiteurs de MUSCL
 ////////////////////////////////////////////////////////////////
 
 
-double minmod(double grad1, double grad2);
+double minmod(double grad1, double grad2)
+{
+  double gradlim=0.;
+  if(grad1*grad2>0.) (dabs(grad1)<dabs(grad2)) ? gradlim=grad1 : gradlim=grad2 ;
+  return gradlim;
+}
 
-double vanleer(double grad1, double grad2);
+double vanleer(double grad1, double grad2)
+{
+  double gradlim=0.;
+  if(grad1*grad2>0.) gradlim=2.*grad1*grad2/(grad1+grad2) ;
+  return gradlim;
+}
 
-double vanalbada(double grad1, double grad2);
+double vanalbada(double grad1, double grad2)
+{
+  double gradlim=0.;
+  if(grad1*grad2>0.) gradlim=grad1*grad2*(grad1+grad2)/(grad1*grad1+grad2*grad2) ;
+  return gradlim;
+}
 
-double chakravarthy(double grad1, double grad2);
 
-double superbee(double grad1, double grad2);
+double chakravarthy(double grad1, double grad2)
+{
+  /*
+    Cerr << " limiteur chakavarthy non preconise (non symetrique) " << finl;
+    exit();
+    return 0;
+  */
+  double gradlim=0.;
+  if ((grad1*grad2)>0)
+    {
+      gradlim=dmin(grad1/grad2,1.8); // 1<<beta<<2
+      gradlim=dmax(gradlim,0.);
+      gradlim*=grad2;
+    }
+  return gradlim;
+}
 
+double superbee(double grad1, double grad2)
+{
+  /*
+    Cerr << " limiteur superbee non preconise (source d'instabilites) " << finl;
+    exit();
+    return 0;
+  */
+  double gradlim=0.;
+  if ((grad1*grad2)>0)
+    {
+      double gradlim1,gradlim2;
+      gradlim1=dmin(2*(grad1/grad2),1);
+      gradlim2=dmin(grad1/grad2,2);
+      gradlim=dmax(gradlim1,gradlim2);
+      gradlim=dmax(gradlim,0.);
+      gradlim*=grad2;
+    }
+  return gradlim;
+}
