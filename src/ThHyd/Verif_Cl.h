7221d2b23500664ac883496f68edf5251aee6cea
abn <adrien.bruneton@cea.fr>
2019-10-24 17:15:27 +0200

    Moving other turbulence pieces to TrioCFD:
    + inactive tests
    + PRM using turbulence (to be revised later some of them should really stay in TRUST in a laminar version)
    + BC Entree_fluide_K_Eps_impose

--- src/ThHyd/Verif_Cl.h
+++ src/ThHyd/Verif_Cl.h
@@ -1,5 +1,5 @@
 /****************************************************************************
-* Copyright (c) 2015 - 2016, CEA
+* Copyright (c) 2019, CEA
 * All rights reserved.
 *
 * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
@@ -27,14 +27,13 @@
 //
 // .DESCRIPTION
 //    Interface du module ThHyd.
-//    Contient 3 fonctions:
+//    Contient 2 fonctions:
 //      int tester_compatibilite_hydr_thermique(const Zone_Cl_dis& , const Zone_Cl_dis& )
 //      int tester_compatibilite_hydr_concentration(const Zone_Cl_dis& , const Zone_Cl_dis& )
-//      int tester_compatibilite_hydr_turb(const Zone_Cl_dis& , const Zone_Cl_dis& )
 //    qui servent a tester la coherence des conditions aux limites
-//    et les 3 fonctions
-//      int message_erreur_[therm|conc|turb](const Cond_lim& , const Cond_lim& , int& )
-//    qui affiche un message d'erreur pour la compatibilite hyd/[therm|conc|turb]
+//    et les 2 fonctions
+//      int message_erreur_[therm|conc](const Cond_lim& , const Cond_lim& , int& )
+//    qui affiche un message d'erreur pour la compatibilite hyd/[therm|conc]
 // .SECTION voir aussi
 //    Fonction de librairie hors classe
 //////////////////////////////////////////////////////////////////////////////
@@ -50,7 +49,3 @@ int message_erreur_therm(const Cond_lim& , const Cond_lim& , int& );
 int tester_compatibilite_hydr_concentration(const Zone_Cl_dis& , const Zone_Cl_dis& )  ;
 
 int message_erreur_conc(const Cond_lim& , const Cond_lim& , int& );
-
-int tester_compatibilite_hydr_turb(const Zone_Cl_dis& , const Zone_Cl_dis& );
-
-int message_erreur_turb(const Cond_lim& , const Cond_lim& , int& );
--- tests/Reference/PETSC_Quasi_Comp_Coupl_keps_VEF_impl_GMRES/Canal_plan_couple_fluide_x100.geom
+++
@@ -1,1 +0,0 @@
-../Quasi_Comp_Coupl_keps_VEF_impl/Canal_plan_couple_fluide_x100.geom
