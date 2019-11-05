0901188826cd5875072e3d3f3ad18d36c6f873b8
triou <triou@cea.fr>
2019-11-05 09:52:16 +0100

    Automatic updates after merge

--- src/VDF/Traitement_particulier_NS_Profils_VDF.cpp
+++ src/VDF/Traitement_particulier_NS_Profils_VDF.cpp
@@ -1,5 +1,5 @@
 /****************************************************************************
-* Copyright (c) 2015 - 2016, CEA
+* Copyright (c) 2019, CEA
 * All rights reserved.
 *
 * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
@@ -15,7 +15,7 @@
 //////////////////////////////////////////////////////////////////////////////
 //
 // File:        Traitement_particulier_NS_Profils_VDF.cpp
-// Directory:   $TRUST_ROOT/src/VDF/Turbulence
+// Directory:   $TRUST_ROOT/src/VDF
 // Version:     /main/21
 //
 //////////////////////////////////////////////////////////////////////////////
