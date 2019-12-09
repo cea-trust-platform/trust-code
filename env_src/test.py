a0873f6850541779499c59850aa4c71e263fae55
Nabil DJATI <nabil.djati@cea.fr>
2019-12-09 16:11:23 +0100

    Installing conda packages from local channel and test.py update

--- env_src/test.py
+++ env_src/test.py
@@ -4,14 +4,10 @@ print((sys.version))
 import hashlib
 print((hashlib.__file__))
 print((hashlib.md5))
-import md5
-print((md5.__file__))
 import matplotlib
 print((matplotlib.__file__))
 import numpy
 print((numpy.__file__))
-import jsonschema
-print((jsonschema.__file__))
-import PyQt4
-PyQt4.__file__
+import PyQt5
+print((PyQt5.__file__))
 print("Python is OK")
