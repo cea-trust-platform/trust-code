d669a495ed83fa92c22400408be724c48fe44a49
Pierre LEDAC <pierre.ledac@cea.fr>
2020-01-09 11:55:04 +0100

    Fix USE_MPIRUN

--- bin/admin/test_reprise_auto.sh
+++ bin/admin/test_reprise_auto.sh
@@ -2,12 +2,12 @@
 log=test_reprise_auto.log
 echo "Preparation step..."
 source $TRUST_ROOT/env_TRUST.sh 1>$log 2>&1
-USE_MPIRUN=1 trust -copy Obstacle 1>>$log 2>&1
+trust -copy Obstacle 1>>$log 2>&1
 cd `pwd`/Obstacle
-USE_MPIRUN=1 trust -clean 1>>$log 2>&1
-USE_MPIRUN=1 make_PAR.data Obstacle 1>>$log 2>&1
+trust -clean 1>>$log 2>&1
+make_PAR.data Obstacle 1>>$log 2>&1
 echo $ECHO_OPTS "1,$ s?tmax 5.0?tmax 100.0?g\nw" | ed PAR_Obstacle.data 1>>$log 2>&1
-USE_MPIRUN=1 trust -create_sub_file PAR_Obstacle 2 1>>$log 2>&1
+trust -create_sub_file PAR_Obstacle 2 1>>$log 2>&1
 echo "Launch step..."
 reprise_auto -test 1>>$log 2>&1
 echo "Waiting step..."
