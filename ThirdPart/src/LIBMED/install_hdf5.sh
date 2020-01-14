833344a80c5c098e142ffb2a2474461130671586
Nabil DJATI <nabil.djati@cea.fr>
2020-01-14 12:22:43 +0100

    use TRUST compilers when building hdf5

--- ThirdPart/src/LIBMED/install_hdf5.sh
+++ ThirdPart/src/LIBMED/install_hdf5.sh
@@ -40,7 +40,8 @@ if [ "x$TRUST_USE_EXTERNAL_HDF" = "x" ]; then
   
   echo "Configuring ..."
   # All of those options are already set with the following values by default, but just to be sure force them again ...:
-  options="-DBUILD_SHARED_LIBS=ON -DHDF5_BUILD_CPP_LIB=ON -DHDF5_BUILD_HL_LIB=ON"
+  options="-DCMAKE_C_COMPILER=$TRUST_cc_BASE -DCMAKE_CXX_COMPILER=$TRUST_CC_BASE"
+  options="$options -DBUILD_SHARED_LIBS=ON -DHDF5_BUILD_CPP_LIB=ON -DHDF5_BUILD_HL_LIB=ON"
   options="$options -DHDF5_BUILD_TOOLS=ON -DHDF5_ENABLE_USING_MEMCHECKER=ON -DHDF5_ENABLE_DIRECT_VFD=OFF"
   cmake $options -DCMAKE_INSTALL_PREFIX="$actual_install_dir" -DCMAKE_BUILD_TYPE=Release ../$src_dir || exit -1
   
