8a879b34ccd9fd19447c93eeae856830cca8aec9
triou <triou@cea.fr>
2020-01-14 16:02:37 +0100

    Automatic update

--- src/Kernel/MEDimpl/med++.h
+++ src/Kernel/MEDimpl/med++.h
@@ -1,5 +1,5 @@
 /****************************************************************************
-* Copyright (c) 2015 - 2016, CEA
+* Copyright (c) 2019, CEA
 * All rights reserved.
 *
 * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
@@ -12,6 +12,13 @@
 * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 *
 *****************************************************************************/
+//////////////////////////////////////////////////////////////////////////////
+//
+// File:        med++.h
+// Directory:   $TRUST_ROOT/src/Kernel/MEDimpl
+// Version:     1
+//
+//////////////////////////////////////////////////////////////////////////////
 
 #ifndef MEDpp_H
 #define MEDpp_H
