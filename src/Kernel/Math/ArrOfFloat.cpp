d6039f1d6c4a52acd853022431f31a8274f19be7
Pierre LEDAC <pierre.ledac@cea.fr>
2020-02-07 16:05:07 +0100

    Regression

--- src/Kernel/Math/ArrOfFloat.cpp
+++ src/Kernel/Math/ArrOfFloat.cpp
@@ -158,7 +158,6 @@ void ArrOfFloat::invalidate(int first_element, int nb_elements)
      (__extension__ ((union {
      int __l __attribute__((__mode__(__DI__)));
      float __d; }) { __l: 0x7ff7ffffffffffffULL }).__d);; */
-#pragma GCC diagnostic ignored "-Wimplicit-int-float-conversion" /* no diagnostic for this one (clang++ Intel) */
   static const float VALEUR_INVALIDE = 0x7ff7ffffffffffffULL ;
 
   const int n = first_element + nb_elements;
