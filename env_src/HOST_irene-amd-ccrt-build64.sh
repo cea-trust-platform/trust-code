85e115352e651be07004d168afdec0c65d87ced8
triou <triou@cea.fr>
2020-08-25 13:01:05 +0200

    update irene's HOST file

--- env_src/HOST_irene-amd-ccrt-build64.sh
+++ env_src/HOST_irene-amd-ccrt-build64.sh
@@ -107,6 +107,6 @@ define_soumission_batch()
    mpirun="ccc_mprun $binding -n \$BRIDGE_MSUB_NPROC"
    sub=CCC
    espacedir="work,scratch"
-   project="dendm2s" && [ "`id | grep gch0406`" != "" ] && project="gch0406"
+   project="dendm2s" #&& [ "`id | grep gch0406`" != "" ] && project="gch0406"
    [ "$project" = "" ] && project=`PYTHONPATH=/usr/lib64/python2.7/site-packages ccc_myproject 2>/dev/null | $TRUST_Awk '/project/ {print $4;exit}'` # Add project
 }
