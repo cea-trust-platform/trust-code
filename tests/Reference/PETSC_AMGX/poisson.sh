557fe02d33540755e69b35516b47be54ab56152f
Pierre LEDAC <pierre.ledac@cea.fr>
2020-08-27 18:13:17 +0200

    Correction pour installation Cuda sans toolkit (runtime seul) + portage sur is234639 + portage sur Fedora

--- tests/Reference/PETSC_AMGX/poisson.sh
+++ tests/Reference/PETSC_AMGX/poisson.sh
@@ -2,4 +2,5 @@
 # Teste la validate du fichier .amgx avec le cas poisson de AmgXWrapper
 echo "Usage: $0 file.amgx" 
 #[ ! -f "$1" ] && exit 1
-$TRUST_ROOT/lib/src/LIBAMGX/AmgXWrapper/example/poisson/bin/poisson -caseName amgx -mode AmgX_GPU -cfgFileName $1 -Nx 10 -Ny 10 -Nz 10
+valgrind="" && [ "$VALGRIND" != "" ] && valgrind=valgrind
+$valgrind $TRUST_ROOT/lib/src/LIBAMGX/AmgXWrapper/example/poisson/bin/poisson -caseName amgx -mode AmgX_GPU -cfgFileName $1 -Nx 10 -Ny 10 -Nz 10
