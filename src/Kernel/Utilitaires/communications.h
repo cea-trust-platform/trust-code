b3e1b0b013b6751e4a6812eccac01b14d24c5471
Anida KHIZAR <anida.khizar@cea.fr>
2020-11-20 23:32:56 +0100

    casting long* in int* using stl + deleted useless communication methods with type long long

--- src/Kernel/Utilitaires/communications.h
+++ src/Kernel/Utilitaires/communications.h
@@ -1,5 +1,5 @@
 /****************************************************************************
-* Copyright (c) 2019, CEA
+* Copyright (c) 2020, CEA
 * All rights reserved.
 *
 * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
@@ -67,7 +67,6 @@ int envoyer_broadcast(double&   t, int source);
 int envoyer_broadcast(Objet_U& t, int source);
 
 int mppartial_sum(int i);
-long long mppartial_sum(long long i);
 int mp_max(int i);
 int mp_min(int i);
 void mpsum_multiple(double& x1, double& x2);
