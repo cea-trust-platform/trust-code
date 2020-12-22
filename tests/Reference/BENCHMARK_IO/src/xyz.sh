e3c340661d5a11762fc3a46b86f6cb6f35de8642
Pierre LEDAC <pierre.ledac@cea.fr>
2020-12-22 08:05:10 +0100

    Update Benchs

--- tests/Reference/BENCHMARK_IO/src/xyz.sh
+++ tests/Reference/BENCHMARK_IO/src/xyz.sh
@@ -3,23 +3,15 @@ jdd=xyz
 source ./prepare
 # Calculs
 options="-journal=0"
-for TRUST_DISABLE_MPIIO in 1 0
+for cas in EcrFicPartageBin EcrFicPartageMPIIO
 do
-   cas=TRUST_ENABLE_MPIIO && [ "$TRUST_DISABLE_MPIIO" = 1 ] && cas=TRUST_DISABLE_MPIIO 
    cp PAR_$jdd.data $cas.data
-   sed -i "1,$ s/cas/$cas/g" $cas.data
-   echo "Running 1e6 cells with NP=$NB_ZONES CPUs TRUST_DISABLE_MPIIO=$TRUST_DISABLE_MPIIO :"
-   TRUST_DISABLE_MPIIO=$TRUST_DISABLE_MPIIO trust $cas $N $options 1>$cas.log 2>&1 
+   sed -i "1,$ s/cas/$cas/g" $cas.data || exit -1
+   echo "Writing xyz file with $cas :"
+   trust $cas $N $options 1>$cas.log 2>&1 
    grep "Secondes / sauvegarde" $cas.TU
    grep "Debit " $cas.TU
 done
-meld TRUST_DISABLE_MPIIO.TU TRUST_ENABLE_MPIIO.TU
-exit
-cmd="sdiff TRUST_DISABLE_MPIIO.TU TRUST_ENABLE_MPIIO.TU"
-eval $cmd
-exit
-echo $cmd
-# Test en sequentiel
-TRUST_DISABLE_MPIIO=1 trust $cas $options 1>$cas.log 2>&1 && PYTHONPATH="" meld $cas.TU TRUST_DISABLE_MPIIO.TU TRUST_ENABLE_MPIIO.TU &
-ls -lart *.xyz
+meld EcrFicPartageBin.TU EcrFicPartageMPIIO.TU &
+meld EcrFicPartageBin_comm.TU EcrFicPartageMPIIO_comm.TU
 
