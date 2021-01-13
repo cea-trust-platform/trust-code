d77abe41fe8a928432afcf7a384bd242fd9b5463
triou <triou@cea.fr>
2021-01-13 09:58:59 +0100

    module openmpi/icc/2.0.4.12 not available for occigen users, switch to openmpi/intel/2.0.4

--- env_src/HOST_occigen.sh
+++ env_src/HOST_occigen.sh
@@ -56,7 +56,7 @@ cb_config_list *:1" > ROMIO_HINTS.env
    # openmpi 2.0.0 2.0.1
    #module="$intel openmpi/intel/2.0.1" # error mpif-sizeof.h
    #module="openmpi/gnu/2.0.1"
-   # int64 build built with: module="intel/19.3 openmpi/icc/2.0.4.12"
+   # int64 build built with: module="intel/19.3 openmpi/intel/2.0.4"
    # int64 build failed with intel/18.1 and intel/19.3+intelmpi/2019.3.199
    #
    echo "# Module $module detected and loaded on $HOST."
