b7557fc0bb4c1c460dd8d721e4d43eb6f427cf81
Pierre LEDAC <pierre.ledac@cea.fr>
2021-02-17 15:49:27 +0100

    [Conf] Update Jean-Zay submission file for GPU and CPU

--- env_src/HOST_jean-zay.sh
+++ env_src/HOST_jean-zay.sh
@@ -44,46 +44,25 @@ define_soumission_batch()
 {
    soumission=2
    [ "$prod" = 1 ] && soumission=1
-   [ "$gpu"  = 1 ] && soumission=1 && qos=qos_gpu-dev
-   # sinfo :
-   #PARTITION AVAIL  TIMELIMIT  NODES  STATE 
-   #cpu_p1*      up   20:00:00    1528 mix
-   #gpu_p1       up   20:00:00     261 mix
-   #visu         up    1:00:00      5   idle 
-   #archive      up   20:00:00      7    mix 
-   #prepost      up   20:00:00      4    mix 
-
-   #queue=cpu_p1 && [ "$gpu" = 1 ] && queue=gpu_p3 && [ "`sinfo | grep $queue | grep idle`" = "" ] && queue=gpu_p1
+   [ "$gpu"  = 1 ] && soumission=1
    # http://www.idris.fr/eng/jean-zay/gpu/jean-zay-gpu-exec_partition_slurm-eng.html Une seule partition gpu_p13
-   queue=cpu_p1 && [ "$gpu" = 1 ] && queue=gpu_p13 && constraint=v100-16g # pour gpu_p3 ou constraint=v100-32g (gpu_p1)
-   # sacctmgr list qos format=Name,Priority,MaxSubmit,MaxWall,MaxNodes :      
-   #   Name   Priority MaxSubmit     MaxWall MaxNodes 
-   #---------- ---------- --------- ----------- -------- 
-   #normal          0                                
-   #qos_cpu-d+         80     10000    02:00:00          
-   #qos_gpu-d+         80     10000    02:00:00          
-   #qos_cpu-gc         50     25000    20:00:00          
-   #qos_gpu-gc         50     25000    20:00:00          
-   #qos_cpu-t3         50     10000    20:00:00          
-   #qos_gpu-t3         50     10000    20:00:00          
-   #qos_cpu-ex          0     10000    20:00:00          
-   #qos_gpu-ex          0     10000    20:00:00          
-   #qos_cpu-d+         50     10000                      
-   #qos_gpu-d+         50     10000                      
-   #qos_cpu-cp         50     10000                      
-   #qos_gpu-cp         50     10000                      
-   #qos_prepo+          0     10000                  
    project=""
    if [ "$gpu" = 1 ]
    then
-      [ "`id | grep ikp`" != "" ] && project="ikp@gpu" && gpus_per_node=4 # Si on ne reserve qu'1 GPU plantage memoire possible...
+      # See http://www.idris.fr/jean-zay/gpu/jean-zay-gpu-exec_partition_slurm.html 
+      queue=gpu_p13 && constraint=v100-16g # pour gpu_p3 ou constraint=v100-32g (gpu_p1)        
+      gpus_per_node=4 # Si on ne reserve qu'1 GPU plantage memoire possible...
+      qos=qos_gpu-t3 && cpu=1200 && [ "$prod" != 1 ] && qos=qos_gpu-dev && cpu=120 
+      #qos=qos_gpu-t4 && cpu=6000
+      [ "`id | grep ikp`" != "" ] && project="ikp@gpu"
    else
+      queue=cpu_p1
+      qos=qos_cpu-t3 && cpu=1200 && [ "$prod" != 1 ] && qos=qos_cpu-dev && cpu=120 
+      #qos=qos_cpu-t4 && cpu=6000
       [ "`id | grep fej`" != "" ] && project="fej@cpu"
-      cpus_per_task=40
    fi
    hintnomultithread=1
-   cpu=30 && [ "$prod" = 1 ] && cpu=110 # 30 minutes or 1 day
-   ntasks=20 # number of cores ?
+   ntasks=40 # number of cores max
    node=1 # --exclusive
    mpirun="srun -n \$SLURM_NTASKS"
    sub=SLURM
