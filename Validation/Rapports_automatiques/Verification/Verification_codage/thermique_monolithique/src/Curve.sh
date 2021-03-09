c39f88460c8d92b919694faf01fa100256e210d7
Antoine Gerschenfeld <antoine.gerschenfeld@cea.fr>
2021-03-09 09:27:25 +0100

    [Pb Multiphase] Update thermique_monolithique test case

--- Validation/Rapports_automatiques/Verification/Verification_codage/thermique_monolithique/src/Curve.sh
+++ Validation/Rapports_automatiques/Verification/Verification_codage/thermique_monolithique/src/Curve.sh
@@ -1,17 +1,4 @@
-paste VDF/mono_avec_iteration_pb1_Diffusion_chaleur.out VDF/mono_avec_iteration_pb2_Diffusion_chaleur.out 		> VDF/mono_avec_iteration_pb1pb2_Diffusion_chaleur.out
-paste VDF/mono_sans_iteration_pb1_Diffusion_chaleur.out VDF/mono_sans_iteration_pb2_Diffusion_chaleur.out		> VDF/mono_sans_iteration_pb1pb2_Diffusion_chaleur.out
-paste VDF/seq_avec_iteration_pb1_Diffusion_chaleur.out VDF/seq_avec_iteration_pb2_Diffusion_chaleur.out			> VDF/seq_avec_iteration_pb1pb2_Diffusion_chaleur.out
-paste VDF/seq_sans_iteration_pb1_Diffusion_chaleur.out VDF/seq_sans_iteration_pb2_Diffusion_chaleur.out			> VDF/seq_sans_iteration_pb1pb2_Diffusion_chaleur.out
-paste PolyMAC/mono_avec_iteration_pb1_Diffusion_chaleur.out PolyMAC/mono_avec_iteration_pb2_Diffusion_chaleur.out	> PolyMAC/mono_avec_iteration_pb1pb2_Diffusion_chaleur.out
-paste PolyMAC/mono_sans_iteration_pb1_Diffusion_chaleur.out PolyMAC/mono_sans_iteration_pb2_Diffusion_chaleur.out	> PolyMAC/mono_sans_iteration_pb1pb2_Diffusion_chaleur.out
-paste PolyMAC/seq_avec_iteration_pb1_Diffusion_chaleur.out PolyMAC/seq_avec_iteration_pb2_Diffusion_chaleur.out 	> PolyMAC/seq_avec_iteration_pb1pb2_Diffusion_chaleur.out
-paste PolyMAC/seq_sans_iteration_pb1_Diffusion_chaleur.out PolyMAC/seq_sans_iteration_pb2_Diffusion_chaleur.out		> PolyMAC/seq_sans_iteration_pb1pb2_Diffusion_chaleur.out
-grep 'The Probleme_Couple problem pbc has converged after' VDF/mono_avec_iteration.out | awk '{print $8}'	> VDF/mono_avec_iteration.txt
-grep 'The Probleme_Couple problem pbc has converged after' VDF/mono_sans_iteration.out | awk '{print $8}'	> VDF/mono_sans_iteration.txt
-grep 'The Probleme_Couple problem pbc has converged after' VDF/seq_avec_iteration.out | awk '{print $8}'	> VDF/seq_avec_iteration.txt
-grep 'The Probleme_Couple problem pbc has converged after' VDF/seq_sans_iteration.out | awk '{print $8}'	> VDF/seq_sans_iteration.txt
-grep 'The Probleme_Couple problem pbc has converged after' PolyMAC/mono_avec_iteration.out | awk '{print $8}'	> PolyMAC/mono_avec_iteration.txt
-grep 'The Probleme_Couple problem pbc has converged after' PolyMAC/mono_sans_iteration.out | awk '{print $8}'	> PolyMAC/mono_sans_iteration.txt
-grep 'The Probleme_Couple problem pbc has converged after' PolyMAC/seq_avec_iteration.out | awk '{print $8}'	> PolyMAC/seq_avec_iteration.txt
-grep 'The Probleme_Couple problem pbc has converged after' PolyMAC/seq_sans_iteration.out | awk '{print $8}'	> PolyMAC/seq_sans_iteration.txt
-
+for dis in VDF PolyMAC CoviMAC; do for type in mono seq; do for iter in avec sans; do
+paste ${dis}/${type}_${iter}_iteration_pb1_Diffusion_chaleur.out ${dis}/${type}_${iter}_iteration_pb2_Diffusion_chaleur.out > ${dis}/${type}_${iter}_iteration_pb1pb2_Diffusion_chaleur.out
+grep 'The Probleme_Couple problem pbc has converged after' ${dis}/${type}_${iter}_iteration.out | awk '{print $8}'          > ${dis}/${type}_${iter}_iteration.txt
+done; done; done
