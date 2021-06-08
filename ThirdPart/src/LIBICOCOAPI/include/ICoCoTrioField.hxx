7f856adc88e78d09bc90b7ace754223b08c14983
abn <adrien.bruneton@cea.fr>
2021-06-08 20:54:40 +0200

    [ICoCo]: updating TrioField to forbid copy ctor

--- ThirdPart/src/LIBICOCOAPI/include/ICoCoTrioField.hxx
+++ ThirdPart/src/LIBICOCOAPI/include/ICoCoTrioField.hxx
@@ -12,48 +12,86 @@
 #include <ICoCoField.hxx>
 namespace ICoCo
 {
-//////////////////////////////////////////////////////////////////////////////
-//
-// .DESCRIPTION
-//     class TrioField, used for coupling Trio codes via the ICoCo interface
-//     This structure contains all the necessary information
-//     for constructing MEDCouplingFieldDouble (with the addition of the MPI
-//     communicator).
-//     This structure can either own or not _field values (_has_field_ownership)
-//     For _coords, _connectivity and _field, a null pointer means no data allocated.
-//     _coords and _connectivity tables, when allocated, are always owned by the TrioField.
-//
-//////////////////////////////////////////////////////////////////////////////
-class TrioField : public Field
-{
- public:
-  TrioField();
-  TrioField(const TrioField& OtherField);
-  ~TrioField();
-  void clear();
-  void set_standalone();
-  void dummy_geom();
-  TrioField& operator=(const TrioField& NewField);
-  void save(std::ostream& os) const;
-  void restore(std::istream& in);
-  int nb_values() const;
 
- public:
-  int _type;  // 0 elem 1 nodes
-  int _mesh_dim;
-  int _space_dim;
-  int _nbnodes;
-  int _nodes_per_elem;
-  int _nb_elems;
-  int _itnumber;
-  int* _connectivity;
-  double* _coords;
+  /*! @brief Field data stored internally as a TrioField object.
+   *
+   *  This structure contains all the necessary information
+   *  for constructing MEDCouplingFieldDouble (with the addition of the MPI
+   *  communicator).
+   *  This structure can either own or not _field values (_has_field_ownership)
+   *  For _coords, _connectivity and _field, a null pointer means no data allocated.
+   *  _coords and _connectivity tables, when allocated, are always owned by the TrioField.
+   *
+   *  Copy constructor and assignment operator raise an exception as they are not implemented.
+   */
+  class TrioField : public Field
+  {
+  public:
+    /*! @brief Builds an empty field.
+     */
+    TrioField();
 
-  double _time1, _time2;
-  int _nb_field_components;
-  double* _field;
-  bool _has_field_ownership;
-};
+    /*! @brief Copy constructor - FORBIDDEN.
+     * @throws ICoCo::NotImplemented
+     */
+    TrioField(const TrioField& OtherField);
+
+    /*! @brief Assignment operator - FORBIDDEN.
+     * @throws ICoCo::NotImplemented
+     */
+    TrioField& operator=(const TrioField& NewField);
+
+    /*! @brief Destructor.
+     */
+    ~TrioField();
+
+    /*! @brief Clear and reset all internal data structures.
+     *
+     * After the call to clear(), all pointers are null and field ownership is false.
+     * Arrays are deleted if necessary
+     */
+    void clear();
+
+    /*! @brief Acquire field ownership.
+     *
+     * After a call to set_standalone(), field ownership is true and field is allocated
+     * to the size _nb_field_components*nb_values().
+     * The values of the field have been copied if necessary.
+     */
+    void set_standalone();
+
+    /*! @brief Used to simulate a 0D geometry (Cathare/Trio for example).
+     */
+    void dummy_geom();
+
+    /*! @brief Save field to a .field file
+     */
+    void save(std::ostream& os) const;
+
+    /*! @brief Restore field from a .field file
+     */
+    void restore(std::istream& in);
+
+    /*! @brief The size of field is nb_values()*_nb_field_components
+     */
+    int nb_values() const;
+
+  public:
+    int _type;         ///< 0 field on element, 1 field on nodes
+    int _mesh_dim;     ///< Mesh dimension
+    int _space_dim;    ///< Space dimension
+    int _nbnodes;      ///< Number of nodes
+    int _nodes_per_elem;  ///< Number of nodes for a single element
+    int _nb_elems;     ///< Number of elements
+    int _itnumber;     ///< (time) iteration number
+    int* _connectivity;  ///< Nodal connectivity array
+    double* _coords;   ///< Coordinate array
+
+    double _time1, _time2;
+    int _nb_field_components;
+    double* _field;
+    bool _has_field_ownership;
+  };
 }  // namespace ICoCo
 
 #endif
