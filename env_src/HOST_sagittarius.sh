6740232ed61ea749f2ac34d33ceec393c7a382ba
Pierre LEDAC <pierre.ledac@cea.fr>
2021-07-07 18:57:57 +0200

    [Portage] 1.8.3 sur sagittarius

--- env_src/HOST_sagittarius.sh
+++ env_src/HOST_sagittarius.sh
@@ -23,7 +23,7 @@ define_modules_config()
    echo "# Module $module detected and loaded on $HOST."
    echo "module purge 1>/dev/null" >> $env
    echo "module load $module 1>/dev/null" >> $env
-   echo "TRUST_ATELIER_CMAKE=0 && export TRUST_ATELIER_CMAKE # To speedup Baltik build on beefgs file system" >> $env  
+   # echo "TRUST_ATELIER_CMAKE=0 && export TRUST_ATELIER_CMAKE # To speedup Baltik build on beefgs file system" >> $env  
    . $env
    # Creation wrapper qstat -> squeue
    echo "#!/bin/bash
