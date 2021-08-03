71f4fe08a55efdc1fbb8a28cbd0011eee090a2fb
Pierre LEDAC <pierre.ledac@cea.fr>
2021-08-03 14:41:06 +0200

    [GPU] Portage sur topaze

--- env_src/HOST_topaze.sh
+++ env_src/HOST_topaze.sh
@@ -27,13 +27,13 @@ define_modules_config()
    # Load modules
    if [ "$TRUST_USE_CUDA" = 1 ]
    then
-      echo "Not supported yet"
-      exit
-      module=""
+      module="gnu/8.3.0 mpi/openmpi/4.0.5 cuda/11.2"
    else
       module="gnu/11.1.0 mpi/openmpi/4.0.5"
       module="intel/20.0.4 mpi/openmpi/4.0.5"
    fi
+   module=$module" texlive"
+   #
    # Ajout pour charger l'espace disque a la place de SCRATCHDIR pas encore disponible sur topaze:
    [ "`id | grep gch0504`" != "" ] && sw=dfldatadir/gch0504
    echo "# Module $module detected and loaded on $HOST."
@@ -54,7 +54,7 @@ define_soumission_batch()
    [ "$gpu"  = 1 ] && soumission=1
    ntasks=128 # AMD Milan 128 cores/node
    [ "$NB_PROCS" = 1 ] && cpus_per_task=$ntasks # Pour avoir le max de memoire (celle du noeud) lors du decoupage
-   # ccc_mqinfo :
+# ccc_mqinfo :
 # Name     Partition  Priority  MaxCPUs  SumCPUs  MaxNodes  MaxRun  MaxSub     MaxTime
 # -------  ---------  --------  -------  -------  --------  ------  ------  ----------
 # -------  ---------  --------  -------  -------  --------  ------  ------  ----------
@@ -65,7 +65,9 @@ define_soumission_batch()
 # test          a100        40                           2               2    00:30:00
 # test         milan        40      768      768         6               2    00:30:00
    cpu=1800 && [ "$prod" = 1 ] && cpu=86400 # 30 minutes or 1 day
-   # Priorite superieure avec test si pas plus de 7-_ coeurs  
+   # Partition a100 (4 cartes a100 par noeud, 48 noeuds au total soit 192 GPU au total): 1 GPU par 32 coeurs alloues donc si moins de 32 coeurs, on fixe a 32:
+   [ "$gpu" = 1 ] && queue=a100 && ntasks=128 && [ $NB_PROCS -lt 32 ] && cpus_per_task=32
+   # Priorite superieure avec test si pas plus de 768 coeurs
    if [ "$prod" = 1 ] || [ $NB_PROCS -gt 768 ]
    then
       qos=normal
@@ -74,11 +76,13 @@ define_soumission_batch()
       qos=test
       cpu=1800
    fi
-   # ccc_mpinfo : 
+# ccc_mpinfo :
 #                       --------------CPUS------------  -------------NODES------------
 # PARTITION    STATUS   TOTAL   DOWN    USED    FREE    TOTAL   DOWN    USED    FREE     MpC   CpN SpN CpS TpC
 # ---------    ------   ------  ------  ------  ------  ------  ------  ------  ------   ----- --- --- --- ---
 # milan        up       108544    3584       0  104960     848      28       0     820    2000  128   2  64   1
+# xlarge       up          512     128       0     384       4       1       0       3   31250  128   2  64   1
+# a100         up         6016       0       0    6016      47       0       0      47    4000  128   2  64   1
    queue=milan
    [ "$bigmem" = 1 ] && queue=xlarge
    [ "$gpu" = 1 ]    && queue=a100  # && [ "`sinfo | grep $queue | grep idle`" = "" ] && [ $qos = test ] && queue=hybrid # Pour tester si v100 pas libre, on prend hybrid&& [ "`sinfo | grep $queue | grep idle`" = "" ] && [ $qos = test ] && queue=hybrid # Pour tester si v100 pas libre, on prend hybrid
