6d7500aed548222aa058bd604410395d5bd9a87a
Pierre LEDAC <pierre.ledac@cea.fr>
2021-08-18 09:49:12 +0200

    [ARM] Support compilateur Armclang sur a64fx mais pas mis par defaut.

--- env_src/HOST_irene-arm-ccrt.sh
+++ env_src/HOST_irene-arm-ccrt.sh
@@ -24,8 +24,9 @@ define_modules_config()
    # qstat inexistente sur les dernieres machines du CCRT/TGCC
    echo "Command qstat created on $HOST"
    cp $TRUST_ROOT/bin/KSH/qstat_wrapper $TRUST_ROOT/bin/KSH/qstat
+   module="arm-compiler/21.0.0 mpi/openmpi/4.0.5 swig/4.0.2" 
    module="fujitsu/1.1.0 mpi/openmpi/4.0.5 swig/4.0.2" 
-   module="gnu/11.1.0 mpi/openmpi/4.0.5 swig/4.0.2" 
+   module="gnu/11.1.0 mpi/openmpi/4.0.5 swig/4.0.2" # Mis en premier car le plus robuste
    #
    echo "# Module $module detected and loaded on $HOST."
    echo "module purge 1>/dev/null" >> $env
