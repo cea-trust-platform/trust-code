15defaa544b18d119decef4ab2c2b8952843ebb4
triou <triou@cea.fr>
2021-08-24 14:09:00 +0200

    Allow VisIt download on Fedora 26

--- bin/gestion_externalpackages/get_files.sh
+++ bin/gestion_externalpackages/get_files.sh
@@ -8,5 +8,5 @@ fichier="$DIR/liste_dep.csv"
 sed "s/General/./g;s/MinicondaLocChannLinux/Miniconda\/local_channel\/linux-64/g; s/MinicondaLocChannNoarch/Miniconda\/local_channel\/noarch/g;s/MinicondaLocChann/Miniconda\/local_channel/g" $fichier |awk -F, '{print "mkdir -p",$1}'  | grep -v "mkdir dir" | sort -u
  
 
-sed "s/General/./g;s/MinicondaLocChannLinux/Miniconda\/local_channel\/linux-64/g; s/MinicondaLocChannNoarch/Miniconda\/local_channel\/noarch/g;s/MinicondaLocChann/Miniconda\/local_channel/g" $fichier | awk -F, '{print "[ ! -f "$1"/"$2" ]  &&  wget "$7" --no-check-certificate -O " $1"/"$2";touch "$1"/"$2 }'  
+sed "s/General/./g;s/MinicondaLocChannLinux/Miniconda\/local_channel\/linux-64/g; s/MinicondaLocChannNoarch/Miniconda\/local_channel\/noarch/g;s/MinicondaLocChann/Miniconda\/local_channel/g" $fichier | awk -F, '{print "[ ! -f "$1"/"$2" ]  &&  wget "$7" --no-check-certificate -O " $1"/"$2";touch "$1"/"$2 }' | grep -v "dir/paquet"
 #sed "s/General/./" $fichier | awk -F, '{print "[ ! -f "$1"/"$2" ]  &&  curl -L "$7" -o " $1"/"$2";touch "$1"/"$2 }'  
