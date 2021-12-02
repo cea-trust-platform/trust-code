cb74119968f09c6f159e5e4e033fcca9794ac5fa
Pierre LEDAC <pl254994@o186i143.gre.smktg.hpecorp.net>
2021-12-02 17:19:31 +0100

    [Portability] Support Cray compilers craycc, crayCC, crayftn

--- env_src/HOST_cray-radeon.sh
+++ env_src/HOST_cray-radeon.sh
@@ -9,7 +9,12 @@ define_modules_config()
    echo "source /etc/profile" >> $env
    #
    # Load modules
-   module="craype-x86-rome craype-network-ofi PrgEnv-cray/8.1.0 rocm/rocm craype-accel-amd-gfx908"
+   if [ "$TRUST_USE_CUDA" = 1 ]
+   then
+      module="craype-x86-rome craype-network-ofi PrgEnv-cray/8.1.0"
+   else
+      module="craype-x86-rome craype-network-ofi PrgEnv-cray/8.1.0"
+   fi
    #
    echo "# Module $module detected and loaded on $HOST."
    echo "module purge 1>/dev/null" >> $env
@@ -28,25 +33,28 @@ squeue" > $TRUST_ROOT/bin/qstat
 define_soumission_batch()
 {
    soumission=128 # Provisoire
+   exit # ToDo
+   soumission=2
    [ "$prod" = 1 ] && soumission=1
    [ "$gpu"  = 1 ] && soumission=1
+   # http://www.idris.fr/eng/jean-zay/gpu/jean-zay-gpu-exec_partition_slurm-eng.html Une seule partition gpu_p13
    project=""
    if [ "$gpu" = 1 ]
    then
-      exit -1 # ToDo
-      # See http://www.idris.fr/jean-zay/gpu/jean-zay-gpu-exec_partition_slurm.html 
-      queue=gpu_p13 && constraint=v100-16g # pour gpu_p3 ou constraint=v100-32g (gpu_p1)        
+      # See http://www.idris.fr/jean-zay/gpu/jean-zay-gpu-exec_partition_slurm.html
+      queue=gpu_p13 && constraint=v100-16g # pour gpu_p3 ou constraint=v100-32g (gpu_p1)
       gpus_per_node=4 # Si on ne reserve qu'1 GPU plantage memoire possible...
-      qos=qos_gpu-t3 && cpu=1200 && [ "$prod" != 1 ] && qos=qos_gpu-dev && cpu=120 
+      qos=qos_gpu-t3 && cpu=1200 && [ "$prod" != 1 ] && qos=qos_gpu-dev && cpu=120
       #qos=qos_gpu-t4 && cpu=6000
       [ "`id | grep eia`" != "" ] && project="eia@gpu"
    else
       queue=cpu_p1
-      qos=qos_cpu-t3 && cpu=1200 && [ "$prod" != 1 ] && qos=qos_cpu-dev && cpu=120 
+      qos=qos_cpu-t3 && cpu=1200 && [ "$prod" != 1 ] && qos=qos_cpu-dev && cpu=120
       #qos=qos_cpu-t4 && cpu=6000
       [ "`id | grep fej`" != "" ] && project="fej@cpu"
    fi
-   ntasks=128 # number of cores max
+   hintnomultithread=1
+   ntasks=40 # number of cores max
    node=1 # --exclusive
    mpirun="srun -n \$SLURM_NTASKS"
    sub=SLURM
