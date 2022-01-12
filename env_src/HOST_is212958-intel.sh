eb5ea1e4b613737833a8c477661e682c9e7a4cd6
triou <triou@cea.fr>
2022-01-12 14:01:38 +0100

    Update release notes

--- env_src/HOST_is212958-intel.sh
+++ env_src/HOST_is212958-intel.sh
@@ -7,8 +7,9 @@ define_modules_config()
 {
    env=$TRUST_ROOT/env/machine.env
    #
+   echo "source /etc/profile" >> $env
    echo "module purge" >> $env
-   echo "module load compilers/intel/2019_update3 mpi/intelmpi/2019_update3 # Load mpi compilers" >> $env
+   echo "module load compilers/intel/2019_update5 mpi/intelmpi/2019_update5 || exit -1 # Load mpi compilers" >> $env
    . $env
 }
 
