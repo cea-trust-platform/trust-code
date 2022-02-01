281091f5ff5ae12a8bb51b398c3ee2ca1ba47b8b
Yannick GORSSE <yannick.gorsse@cea.fr>
2022-02-01 15:02:39 +0100

    [Operateurs VDF] Validation file for a 2D driven cavity

--- Validation/Rapports_automatiques/Verification/PolyMAC/cavite_entrainee_2D/src/prepare_meshes.py
+++
@@ -1,8 +0,0 @@
-import sys
-from weird_mesh import getWeirdMesh
-argument = sys.argv[1]
-
-maillages = {"cartesian": "o", "triangle": "///\\//\\", "non_conformal": "o743o52"}
-xmin, xmax, ymin, ymax = 0., 1., 0., 1.
-n = 12 if argument == "non_conformal" else 26 if argument == "triangle" else 32
-getWeirdMesh(xmin, xmax, ymin, ymax, list(range(n + 1)), list(range(n + 1)), maillages[argument]).write("mesh.med", 2)
---
+++ Validation/Rapports_automatiques/Verification/Verification_codage/cavite_entrainee_2D/src/prepare_python.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+# -*-coding:utf-8 -*
+from string import Template
+from weird_mesh import getWeirdMesh
+import os
+
+def build_mesh(type_elem, xmin, xmax, ymin, ymax, nx, ny, path_to_folder):
+    maillages = {"cartesian": "o", "triangle": "x", "NCa": "o3", "NCr": "o3"}
+    func_mesh = {"cartesian": None, "triangle": None, "NCa": lambda x, y: 5 * int(y > 0.4 and y < 0.6), "NCr": lambda x, y: 5 * int(x > 0.4 and x < 0.6)}
+    getWeirdMesh(xmin, xmax, ymin, ymax, list(range(nx + 1)), list(range(ny + 1)), maillages[type_elem], unpo=(not type_elem.startswith("NC")), func=func_mesh[type_elem]).write("{}/mesh.med".format(path_to_folder), 2)
+
+if __name__ == "__main__":
+    lx, ly = 1, 1
+    mu, vwall = 2.5, 1
+    # assert (lx / ly).is_integer(), "lx must be a multiple of ly!"
+    ny = 41
+    dic_dis = {"cartesian" : ["VDF", "PolyMAC", "CoviMAC"], "triangle" : ["VEFPreP1B", "PolyMAC", "CoviMAC"], "NCa" : ["PolyMAC", "CoviMAC"], "NCr" : ["PolyMAC", "CoviMAC"]}
+    list_dis = []
+    for k, v in dic_dis.items():
+        for d in v:
+            list_dis.append(f"{k}/{d}")
+            nx = int(lx / ly * ny)
+            os.system(f"mkdir -p {k}/{d}")
+            build_mesh(k, 0, lx, 0, ly, nx, ny, f"{k}/{d}")
+            dic = {
+                "vwall" : vwall,
+                "dis" : d,
+                # "lx" : lx,
+                # "ly" : ly,
+                "mu" : mu,
+            }
+
+            with open("jdd.data", "r") as file:
+                filedata = Template(file.read())
+            result = filedata.substitute(dic)
+            with open(f"{k}/{d}/jdd.data", "w") as file:
+                file.write(result)
+
+    with open("cavite_2D.prm.P.m", "r") as file:
+        fileprm = Template(file.read())
+    result = fileprm.safe_substitute(list_dis=" ".join(sorted(list(set(list(list_dis))))))
+
+    with open("cavite_2D.prm.P", "w") as file:
+        file.write(result)
+
