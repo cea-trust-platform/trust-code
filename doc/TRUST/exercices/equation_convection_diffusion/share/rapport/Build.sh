a03385f361b605f3ece13d8a8d2d4f7262b2ed2f
triou <triou@cea.fr>
2022-05-06 15:19:36 +0200

    Update equation_convection_diffusion report. tip: when updating report, use png format in Doxyfile, otherwise figs should be converted to pdf

--- doc/TRUST/exercices/equation_convection_diffusion/share/rapport/Build.sh
+++ doc/TRUST/exercices/equation_convection_diffusion/share/rapport/Build.sh
@@ -17,10 +17,10 @@ make check_all_optim validation check_gui #  doxygen
 
 cd $proj/build/rapport
 
-for file in classProbleme__base.png classEquation__base.png classConvection__Diffusion__std.png classConvection__Diffusion__Concentration.png
+for file in classProbleme__base classEquation__base classConvection__Diffusion__std classConvection__Diffusion__Concentration
 do
 
-cp $TRUST_ROOT/doc/html/$file .
+cp $TRUST_ROOT/doc/html/$file"__inherit__graph.png" $file.png
 
 done
 
