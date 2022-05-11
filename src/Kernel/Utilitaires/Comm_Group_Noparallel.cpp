483a3dc5b5cd4f06355995358690e4f13ddabd7a
Elie Saikali <elie.saikali@cea.fr>
2022-05-11 09:57:29 +0200

    [Templates Divers] Function templates for Comm_Group_Noparallel

--- src/Kernel/Utilitaires/Comm_Group_Noparallel.cpp
+++ src/Kernel/Utilitaires/Comm_Group_Noparallel.cpp
@@ -1,5 +1,5 @@
 /****************************************************************************
-* Copyright (c) 2020, CEA
+* Copyright (c) 2022, CEA
 * All rights reserved.
 *
 * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
@@ -26,13 +26,8 @@ Implemente_instanciable_sans_constructeur_ni_destructeur(Comm_Group_Noparallel,"
 int Comm_Group_Noparallel::sending_ = 0;
 
 // Description : constructeur pour le groupe Tous
-Comm_Group_Noparallel::Comm_Group_Noparallel()
-{
-  Comm_Group::init_group_trio(1 /* nproc */, 0 /* rank */);
-}
-Comm_Group_Noparallel::~Comm_Group_Noparallel()
-{
-}
+Comm_Group_Noparallel::Comm_Group_Noparallel() { Comm_Group::init_group_trio(1 /* nproc */, 0 /* rank */); }
+Comm_Group_Noparallel::~Comm_Group_Noparallel() { }
 
 // Description: constructeur pour un sous-groupe */
 void Comm_Group_Noparallel::init_group(const ArrOfInt& pe_list)
@@ -41,9 +36,7 @@ void Comm_Group_Noparallel::init_group(const ArrOfInt& pe_list)
   Comm_Group::init_group(pe_list);
 }
 
-void Comm_Group_Noparallel::abort() const
-{
-}
+void Comm_Group_Noparallel::abort() const { }
 
 Sortie& Comm_Group_Noparallel::printOn(Sortie& os) const
 {
@@ -59,79 +52,17 @@ Entree& Comm_Group_Noparallel::readOn(Entree& is)
   return is;
 }
 
-// Description: Operation collective sur chaque element d'un tableau de taille n.
-//  x et resu doivent pointer sur un tableau de taille n.
-void Comm_Group_Noparallel::mp_collective_op(const double *x, double *resu, int n, Collective_Op op) const
-{
-  if (op != COLL_PARTIAL_SUM)
-    for (int i = 0; i < n; i++)
-      resu[i] = x[i];
-  else
-    for (int i = 0; i < n; i++)
-      resu[i] = 0.;
-}
+void Comm_Group_Noparallel::barrier(int tag) const { return; }
 
-// Description: Operation collective sur chaque element d'un tableau de taille n.
-//  x, resu et op doivent pointer sur un tableau de taille n.
-//  Pour chaque element x[i] on effectue l'operation op[i] et on met le resultat dans resu[i]
-void Comm_Group_Noparallel::mp_collective_op(const int *x, int *resu, int n, Collective_Op op) const
-{
-  if (op != COLL_PARTIAL_SUM)
-    for (int i = 0; i < n; i++)
-      resu[i] = x[i];
-  else
-    for (int i = 0; i < n; i++)
-      resu[i] = 0;
-}
-
-
-// Description: Operation collective sur chaque element d'un tableau de taille n.
-//  x et resu doivent pointer sur un tableau de taille n.
-void Comm_Group_Noparallel::mp_collective_op(const double *x, double *resu, const Collective_Op *op, int n) const
-{
-  for (int i = 0; i < n; i++)
-    {
-      if (op[i] != COLL_PARTIAL_SUM)
-        resu[i] = x[i];
-      else
-        resu[i] = 0.;
-    }
-}
-
-// Description: Operation collective sur chaque element d'un tableau de taille n.
-//  x, resu et op doivent pointer sur un tableau de taille n.
-//  Pour chaque element x[i] on effectue l'operation op[i] et on met le resultat dans resu[i]
-void Comm_Group_Noparallel::mp_collective_op(const int *x, int *resu, const Collective_Op *op, int n) const
-{
-  for (int i = 0; i < n; i++)
-    {
-      if (op[i] != COLL_PARTIAL_SUM)
-        resu[i] = x[i];
-      else
-        resu[i] = 0;
-    }
-}
-
-void Comm_Group_Noparallel::barrier(int tag) const
-{
-  return;
-}
-
-int Comm_Group_Noparallel::reverse_send_recv_list(const ArrOfInt& src_list,
-                                                  ArrOfInt& dest_list) const
+int Comm_Group_Noparallel::reverse_send_recv_list(const ArrOfInt& src_list, ArrOfInt& dest_list) const
 {
   assert(src_list.size_array() == 0);
   assert(dest_list.size_array() == 0);
   return 0;
 }
 
-void Comm_Group_Noparallel::send_recv_start(const ArrOfInt& send_list,
-                                            const ArrOfInt& send_size,
-                                            const char * const * const send_buffers,
-                                            const ArrOfInt& recv_list,
-                                            const ArrOfInt& recv_size,
-                                            char * const * const recv_buffers,
-                                            TypeHint typehint) const
+void Comm_Group_Noparallel::send_recv_start(const ArrOfInt& send_list, const ArrOfInt& send_size, const char *const*const send_buffers, const ArrOfInt& recv_list, const ArrOfInt& recv_size,
+                                            char *const*const recv_buffers, TypeHint typehint) const
 {
   assert(sending_ == 0);
   if (send_list.size_array() == 1 && send_list[0] == 0 &&
@@ -156,20 +87,11 @@ void Comm_Group_Noparallel::send_recv_finish() const
   sending_ = 0;
 }
 
-void Comm_Group_Noparallel::send(int pe, const void *buffer, int size, int tag) const
-{
-  assert(0);
-}
+void Comm_Group_Noparallel::send(int pe, const void *buffer, int size, int tag) const { assert(0); }
 
-void Comm_Group_Noparallel::recv(int pe, void *buffer, int size, int tag) const
-{
-  assert(0);
-}
+void Comm_Group_Noparallel::recv(int pe, void *buffer, int size, int tag) const { assert(0); }
 
-void Comm_Group_Noparallel::broadcast(void *buffer, int size, int pe_source) const
-{
-  return;
-}
+void Comm_Group_Noparallel::broadcast(void *buffer, int size, int pe_source) const { return; }
 
 void Comm_Group_Noparallel::all_to_all(const void *src_buffer, void *dest_buffer, int data_size) const
 {
--- src/Kernel/Utilitaires/Comm_Group_Noparallel.h
+++ src/Kernel/Utilitaires/Comm_Group_Noparallel.h
@@ -19,6 +19,7 @@
 // Version:     /main/16
 //
 //////////////////////////////////////////////////////////////////////////////
+
 #ifndef Comm_Group_Noparallel_included
 #define Comm_Group_Noparallel_included
 
@@ -32,33 +33,54 @@ public:
   ~Comm_Group_Noparallel() override;
   void abort() const override;
 
-  void mp_collective_op(const double *x, double *resu, int n, Collective_Op op) const override;
-  void mp_collective_op(const double *x, double *resu, const Collective_Op *op, int n) const override;
-  void mp_collective_op(const int *x, int *resu, int n, Collective_Op op) const override;
-  void mp_collective_op(const int *x, int *resu, const Collective_Op *op, int n) const override;
+  void mp_collective_op(const double *x, double *resu, int n, Collective_Op op) const override { mp_collective_op_template<double>(x,resu,n,op); }
+  void mp_collective_op(const double *x, double *resu, const Collective_Op *op, int n) const override { mp_collective_op_template<double>(x,resu,op,n); }
+  void mp_collective_op(const int *x, int *resu, int n, Collective_Op op) const override { mp_collective_op_template<int>(x,resu,n,op); }
+  void mp_collective_op(const int *x, int *resu, const Collective_Op *op, int n) const override { mp_collective_op_template<int>(x,resu,op,n); }
 
-  void   barrier(int tag) const override;
-  int    reverse_send_recv_list(const ArrOfInt& src_list,
-                                ArrOfInt& dest_list) const;
+  void barrier(int tag) const override;
+  int reverse_send_recv_list(const ArrOfInt& src_list, ArrOfInt& dest_list) const;
 
   Comm_Group_Noparallel(const ArrOfInt& pe_list);
-  void send_recv_start(const ArrOfInt& send_list,
-                       const ArrOfInt& send_size,
-                       const char * const * const send_buffers,
-                       const ArrOfInt& recv_list,
-                       const ArrOfInt& recv_size,
-                       char * const * const recv_buffers,
+  void send_recv_start(const ArrOfInt& send_list, const ArrOfInt& send_size, const char *const*const send_buffers, const ArrOfInt& recv_list, const ArrOfInt& recv_size, char *const*const recv_buffers,
                        TypeHint typehint = CHAR) const override;
+
   void send_recv_finish() const override;
   void send(int pe, const void *buffer, int size, int tag) const override; // Envoi bloquant
   void recv(int pe, void *buffer, int size, int tag) const override; // Reception bloquante
   void broadcast(void *buffer, int size, int pe_source) const override;
   void all_to_all(const void *src_buffer, void *dest_buffer, int data_size) const override;
+
 protected:
   void init_group(const ArrOfInt& pe_list) override;
-private:
 
+private:
   static int sending_;
+
+  //  Operation collective sur chaque element d'un tableau de taille n.
+  //  x et resu doivent pointer sur un tableau de taille n.
+  template <typename _TYPE_>
+  void mp_collective_op_template(const _TYPE_ *x, _TYPE_ *resu, int n, Collective_Op op) const
+  {
+    if (op != COLL_PARTIAL_SUM)
+      for (int i = 0; i < n; i++) resu[i] = x[i];
+    else
+      for (int i = 0; i < n; i++) resu[i] = 0;
+  }
+
+  // Description: Operation collective sur chaque element d'un tableau de taille n.
+  //  x, resu et op doivent pointer sur un tableau de taille n.
+  //  Pour chaque element x[i] on effectue l'operation op[i] et on met le resultat dans resu[i]
+  template <typename _TYPE_>
+  void mp_collective_op_template(const _TYPE_ *x, _TYPE_ *resu, const Collective_Op *op, int n) const
+  {
+    for (int i = 0; i < n; i++)
+      {
+        if (op[i] != COLL_PARTIAL_SUM) resu[i] = x[i];
+        else resu[i] = 0;
+      }
+  }
 };
-#endif
+
+#endif /* Comm_Group_Noparallel_included */
 
