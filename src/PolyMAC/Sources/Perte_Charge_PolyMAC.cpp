9066bf256ce8fc26537129eca501dbbfe862f42f
Antoine Gerschenfeld <antoine.gerschenfeld@cea.fr>
2022-05-16 11:07:29 +0200

    [PolyMAC] Perte_Charge_PolyMAC: bugfix

--- src/PolyMAC/Sources/Perte_Charge_PolyMAC.cpp
+++ src/PolyMAC/Sources/Perte_Charge_PolyMAC.cpp
@@ -63,14 +63,14 @@ void Perte_Charge_PolyMAC::ajouter_blocs(matrices_t matrices, DoubleTab& secmem,
   const Zone_Poly_base& zone = ref_cast(Zone_Poly_base, equation().zone_dis().valeur());
   const Champ_Face_PolyMAC& ch = ref_cast(Champ_Face_PolyMAC, equation().inconnue().valeur());
   const Champ_Don& dh = diam_hydr;
-  const DoubleTab& xp = zone.xp(), &xv = zone.xv(), &vit = la_vitesse->valeurs(), &nu = le_fluide->viscosite_cinematique().valeurs(), &vfd = zone.volumes_entrelaces_dir();
+  const DoubleTab& xp = zone.xp(), &xv = zone.xv(), &vit = la_vitesse->valeurs(), &pvit = la_vitesse->passe(), &nu = le_fluide->viscosite_cinematique().valeurs(), &vfd = zone.volumes_entrelaces_dir();
   const DoubleVect& pe = zone.porosite_elem(), &pf = zone.porosite_face(), &fs = zone.face_surfaces(), &ve = zone.volumes();
   const Sous_Zone *pssz = sous_zone ? &la_sous_zone.valeur() : NULL;
   const IntTab& e_f = zone.elem_faces(), &f_e = zone.face_voisins(), &fcl = ch.fcl();
   Matrice_Morse *mat = matrices.count(ch.le_nom().getString()) ? matrices.at(ch.le_nom().getString()) : NULL;
   int i, j, f, d, D = dimension, C_nu = nu.dimension(0) == 1, C_dh = sub_type(Champ_Uniforme,diam_hydr.valeur()), n, N = vit.line_size(), poly_v2 = sub_type(Zone_PolyMAC_P0, zone), nf_tot = zone.nb_faces_tot();
   double t = equation().schema_temps().temps_courant();
-  DoubleTrav pos(D), vit_e(D), dir(D), C(N);
+  DoubleTrav pos(D), pvit_e(D), dir(D), C(N);
 
   /* contribution de chaque element ou on applique la perte de charge */
   for (i = 0; i < (pssz ? pssz->nb_elem_tot() : zone.nb_elem_tot()); i++)
@@ -83,13 +83,13 @@ void Perte_Charge_PolyMAC::ajouter_blocs(matrices_t matrices, DoubleTab& secmem,
 
       for (n = 0; n < N; n++)
         {
-          if (poly_v2) for (d = 0; d < D; d++) vit_e(d) = vit(nf_tot + D * e + d, n); //vitesse par variable auxiliaire
-          else for (vit_e = 0, j = 0; j < e_f.dimension(1) && (f = e_f(e, j)) >= 0; j++) for (d = 0; d < D; d++) /* PolyMAC V1 : vitesse a reconstruire */
-                vit_e(d) += fs(f) * pf(f) / (ve(e) * pe(e)) * (xv(f, d) - xp(e, d)) * (e == f_e(f, 0) ? 1 : -1) * vit(f, n);
-          double n2_ve = zone.dot(vit_e.addr(), vit_e.addr()), n_ve = sqrt(n2_ve), nu_e = nu(!C_nu * e, n), Re = std::max( n_ve * dh_e / nu_e, 1e-10), C_iso, C_dir, v_dir;
-          coeffs_perte_charge(vit_e, pos, t, n_ve, dh_e, nu_e, Re, C_iso, C_dir, v_dir, dir);
+          if (poly_v2) for (d = 0; d < D; d++) pvit_e(d) = pvit(nf_tot + D * e + d, n); //vitesse par variable auxiliaire
+          else for (pvit_e = 0, j = 0; j < e_f.dimension(1) && (f = e_f(e, j)) >= 0; j++) for (d = 0; d < D; d++) /* PolyMAC V1 : vitesse a reconstruire */
+                pvit_e(d) += fs(f) * pf(f) / (ve(e) * pe(e)) * (xv(f, d) - xp(e, d)) * (e == f_e(f, 0) ? 1 : -1) * pvit(f, n);
+          double n2_ve = zone.dot(pvit_e.addr(), pvit_e.addr()), n_ve = sqrt(n2_ve), nu_e = nu(!C_nu * e, n), Re = std::max( n_ve * dh_e / nu_e, 1e-10), C_iso, C_dir, v_dir;
+          coeffs_perte_charge(pvit_e, pos, t, n_ve, dh_e, nu_e, Re, C_iso, C_dir, v_dir, dir);
           /* coefficient correspondant a la bonne direction */
-          C(n) = C_iso + (C_dir - C_iso) * (n_ve > 1e-8 ? std::pow(zone.dot(vit_e.addr(), dir.addr()) , 2) / n2_ve : 0);
+          C(n) = C_iso + (C_dir - C_iso) * (n_ve > 1e-8 ? std::pow(zone.dot(pvit_e.addr(), dir.addr()) , 2) / n2_ve : 0);
         }
 
       /* contributions aux faces de e */
