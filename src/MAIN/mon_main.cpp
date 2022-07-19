29901a94560a787c857857307078ba02293fc4e9
triou <triou@cea.fr>
2022-07-19 10:59:15 +0200

    First commit following the 1.9.0 version

--- src/MAIN/mon_main.cpp
+++ src/MAIN/mon_main.cpp
@@ -345,7 +345,7 @@ void mon_main::dowork(const Nom& nom_du_cas)
   Cout<< " *     | |    | ) \\ \\__ | (___) | /\\____) |    | |     *  " << finl;
   Cout<< " *     )_(    |/   \\__/ (_______) \\_______)    )_(     *   " << finl;
   Cout<< " *                                                     *     " << finl;
-  Cout<< " *                  version : 1.9.0               *     "  << finl;
+  Cout<< " *                  version : 1.9.1_beta               *     "  << finl;
   Cout<< " *                       CEA - DES                     *     " << finl;
   Cout<< " *                                                     *     " << finl;
   Cout<< " * * * * * * * * * * * * * * * * * * * * * * * * * * * * " << finl;
