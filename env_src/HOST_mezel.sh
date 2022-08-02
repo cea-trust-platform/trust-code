c77ba3796358cf855d0ef34e08ff6457ed367c53
triou <triou@cea.fr>
2022-08-02 16:20:12 +0200

    Update MEZEL

--- env_src/HOST_mezel.sh
+++ env_src/HOST_mezel.sh
@@ -1,25 +1,23 @@
 #!/bin/bash
-#########################################################################
-#                                                                       #
-#    $$\      $$\$$$$$$$$\$$$$$$$$\$$$$$$$$\$$\              $$$$$$\    #
-#    $$$\    $$$ $$  _____\____$$  $$  _____$$ |            $$  __$$\   #
-#    $$$$\  $$$$ $$ |         $$  /$$ |     $$ |            \__/  $$ |  #
-#    $$\$$\$$ $$ $$$$$\      $$  / $$$$$\   $$ |             $$$$$$  |  #
-#    $$ \$$$  $$ $$  __|    $$  /  $$  __|  $$ |            $$  ____/   #
-#    $$ |\$  /$$ $$ |      $$  /   $$ |     $$ |            $$ |        #
-#    $$ | \_/ $$ $$$$$$$$\$$$$$$$$\$$$$$$$$\$$$$$$$$\       $$$$$$$$\   #
-#    \__|     \__\________\________\________\________|      \________|  #
-#                                                                       #
-#########################################################################
-#                        Bienvenue sur MEZEL2                           #
-#########################################################################
-#                                                                       #
-# Informations disponibles sur                                          #
-# https://www-linuxcad.intra.cea.fr/dokuwiki/doku.php/cluster           #
-# et                                                                    #
-# https://mezel-ms0/doku.php                                            #
-#                                                                       #
-#########################################################################
+#############################################################
+#                                                           #
+#    $$\      $$\$$$$$$$$\$$$$$$$$\$$$$$$$$\$$\             #
+#    $$$\    $$$ $$  _____\____$$  $$  _____$$ |            #
+#    $$$$\  $$$$ $$ |         $$  /$$ |     $$ |            #
+#    $$\$$\$$ $$ $$$$$\      $$  / $$$$$\   $$ |            #
+#    $$ \$$$  $$ $$  __|    $$  /  $$  __|  $$ |            #
+#    $$ |\$  /$$ $$ |      $$  /   $$ |     $$ |            #
+#    $$ | \_/ $$ $$$$$$$$\$$$$$$$$\$$$$$$$$\$$$$$$$$\       #
+#    \__|     \__\________\________\________\________|      #
+#                                                           #
+#############################################################
+#                        Bienvenue sur MEZEL                #
+#############################################################
+#                                                           #
+# Informations disponibles sur                              #
+# https://mezel-i01/                                        #
+#                                                           #
+#############################################################
 
 ##################################
 # Variables for configure script #
@@ -27,16 +25,10 @@
 define_modules_config()
 {
    env=$TRUST_ROOT/env/machine.env
-   #
-   # Load modules
-   # intel 
-   #intel=""
-   # mpi
-   #module="$intel "
-   #
-   #echo "# Module $module detected and loaded on $HOST."
-   #echo "module purge 1>/dev/null" >> $env
-   #echo "module load $module 1>/dev/null" >> $env     
+   module="gnu9/9.4.0 openmpi4/4.1.1"
+   echo "# Module $module detected and loaded on $HOST."
+   echo "module purge 1>/dev/null" >> $env
+   echo "module load $module 1>/dev/null" >> $env
    . $env
    # Creation wrapper qstat -> squeue
    echo "#!/bin/bash
@@ -50,22 +42,23 @@ squeue" > $TRUST_ROOT/bin/qstat
 define_soumission_batch()
 {
    soumission=2 && [ "$prod" = 1 ] && soumission=1
-   # sacctmgr list qos format=Name,Priority,MaxSubmit,MaxWall,MaxNodes :
+   # sacctmgr list qos format=Name,Priority,MaxSubmit,MaxWall,MaxNodes
    #      Name   Priority MaxSubmit     MaxWall MaxNodes 
    #---------- ---------- --------- ----------- -------- 
-   #    normal          0            7-00:00:00          
-   #      long          0           30-00:00:00 
+   #    normal          0
+   #    qos_max_1+      0
    qos=normal
    cpu=30 && [ "$prod" = 1 ] && cpu=1440 # 30 minutes or 1 day
    ntasks=12 # 12 cores per node for prod_amd_12c queue (48 for prod_amd_48c and 20 for prod_intel)
    # sinfo :
-   #PARTITION    AVAIL  TIMELIMIT  NODES  STATE
-   #prod*           up   infinite     31    mix
-   #prod_intel      up   infinite      5    mix
-   #prod_amd_12c    up   infinite     20    mix
-   #prod_amd_48c    up   infinite      6    mix
-   #mono            up   infinite      1    mix
-   queue=prod
+   #PARTITION   AVAIL  TIMELIMIT    NODES  STATE
+   # longq        up   30-00:00:00
+   # mediumq      up    7-00:00:00
+   # shortq*      up    1-00:00:00
+   # xshortq      up    1:00:00
+   # rjhq         up   10-00:00:00
+   # full         up    3-00:00:00
+   queue=xshortq && [ "$prod" = 1 ] && queue=mediumq
    node=0
    #if [ "$prod" = 1 ] || [ $NB_PROCS -gt $ntasks ]
    #then
