341a3a6bcb9e3b520dfc0153b16d1a2e9ff680a2
Elie Saikali <elie.saikali@cea.fr>
2022-08-25 13:53:25 +0200

    [Lata2dx] Clean Cmake + rename files/pathes (merci Adrien !)

---
+++ Outils/lata2dx/src/build.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+export MED_COUPLING_ROOT=$TRUST_MEDCOUPLING_ROOT
+ORG=`pwd`
+# Something changed -> we delete all
+# Sometime TRUST_CC changes and produces recursive cmake so the last condition IS important
+Build=$TRUST_ROOT/build/Outils/lata2dx
+if [ build.sh -nt $Build ] || [ CMakeLists.txt -nt $Build ] || [ "`grep $TRUST_CC $Build/CMakeCache.txt`" = "" ]
+then
+   rm -r -f $Build
+fi
+mkdir -p $Build
+cd $Build
+# Better detection of SWIG on Ubuntu 16
+SWIG_EXECUTABLE=`type -p swig`
+# PL inutile de specifier swig (dans PATH normalement):
+cmake $ORG -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$TRUST_ROOT/exec/lata2dx_install -DCMAKE_C_COMPILER=$TRUST_cc -DCMAKE_CXX_COMPILER=$TRUST_CC -DSWIG_EXECUTABLE=$SWIG_EXECUTABLE
+make -j  $TRUST_NB_PROCS  install || exit -1
+
+echo "Check if compare_lata works"
+tar xzf $TRUST_ROOT/Outils/lata2dx/src/vdf.tar.gz
+$TRUST_ROOT/exec/lata2dx_install/bin/compare_lata vdf/vdf.lata vdf/vdf.lata || exit -1
+cd ..
+rm -rf $Build
