1aa2653c94738106580697bbeb5748e18e1083f1
Corentin Reiss <corentin.reiss@cea.fr>
2022-08-25 17:25:11 +0200

    [Multiphase] interfacial friction for a group of bubbles

--- src/ThHyd/Multiphase/Correlations/instancie_src_ThHyd_Multiphase_Correlations.cpp
+++ src/ThHyd/Multiphase/Correlations/instancie_src_ThHyd_Multiphase_Correlations.cpp
@@ -10,6 +10,7 @@
 #include <Diametre_bulles_constant.h>
 #include <Flux_interfacial_Coef_Constant.h>
 #include <Flux_interfacial_Ranz_Marshall.h>
+#include <Frottement_interfacial_Simonnet.h>
 #include <Frottement_interfacial_Sonnenburg.h>
 #include <Frottement_interfacial_Tomiyama.h>
 #include <Frottement_interfacial_Wallis.h>
@@ -29,15 +30,16 @@ Diametre_bulles_champ inst3;verifie_pere(inst3);
 Diametre_bulles_constant inst4;verifie_pere(inst4);
 Flux_interfacial_Coef_Constant inst5;verifie_pere(inst5);
 Flux_interfacial_Ranz_Marshall inst6;verifie_pere(inst6);
-Frottement_interfacial_Sonnenburg inst7;verifie_pere(inst7);
-Frottement_interfacial_Tomiyama inst8;verifie_pere(inst8);
-Frottement_interfacial_Wallis inst9;verifie_pere(inst9);
-Frottement_interfacial_Weber inst10;verifie_pere(inst10);
-Frottement_interfacial_bulles inst11;verifie_pere(inst11);
-Masse_ajoutee_Coef_Constant inst12;verifie_pere(inst12);
-Multiplicateur_diphasique_Friedel inst13;verifie_pere(inst13);
-Multiplicateur_diphasique_Lottes_Flinn inst14;verifie_pere(inst14);
-Multiplicateur_diphasique_Muhler_Steinhagen inst15;verifie_pere(inst15);
-Multiplicateur_diphasique_homogene inst16;verifie_pere(inst16);
-Portance_interfaciale_Tomiyama inst17;verifie_pere(inst17);
+Frottement_interfacial_Simonnet inst7;verifie_pere(inst7);
+Frottement_interfacial_Sonnenburg inst8;verifie_pere(inst8);
+Frottement_interfacial_Tomiyama inst9;verifie_pere(inst9);
+Frottement_interfacial_Wallis inst10;verifie_pere(inst10);
+Frottement_interfacial_Weber inst11;verifie_pere(inst11);
+Frottement_interfacial_bulles inst12;verifie_pere(inst12);
+Masse_ajoutee_Coef_Constant inst13;verifie_pere(inst13);
+Multiplicateur_diphasique_Friedel inst14;verifie_pere(inst14);
+Multiplicateur_diphasique_Lottes_Flinn inst15;verifie_pere(inst15);
+Multiplicateur_diphasique_Muhler_Steinhagen inst16;verifie_pere(inst16);
+Multiplicateur_diphasique_homogene inst17;verifie_pere(inst17);
+Portance_interfaciale_Tomiyama inst18;verifie_pere(inst18);
 }
