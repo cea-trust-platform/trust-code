e5da4fd45e669032587038363d543401c5f9c502
Pierre LEDAC <pierre.ledac@cea.fr>
2022-10-04 17:27:09 +0200

    [NVidia] Configuration d'orcus et altair (probleme undefined reference __pgi regle. Il faut charger le module NVidia AVANT le module gcc)

--- env_src/HOST_altair-nvidia.sh
+++ env_src/HOST_altair-nvidia.sh
@@ -9,7 +9,7 @@ define_modules_config()
    #
    echo "source /etc/profile" >> $env
    echo "module purge" >> $env
-   echo "module load gcc/6.3.0 nvhpc/22.1 || exit -1" >> $env
+   echo "module load nvhpc-nompi/22.1 gcc/9.3.0 || exit -1" >> $env
    . $env
 }
 
