bc375d17470bfdba7963cb59b9937aded60ec38c
Elie Saikali <elie.saikali@cea.fr>
2022-12-28 21:48:00 +0100

    [TRUST_STLList] Add a simple class that encapsulates an std list + iterators (hope to be useful ...)

--- src/Kernel/Utilitaires/TRUST_List.h
+++ src/Kernel/Utilitaires/TRUST_List.h
@@ -18,6 +18,7 @@
 
 #include <Motcle.h>
 #include <liste.h>
+#include <list> // pour stl list
 
 /*! @brief classe TRUST_List
  *
@@ -189,6 +190,145 @@ public:
 
   TRUST_List<_CLASSE_>& list() { return static_cast<TRUST_List<_CLASSE_>&>(liste_curseur::list()); }
   const TRUST_List<_CLASSE_>& list() const { return static_cast<const TRUST_List<_CLASSE_>&>(liste_curseur::list()); }
+};
+
+/* ======================================================= *
+ * ======================================================= *
+ * ======================================================= */
+
+template<typename _CLASSE_>
+class TRUST_STLList : public Objet_U
+{
+  using value_type = _CLASSE_;
+  using STLList = std::list<_CLASSE_>;
+
+protected:
+  unsigned taille_memoire() const override { throw; }
+
+  int duplique() const override
+  {
+    TRUST_STLList *xxx = new TRUST_STLList(*this);
+    if (!xxx) Process::exit("Not enough memory !");
+    return xxx->numero();
+  }
+
+  Sortie& printOn(Sortie& os) const override
+  {
+    Nom accouverte = "{", accfermee = "}", virgule = ",", blanc = " ";
+    if (size() == 0)
+      os << "vide" << finl;
+    else
+      {
+        os << accouverte << finl;
+        for (auto &itr : list_)
+          {
+            os << itr;
+            if (itr != list_.end()) os << blanc << virgule << finl;
+          }
+        os << finl << accfermee << finl;
+      }
+    return os;
+  }
+
+  Entree& readOn(Entree& is) override
+  {
+    Nom accouverte = "{", accfermee = "}", virgule = ",";
+    Motcle nom_;
+    is >> nom_;
+    if (nom_ == (const char*) "vide") return is;
+
+    if (nom_ != accouverte) Cerr << "Error while reading a list. One expected an opened bracket { to start." << finl, Process::exit();
+    _CLASSE_ t;
+
+    while (1)
+      {
+        is >> add(t);
+        is >> nom_;
+        if (nom_ == accfermee) return is;
+        if (nom_ != virgule) Cerr << nom_ << " one expected a ',' or a '}'" << finl, Process::exit();
+      }
+  }
+
+private:
+  STLList list_;
+
+public:
+  // Iterator class to enable iterating over the elements of TRUST_STLList
+  struct Iterator
+  {
+  private:
+    typename std::list<_CLASSE_>::iterator it;
+  public:
+    Iterator(typename std::list<_CLASSE_>::iterator i) : it(i) { }
+    int operator*() { return *it; }
+    Iterator& operator++() { ++it; return *this; }
+    bool operator!=(const Iterator& other) { return it != other.it; }
+  };
+
+  inline Iterator begin() { return Iterator(list_.begin()); }
+  inline Iterator end() { return Iterator(list_.end()); }
+  inline Iterator cbegin() const { return Iterator(list_.cbegin()); }
+  inline Iterator cend() const { return Iterator(list_.cend()); }
+  const std::list<_CLASSE_>& get_stl_list() const { return list_; }
+  std::list<_CLASSE_>& get_stl_list() { return list_; }
+
+  TRUST_STLList() : list_() { }
+  TRUST_STLList(const _CLASSE_& t) : list_(t) { }
+  TRUST_STLList(const TRUST_STLList& t) { list_ = t.list_; }
+
+  int size() const { return (int)list_.size(); }
+  void vide() { list_.clear(); }
+  _CLASSE_& add(const _CLASSE_& t) { list_.push_back(t); return (_CLASSE_&)t; }
+  const _CLASSE_& front() const { return list_.front(); }
+
+  _CLASSE_& operator[](int i) = delete;
+  const _CLASSE_& operator[](int i) const = delete;
+  _CLASSE_& operator[](const Nom& nom) = delete;
+  const _CLASSE_& operator[](const Nom& nom) const = delete;
+
+  _CLASSE_& operator()(int i) = delete;
+  const _CLASSE_& operator()(int i) const = delete;
+  _CLASSE_& operator()(const Nom& n) = delete;
+  const _CLASSE_& operator()(const Nom& n) const = delete;
+
+  TRUST_STLList& operator=(const _CLASSE_ &t)
+  {
+    list_ = t.list_;
+    return *this;
+  }
+
+  TRUST_STLList& operator=(const TRUST_STLList& t)
+  {
+    list_ = t.list_;
+    return *this;
+  }
+
+  int rang(const char* const ch) const
+  {
+    Nom nom(ch);
+    int ind = 0;
+    for (auto& itr : list_)
+      {
+        if (itr->le_nom() == nom) return ind;
+        ind++;
+      }
+    return -1;
+  }
+
+  int contient(const char* const ch) const
+  {
+    Nom nom(ch);
+    for (auto& itr : list_)
+      if (itr->le_nom() == nom) return 1;
+    return -1;
+  }
+
+  int contient(const Objet_U& obj) const
+  {
+    for (auto &itr : list_)
+      if (itr.valeur() == obj) return 1;
+    return 0;
+  }
 };
 
 #endif /* TRUST_List_included */
