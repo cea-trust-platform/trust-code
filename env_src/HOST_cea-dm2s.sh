6a9cc7fc6ead6cb947ea220a78c94fa4ab41d778
Pierre LEDAC <pierre.ledac@cea.fr>
2023-01-04 18:25:43 +0100

    [Env] Suppress TRUST_MPI_NATIF env (replaced by BATCH_SYSTEM_DETECTED)

--- env_src/HOST_cea-dm2s.sh
+++ env_src/HOST_cea-dm2s.sh
@@ -14,7 +14,6 @@ define_modules_config()
       echo "source /etc/profile" >> $env
       echo "module purge" >> $env
       #echo "module load gcc/6.5.0 mpich/gcc_6.5.0/3.2.1 || exit -1" >> $env
-      #echo "export TRUST_MPI_NATIF=1" >> $env
       echo "module load gcc/6.5.0 || exit -1" >> $env
    fi
    if [ $TRUST_USE_CUDA = 1 ]
@@ -23,7 +22,6 @@ define_modules_config()
       PATH_COMPILER=`source /etc/profile;module show nvhpc-nompi 2>&1 | awk '/compilers/ && ($2=="PATH") {print $3}'`
       # Mettre dans cet ordre, pour que nvcc soit detecte avant:
       echo "export PATH=\$PATH:$PATH_COMPILER" >> $env
-      echo "export TRUST_MPI_NATIF=0" >> $env
    fi
    . $env
 }
