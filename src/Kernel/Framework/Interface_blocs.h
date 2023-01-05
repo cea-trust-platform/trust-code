127fb0922bb56425ed9ae982da679e7a33f4b2b6
triou <triou@cea.fr>
2023-01-05 14:59:07 +0100

    Fix compilation issue with icc

--- src/Kernel/Framework/Interface_blocs.h
+++ src/Kernel/Framework/Interface_blocs.h
@@ -1,5 +1,5 @@
 /****************************************************************************
-* Copyright (c) 2022, CEA
+* Copyright (c) 2023, CEA
 * All rights reserved.
 *
 * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
@@ -16,7 +16,8 @@
 #ifndef Interface_blocs_included
 #define Interface_blocs_included
 
-#include <TRUSTTabs_forward.h>
+#include <TRUSTTab.h>
+
 #include <stdexcept>
 #include <map>
 
