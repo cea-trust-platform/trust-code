a824e9c5bd7e7260725b19ce6c638532adb1bbda
Pierre LEDAC <pierre.ledac@cea.fr>
2023-01-06 11:01:07 +0100

    Fix after rebase

---
+++ src/Kernel/Geometrie/Interpolation_IBM/instancie_src_Kernel_Geometrie_Interpolation_IBM.cpp
@@ -0,0 +1,23 @@
+//
+// Warning : DO NOT EDIT !
+// To update this file, run: make depend
+// TRUST_NO_INDENT
+//
+#include <verifie_pere.h>
+#include <Interpolation_IBM.h>
+#include <Interpolation_IBM_aucune.h>
+#include <Interpolation_IBM_elem_fluid.h>
+#include <Interpolation_IBM_hybrid.h>
+#include <Interpolation_IBM_mean_gradient.h>
+#include <Interpolation_IBM_power_law_tbl.h>
+#include <Interpolation_IBM_power_law_tbl_u_star.h>
+void instancie_src_Kernel_Geometrie_Interpolation_IBM() {
+Cerr << "src_Kernel_Geometrie_Interpolation_IBM" << finl;
+Interpolation_IBM inst1;verifie_pere(inst1);
+Interpolation_IBM_aucune inst2;verifie_pere(inst2);
+Interpolation_IBM_elem_fluid inst3;verifie_pere(inst3);
+Interpolation_IBM_hybrid inst4;verifie_pere(inst4);
+Interpolation_IBM_mean_gradient inst5;verifie_pere(inst5);
+Interpolation_IBM_power_law_tbl inst6;verifie_pere(inst6);
+Interpolation_IBM_power_law_tbl_u_star inst7;verifie_pere(inst7);
+}
