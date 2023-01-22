5878f89fc0a89c5ce7f0bf7a81c84d86f6d5015c
Elie Saikali <elie.saikali@cea.fr>
2023-01-22 19:57:33 +0100

    [Operateurs VDF] Pass turbulent viscosity vect to DoubleTab so that it can handle anisotrope/multiphase cases

--- src/VDF/Operateurs/Eval_Diff_Dift/Eval_Dift_VDF.h
+++ src/VDF/Operateurs/Eval_Diff_Dift/Eval_Dift_VDF.h
@@ -40,11 +40,11 @@ public:
   inline double compute_heq_impl(double d0, int i, double d1, int j, int compo) const
   {
     const double heq_lam = Eval_Diff_VDF::compute_heq_impl(d0, i, d1, j, compo);
-    const double heq_turb = 0.5 * (dv_diffusivite_turbulente(i) + dv_diffusivite_turbulente(j)) / (d1 + d0);
+    const double heq_turb = 0.5 * (tab_diffusivite_turbulente(i, is_multi_ * compo) + tab_diffusivite_turbulente(j, is_multi_ * compo)) / (d1 + d0);
     return heq_lam + heq_turb;
   }
 
-  inline double nu_t_impl(int i, int compo) const { return dv_diffusivite_turbulente(i); }
+  inline double nu_t_impl(int i, int compo) const { return tab_diffusivite_turbulente(i, is_multi_ * compo); }
 
   inline double nu_lam_impl_face(int i, int j, int k, int l, int compo) const
   {
@@ -59,7 +59,7 @@ public:
   inline double nu_1_impl(int i, int compo) const
   {
     const double nu_lam = Eval_Diff_VDF::nu_1_impl(i, compo);
-    const double nu_turb = dv_diffusivite_turbulente(i);
+    const double nu_turb = tab_diffusivite_turbulente(i, is_multi_ * compo);
     return nu_lam + nu_turb;
   }
 
@@ -70,12 +70,13 @@ public:
 
   inline double nu_1_impl_face(int i, int j, int compo) const
   {
-    return 0.5 * (dv_diffusivite_turbulente(i) + dv_diffusivite_turbulente(j));
+    return 0.5 * (tab_diffusivite_turbulente(i, is_multi_ * compo) + tab_diffusivite_turbulente(j, is_multi_ * compo));
   }
 
   inline double nu_2_impl_face(int i, int j, int k, int l, int compo) const
   {
-    return 0.25 * (dv_diffusivite_turbulente(i) + dv_diffusivite_turbulente(j) + dv_diffusivite_turbulente(k) + dv_diffusivite_turbulente(l));
+    return 0.25 * (tab_diffusivite_turbulente(i, is_multi_ * compo) + tab_diffusivite_turbulente(j, is_multi_ * compo) +
+                   tab_diffusivite_turbulente(k, is_multi_ * compo) + tab_diffusivite_turbulente(l, is_multi_ * compo));
   }
 
   void update_equivalent_distance()
@@ -93,17 +94,19 @@ public:
   inline void associer_diff_turb(const Champ_Fonc& diff_turb)
   {
     ref_diffusivite_turbulente_ = diff_turb;
-    dv_diffusivite_turbulente.ref(diff_turb.valeurs());
+    tab_diffusivite_turbulente.ref(diff_turb.valeurs());
+    is_multi_ = (diff_turb.valeurs().dimension(1) > 1) ? 1 : 0;
   }
 
   inline virtual void associer_loipar(const Turbulence_paroi_scal& loi_paroi) { loipar = loi_paroi; }
   inline virtual void init_ind_fluctu_term() { /* do nothing */}
 
 protected:
+  int is_multi_ = 0;
   REF(Champ_Fonc) ref_diffusivite_turbulente_;
   REF(Turbulence_paroi_scal) loipar;
   DoubleVects equivalent_distance;
-  DoubleVect dv_diffusivite_turbulente;
+  DoubleTab tab_diffusivite_turbulente;
 };
 
 #endif /* Eval_Dift_VDF_included */
