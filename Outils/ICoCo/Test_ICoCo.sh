508fedfcac1a0fe4278bc5538c4cb14093b16190
Elie Saikali <elie.saikali@cea.fr>
2023-03-06 15:07:14 +0100

    [ICoCo] Build ICoCo baltik when compiling TRUST (not only module ...)

--- Outils/ICoCo/Test_ICoCo.sh
+++ Outils/ICoCo/Test_ICoCo.sh
@@ -3,8 +3,9 @@ cd ICoCo_src
 ./configure
 echo "Compiling ICoCo test"
 debug=""
-[ -f $exec_debug ] && debug="module_debug"
-make module_optim $debug
+[ -f $exec_debug ] && debug="debug module_debug"
+#[ -f $exec_semi_opt ] && semi_opt="semi_optim module_semi_optim"
+make optim module_optim $debug #$semi_opt
 status=$?
 
 exit $status
