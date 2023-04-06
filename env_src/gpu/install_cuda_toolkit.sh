70692a270fd4222f7b6d164497619f4aa03a1d9b
Pierre LEDAC <pierre.ledac@cea.fr>
2023-04-06 18:01:53 +0200

    [GPU] Download if needed under /export/home/catA/pl254994/.tmp_TRUST_trust, install under /export/home/catA/pl254994/trust/amgx_openmp/env/gpu/install

--- env_src/gpu/install_cuda_toolkit.sh
+++ env_src/gpu/install_cuda_toolkit.sh
@@ -2,24 +2,25 @@
 # CUDA TOOLKIT
 CUDA_VERSION=11.8.0 && installer=cuda_$CUDA_VERSION"_520.61.05_linux.run" # Ne converge pas (c-amg dans PAR_gpu_3D) semble se produire des 11.3,11.6,11.7, par contre sa-amg fonctionne sauf si Cuda plus recent que driver CUDENSE handle...
 CUDA_VERSION=11.2.0 && installer=cuda_$CUDA_VERSION"_460.27.04_linux.run" 
-[ "$TRUST_TMP" = "" ] && TRUST_TMP="."
-CUDA_BIN=$TRUST_TMP/cuda-$CUDA_VERSION/bin
+INSTALL=$TRUST_ROOT/env/gpu/install
+CUDA_BIN=$INSTALL/cuda-$CUDA_VERSION/bin
 if [ ! -f $CUDA_BIN/nvcc ]
 then
    echo "It may take some minutes to download Cuda Toolkit $CUDA_VERSION ..."
    (
-      cd $TRUST_TMP
-      if [ ! -f $installer ]
+      if [ ! -f $TRUST_TMP/$installer ]
       then
-          wget https://developer.download.nvidia.com/compute/cuda/$CUDA_VERSION/local_installers/$installer 1>/dev/null 2>&1 
-          [ $? != 0 ] && echo "Error when downloading. See https://developer.nvidia.com/cuda-downloads" && exit -1
+         cd $TRUST_TMP
+         wget https://developer.download.nvidia.com/compute/cuda/$CUDA_VERSION/local_installers/$installer 1>/dev/null 2>&1 
+         [ $? != 0 ] && echo "Error when downloading. See https://developer.nvidia.com/cuda-downloads" && exit -1
       fi
+      mkdir -p $INSTALL && cd $INSTALL
       echo "It may take some minutes to install Cuda Toolkit..."
       # --override pour ne pas verifier la version de gcc par rapport a Cuda (risque...)
       # ToDo supprimer la fenetre lors d'une remote install par ssh... Done: --nox11
-      bash ./$installer --override --silent --no-drm --no-man-page --toolkit --nox11 --installpath=`dirname $CUDA_BIN`
+      bash $TRUST_TMP/$installer --override --silent --no-drm --no-man-page --toolkit --nox11 --installpath=`dirname $CUDA_BIN`
       [ $? != 0 ] && echo "Error during Cuda install under `dirname $CUDA_BIN`" && exit -1
-      rm -f $installer
+      #rm -f $installer
       # Bizarre petra:
       chmod gou+x $CUDA_BIN/*
    )
--- env_src/gpu/install_nvhpc_sdk_toolkit.sh
+++ env_src/gpu/install_nvhpc_sdk_toolkit.sh
@@ -3,22 +3,23 @@
 SDK_VERSION=22.9 && CUDA_VERSION=11.7 && installer=nvhpc_2022_229_Linux_x86_64_cuda_$CUDA_VERSION  # 11.7 uniquement
 SDK_VERSION=22.2 && CUDA_VERSION=11.6 && installer=nvhpc_2022_222_Linux_x86_64_cuda_multi # 11.6, 11.2, 11.0, 10.2 
 SDK_VERSION=22.1 && CUDA_VERSION=11.5 && installer=nvhpc_2022_221_Linux_x86_64_cuda_multi # 11.5, 11.0, 10.2 (nvcc 11.5 OK pour gcc 11.0 de Fedora 34)
-[ "$TRUST_TMP" = "" ] && TRUST_TMP="."
-NVHPC=$TRUST_TMP/nvhpc-$SDK_VERSION/Linux_x86_64/$SDK_VERSION/compilers
+INSTALL=$TRUST_ROOT/env/gpu/install
+NVHPC=$INSTALL/nvhpc-$SDK_VERSION/Linux_x86_64/$SDK_VERSION/compilers
 if [ ! -f $NVHPC/bin/nvc++ ]
 then
    echo "It may take some minutes to download NVidia HPC SDK $SDK_VERSION..."
    (
-   cd $TRUST_TMP
-   if [ ! -f $installer.tar.gz ]
+   if [ ! -f $TRUST_TMP/$installer.tar.gz ]
    then
+      cd $TRUST_TMP
       wget https://developer.download.nvidia.com/hpc-sdk/$SDK_VERSION/$installer.tar.gz 1>/dev/null 2>&1 
       [ $? != 0 ] && echo "Error when downloading. See https://developer.nvidia.com/nvidia-hpc-sdk-releases" && exit -1
    fi
+   mkdir -p $INSTALL && cd $INSTALL
    echo "It may take some minutes to install NVidia HPC SDK..."
-   tar xpzf $installer.tar.gz && rm -f $installer.tar.gz
-   echo -e "\n1\n$TRUST_TMP/nvhpc-$SDK_VERSION\n" | $installer/install
+   tar xpzf $TRUST_TMP/$installer.tar.gz && rm -f $installer.tar.gz
+   echo -e "\n1\n$INSTALL/nvhpc-$SDK_VERSION\n" | $installer/install
    [ $? != 0 ] && echo "Error during NVidia HPC SDK install under $NVHPC" && exit -1
-   rm -r -f $installer
+   #rm -r -f $installer
    )
 fi
