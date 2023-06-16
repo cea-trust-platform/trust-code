8dcabdd42cff5c25e148c4e2b2f5e52e6d29f56b
Corentin Reiss <corentin.reiss@cea.fr>
2023-06-16 10:50:14 +0200

    [Turbulence] allow zero-flux neumann BC to be used in turbulent equaitons

--- src/ThHyd/Incompressible/Cond_Lim/Neumann_paroi_adiabatique.cpp
+++ src/ThHyd/Incompressible/Cond_Lim/Neumann_paroi_adiabatique.cpp
@@ -1,5 +1,5 @@
 /****************************************************************************
-* Copyright (c) 2022, CEA
+* Copyright (c) 2023, CEA
 * All rights reserved.
 *
 * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
@@ -22,7 +22,7 @@ Sortie& Neumann_paroi_adiabatique::printOn(Sortie& s) const { return s << que_su
 
 Entree& Neumann_paroi_adiabatique::readOn(Entree& s)
 {
-  if (app_domains.size() == 0) app_domains = { Motcle("Diphasique_moyenne"), Motcle("Thermique"), Motcle("Thermique_H"), Motcle("indetermine") };
+  if (app_domains.size() == 0) app_domains = { Motcle("Diphasique_moyenne"), Motcle("Thermique"), Motcle("Thermique_H"), Motcle("indetermine"), Motcle("Turbulence") };
 
   return Neumann_homogene::readOn(s);
 }
