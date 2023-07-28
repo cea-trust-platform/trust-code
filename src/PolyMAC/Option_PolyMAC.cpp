0baf4e715442f9759f2d95805eef8939fbd4a294
Yannick GORSSE <yannick.gorsse@cea.fr>
2023-07-28 15:41:18 +0200

    [PolyMAC] option_PolyMAC : new m2 is default, 'use_osqp' to use the old version

--- src/PolyMAC/Option_PolyMAC.cpp
+++ src/PolyMAC/Option_PolyMAC.cpp
@@ -22,7 +22,7 @@ Implemente_instanciable(Option_PolyMAC,"Option_PolyMAC",Interprete);
 // XD Option_PolyMAC interprete Option_PolyMAC 1 Class of PolyMAC options.
 // XD attr use_osqp rien use_new_m2 1 Flag to sse the new formulation of the M2 matrix
 
-int Option_PolyMAC::USE_NEW_M2 = 0;
+int Option_PolyMAC::USE_NEW_M2 = 1;
 
 Sortie& Option_PolyMAC::printOn(Sortie& os) const { return Interprete::printOn(os); }
 
@@ -31,14 +31,14 @@ Entree& Option_PolyMAC::readOn(Entree& is) { return Interprete::readOn(is); }
 Entree& Option_PolyMAC::interpreter(Entree& is)
 {
   Param param(que_suis_je());
-  param.ajouter_non_std("use_new_m2",(this)); // chaine Use the new formulation of the M2 matrix
+  param.ajouter_non_std("use_osqp",(this)); // chaine Use the new formulation of the M2 matrix
   param.lire_avec_accolades_depuis(is);
   return is;
 }
 
 int Option_PolyMAC::lire_motcle_non_standard(const Motcle& mot, Entree& is)
 {
-  if (mot=="use_new_m2") USE_NEW_M2 = 1;
+  if (mot=="use_osqp") USE_NEW_M2 = 0;
   else return -1;
   return 1;
 }
