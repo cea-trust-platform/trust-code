f06a5e3b2c6ac34f4e207f8f4330a711c334cf65
Elie Saikali <elie.saikali@cea.fr>
2023-07-28 11:22:44 +0200

    [VEF Templates] Simplify/factorize Op_Dift_Stab_VEF_Face

--- src/VEF/Operateurs/Op_Diff_Dift/Op_Dift_Stab_VEF_Face.h
+++ src/VEF/Operateurs/Op_Diff_Dift/Op_Dift_Stab_VEF_Face.h
@@ -16,88 +16,78 @@
 #ifndef Op_Dift_Stab_VEF_Face_included
 #define Op_Dift_Stab_VEF_Face_included
 
+#include <Op_Dift_VEF_Face.h>
 #include <Operateur_Div.h>
-#include <Op_Dift_VEF_Face_old.h>
 
-/*! @brief class Op_Dift_Stab_VEF_Face
- *
- */
-
-class Op_Dift_Stab_VEF_Face : public Op_Dift_VEF_Face_old
+class Op_Dift_Stab_VEF_Face: public Op_Dift_VEF_Face
 {
   Declare_instanciable(Op_Dift_Stab_VEF_Face);
 
 public:
 
-  DoubleTab& ajouter(const DoubleTab& ,  DoubleTab& ) const override;
+  DoubleTab& ajouter(const DoubleTab&, DoubleTab&) const override;
 
-  void ajouter_cas_scalaire(const DoubleTab& inconnue,const DoubleTab& nu,const DoubleTab& nu_turb_m, DoubleTab& resu2) const;
-  void ajouter_cas_multiscalaire(const DoubleTab& inconnue,const DoubleTab& nu,const DoubleTab& nu_turb_m, DoubleTab& resu2) const;
-  void ajouter_cas_vectoriel(const DoubleTab& inconnue,const DoubleTab& nu,const DoubleTab& nu_turb_m, DoubleTab& resu2) const;
+  void ajouter_cas_scalaire     (const DoubleTab& inconnue, const DoubleTab& nu, const DoubleTab& nu_turb_m, DoubleTab& resu2) const;
+  void ajouter_cas_vectoriel    (const DoubleTab& inconnue, const DoubleTab& nu, const DoubleTab& nu_turb_m, DoubleTab& resu2) const;
 
-  void ajouter_contribution(const DoubleTab& transporte, Matrice_Morse& matrice) const;
+  void ajouter_contribution               (const DoubleTab& transporte, Matrice_Morse& matrice) const;
   void ajouter_contribution_multi_scalaire(const DoubleTab& transporte, Matrice_Morse& matrice) const;
-  void ajouter_contribution_cl(const DoubleTab& transporte, Matrice_Morse& matrice, const DoubleTab& nu, const DoubleTab& nu_turb, const DoubleVect& porosite_eventuelle) const;
+
+  void ajouter_contribution_cl               (const DoubleTab& transporte, Matrice_Morse& matrice, const DoubleTab& nu, const DoubleTab& nu_turb, const DoubleVect& porosite_eventuelle) const;
+  void ajouter_contribution_multi_scalaire_cl(const DoubleTab& transporte, Matrice_Morse& matrice, const DoubleTab& nu, const DoubleTab& nu_turb, const DoubleVect& porosite_eventuelle) const;
+
   void contribuer_a_avec(const DoubleTab&, Matrice_Morse&) const override;
 
   void completer() override;
 
-protected :
+protected:
 
   void calculer_coefficients(const DoubleTab&, DoubleTab&) const;
   void calculer_coefficients_vectoriel_diag(const DoubleTab& nu, const DoubleTab& nu2, DoubleTab& Aij) const;
-  /*   void calculer_coefficients_vectoriel_extradiag(const DoubleTab& nu, DoubleTab& Aij) const ; */
   inline double aij_extradiag(const int elem, const int facei, const int facej, const int dim, const int dim2, const double nu_elem) const;
 
-  void calculer_min(const DoubleTab&, int&, DoubleTab&) const ;
-  void calculer_max(const DoubleTab&, int&, DoubleTab&) const ;
-  double calculer_gradients(int, const DoubleTab&) const ;
+  void calculer_min(const DoubleTab&, int&, DoubleTab&) const;
+  void calculer_max(const DoubleTab&, int&, DoubleTab&) const;
+  double calculer_gradients(int, const DoubleTab&) const;
 
   void modifie_pour_Cl(const DoubleTab&, DoubleTab&) const;
-  void ajouter_operateur_centre(const DoubleTab&, const DoubleTab&, DoubleTab&) const ;
+  void ajouter_operateur_centre(const DoubleTab&, const DoubleTab&, DoubleTab&) const;
   void ajouter_operateur_centre_vectoriel(const DoubleTab& Aij_diag, const DoubleTab& nu, const DoubleTab& inconnue, DoubleTab& resu2) const;
-  void ajouter_diffusion(const DoubleTab&, const DoubleTab&, DoubleTab&) const ;
+  void ajouter_diffusion(const DoubleTab&, const DoubleTab&, DoubleTab&) const;
   void ajouter_diffusion_vectoriel(const DoubleTab& Aij, const DoubleTab& inconnueTab, DoubleTab& resuTab) const;
-  void ajouter_antidiffusion(const DoubleTab&, const DoubleTab&, DoubleTab&) const ;
+  void ajouter_antidiffusion(const DoubleTab&, const DoubleTab&, DoubleTab&) const;
   void ajouter_antidiffusion_vectoriel(const DoubleTab& Aij, const DoubleTab& inconnueTab, DoubleTab& resuTab) const;
 
   IntTab is_dirichlet_faces_;
   REF(Champ_Inc) divergence_U;
 
-  int nu_lu_ = 1, nut_lu_ = 1;
-  int nu_transp_lu_ = 0, nut_transp_lu_ = 1;
-  int new_jacobian_ = 0;
-  int standard_ = 0;
+  int nu_lu_ = 1, nut_lu_ = 1, nu_transp_lu_ = 0, nut_transp_lu_ = 1;
+  int new_jacobian_ = 0, standard_ = 0;
 };
 
 inline double Op_Dift_Stab_VEF_Face::aij_extradiag(const int elem, const int facei, const int facej, const int dim, const int dim2, const double nu_elem) const
 {
   const Domaine_VEF& domaine_VEF = le_dom_vef.valeur();
+  const IntTab& face_voisins = domaine_VEF.face_voisins();
+  const DoubleTab& face_normales = domaine_VEF.face_normales();
+  const DoubleVect& volumes = domaine_VEF.volumes();
 
-  const IntTab& face_voisins=domaine_VEF.face_voisins();
-
-  const DoubleTab& face_normales=domaine_VEF.face_normales();
-  const DoubleVect& volumes=domaine_VEF.volumes();
-
-  double volume=0.;
-  double signei=1.;
-  double signej=1.;
-  double aij=0.;
+  double volume = 0., signei = 1., signej = 1., aij = 0.;
 
   //Debut de l'algo
-  assert(dim<equation().inconnue().valeurs().dimension(1));
-  assert(dim2<equation().inconnue().valeurs().dimension(1));
-  assert(dim<dim2);
+  assert(dim < equation().inconnue().valeurs().dimension(1));
+  assert(dim2 < equation().inconnue().valeurs().dimension(1));
+  assert(dim < dim2);
 
-  volume=1./volumes(elem);
-  volume*=nu_elem;
+  volume = 1. / volumes(elem);
+  volume *= nu_elem;
 
-  if (face_voisins(facei,0)!=elem) signei=-1.;
-  if (face_voisins(facej,0)!=elem) signej=-1.;
+  if (face_voisins(facei, 0) != elem) signei = -1.;
+  if (face_voisins(facej, 0) != elem) signej = -1.;
 
-  aij=face_normales(facei,dim2)*face_normales(facej,dim);
-  aij*=signei*signej;
-  aij*=volume;
+  aij = face_normales(facei, dim2) * face_normales(facej, dim);
+  aij *= signei * signej;
+  aij *= volume;
 
   return aij;
 }
@@ -105,13 +95,13 @@ inline double Op_Dift_Stab_VEF_Face::aij_extradiag(const int elem, const int fac
 inline void Op_Dift_Stab_VEF_Face::contribuer_a_avec(const DoubleTab& inco, Matrice_Morse& matrice) const
 {
   if (!new_jacobian_)
-    Op_Dift_VEF_Face_old::contribuer_a_avec(inco,matrice);
+    Op_Dift_VEF_Face::contribuer_a_avec(inco, matrice);
   else
     {
       const Champ_base& inconnue = equation().inconnue().valeur();
       const Nature_du_champ nature_champ = inconnue.nature_du_champ();
 
-      if (nature_champ!=multi_scalaire)
+      if (nature_champ != multi_scalaire)
         ajouter_contribution(inco, matrice);
       else
         ajouter_contribution_multi_scalaire(inco, matrice);
@@ -119,4 +109,3 @@ inline void Op_Dift_Stab_VEF_Face::contribuer_a_avec(const DoubleTab& inco, Matr
 }
 
 #endif
-
