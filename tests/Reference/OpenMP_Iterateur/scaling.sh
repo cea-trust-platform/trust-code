dab2f5fc61d45b51ce5197b1fe705c1ba1f29f56
Pierre LEDAC <pierre.ledac@cea.fr>
2023-08-15 18:12:00 +0200

    [Bench] Update scripts

--- tests/Reference/OpenMP_Iterateur/scaling.sh
+++ tests/Reference/OpenMP_Iterateur/scaling.sh
@@ -2,7 +2,7 @@
 # Scaling a mesh on several GPU
 mkdir -p scaling && cd scaling
 echo "Config     Ax=B B"
-for version in gpu # cpu
+for version in gpu cpu
 do
    gpus="1"
    [ $HOST = adastra ] && mpis=192 && gpus="1 2 3 4 5 6 7 8"   && sizes="15x15x4 20x20x5 24x24x7 31x31x10 41x41x14 51x51x17 61x61x20 74x74x25 92x92x32"
@@ -22,8 +22,8 @@ do
             run=1 && [ -f $jdd.TU ] && run=0
             [ $run = 1 ] && echo "$jdd ..."
             # Creation data
-            [ $version = gpu ] && [ $TRUST_USE_ROCM = 1 ] && cp ../../OpenMP_Iterateur_BENCH_rocALUTION.data $jdd.data  && exec=$local/trust/amgx_openmp/exec/TRUST_mpi_opt
-            [ $version = gpu ] && [ $TRUST_USE_CUDA = 1 ] && cp ../../OpenMP_Iterateur_BENCH_AmgX.data $jdd.data  && exec=$local/trust/amgx_openmp/exec/TRUST_mpi_opt
+            [ $version = gpu ] && [ "$TRUST_USE_ROCM" = 1 ] && cp ../../OpenMP_Iterateur_BENCH_rocALUTION.data $jdd.data  && exec=$local/trust/amgx_openmp/exec/TRUST_mpi_opt
+            [ $version = gpu ] && [ "$TRUST_USE_CUDA" = 1 ] && cp ../../OpenMP_Iterateur_BENCH_AmgX.data $jdd.data  && exec=$local/trust/amgx_openmp/exec/TRUST_mpi_opt
             [ $version = cpu ] && cp ../../OpenMP_Iterateur_BENCH_PETSc.data $jdd.data && exec=$local/trust/tma/exec/TRUST_mpi_opt
             size=`echo $size | awk '{gsub("x"," ",$0);print $0}'`
             sed -i "1,$ s?31 31 10?$size?" $jdd.data
@@ -34,7 +34,7 @@ do
             # Analyse
             awk '/RAM taken/ {if ($1>RAM) RAM=$1} END {print "RAM= "RAM}' $jdd.out_err >> $jdd.TU
             dof=`awk '/Total number of elements/ {print $NF;exit}' $jdd.out_err`
-	    awk -v mpi=$mpi -v gpu=$gpu -v dof=$dof '/Secondes/ && /pas de temps/ {dt=$NF} /Dont solveurs/ {s=$4;b=dt-s;gsub("\\.",",",s);gsub("\\.",",",b)} END {print mpi"MPI"(gpu==0?"":"+"gpu"GPU")" "dof" "s" "b}' $jdd.TU
+	    awk -v mpi=$mpi -v gpu=$gpu -v dof=$dof '/Secondes/ && /pas de temps/ {dt=$NF} /Dont solveurs/ {s=$4;b=dt-s} END {print mpi"MPI"(gpu==0?"":"+"gpu"GPU")" "dof" "s" "b}' $jdd.TU
             cd - 1>/dev/null 2>&1
 	 done   
       done
