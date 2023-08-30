1164d9e7a1fd4064bbeb6239550d492725096fe0
Elie Saikali <elie.saikali@cea.fr>
2023-08-30 09:44:04 +0200

    [Turbulence] Add method for turbulent dilatable problems

--- src/ThHyd/Dilatable/Common/Modifier_pour_fluide_dilatable.cpp
+++ src/ThHyd/Dilatable/Common/Modifier_pour_fluide_dilatable.cpp
@@ -1,5 +1,5 @@
 /****************************************************************************
-* Copyright (c) 2022, CEA
+* Copyright (c) 2023, CEA
 * All rights reserved.
 *
 * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
@@ -15,21 +15,26 @@
 
 #include <Modifier_pour_fluide_dilatable.h>
 #include <Fluide_Dilatable_base.h>
-#include <TRUSTVect.h>
+#include <Mod_turb_hyd_base.h>
+#include <Equation_base.h>
+#include <Probleme_base.h>
 #include <Domaine_VF.h>
+#include <TRUSTVect.h>
 
 static void multiplier_ou_diviser(DoubleVect& x, const DoubleVect& y, int diviser)
 {
-  if (!diviser) tab_multiply_any_shape(x, y ,VECT_REAL_ITEMS);
-  else tab_divide_any_shape(x, y ,VECT_REAL_ITEMS);
+  if (!diviser)
+    tab_multiply_any_shape(x, y, VECT_REAL_ITEMS);
+  else
+    tab_divide_any_shape(x, y, VECT_REAL_ITEMS);
   x.echange_espace_virtuel();
   //Debog::verifier("multiplier_ou_diviser x/y apres x:",x);
 }
 
 // Modif B.M: on ne remplit que la partie reelle du tableau.
-void multiplier_diviser_rho(DoubleVect& tab,const Fluide_Dilatable_base& le_fluide,int diviser)
+void multiplier_diviser_rho(DoubleVect& tab, const Fluide_Dilatable_base& le_fluide, int diviser)
 {
-  const Domaine_VF& zvf = ref_cast(Domaine_VF,le_fluide.vitesse().valeur().domaine_dis_base());
+  const Domaine_VF& zvf = ref_cast(Domaine_VF, le_fluide.vitesse().valeur().domaine_dis_base());
   // Descripteurs des tableaux aux elements et aux faces:
   const Domaine& domaine = zvf.domaine();
   const MD_Vector& md_elem = domaine.les_elems().get_md_vector();
@@ -42,23 +47,23 @@ void multiplier_diviser_rho(DoubleVect& tab,const Fluide_Dilatable_base& le_flui
       DoubleVect rho_bord;
       // B.M. je cree une copie sinon il faut truander les tests sur les tailles dans multiply_any_shape
       // ou creer un DoubleTab qui pointe sur rho...
-      zvf.creer_tableau_faces_bord(rho_bord,Array_base::NOCOPY_NOINIT);
-      rho_bord.inject_array(rho,rho_bord.size());
-      multiplier_ou_diviser(tab,rho_bord,diviser);
+      zvf.creer_tableau_faces_bord(rho_bord, Array_base::NOCOPY_NOINIT);
+      rho_bord.inject_array(rho, rho_bord.size());
+      multiplier_ou_diviser(tab, rho_bord, diviser);
       return;
     }
 
   if (tab.get_md_vector() == md_faces)
     {
       const DoubleTab& rho_faces = ref_cast(Fluide_Dilatable_base,le_fluide).rho_discvit();
-      multiplier_ou_diviser(tab,rho_faces,diviser);
+      multiplier_ou_diviser(tab, rho_faces, diviser);
       return;
     }
 
   const DoubleTab& rho = le_fluide.masse_volumique().valeurs();
   if (tab.get_md_vector() == md_elem && rho.get_md_vector() == md_elem)
     {
-      multiplier_ou_diviser(tab,rho,diviser);
+      multiplier_ou_diviser(tab, rho, diviser);
       return;
     }
 
@@ -75,14 +80,14 @@ void multiplier_diviser_rho(DoubleVect& tab,const Fluide_Dilatable_base& le_flui
       for (int elem = 0; elem < nb_elem_tot; elem++)
         {
           double x = 0.;
-          for (int face=0; face < nfe; face++)
+          for (int face = 0; face < nfe; face++)
             {
               int f = elem_faces(elem, face);
               x += tab_rho[f];
             }
           rho_elem[elem] = x * facteur;
         }
-      multiplier_ou_diviser(tab,rho_elem,diviser);
+      multiplier_ou_diviser(tab, rho_elem, diviser);
       return;
     }
 
@@ -96,9 +101,10 @@ void multiplier_diviser_rho(DoubleVect& tab,const Fluide_Dilatable_base& le_flui
  *   et peut etre de type DoubleTab avec des dimension(i>0) quelconques (plusieurs composantes)
  *
  */
-void multiplier_par_rho_si_dilatable(DoubleVect& val,const Milieu_base& mil)
+void multiplier_par_rho_si_dilatable(DoubleVect& val, const Milieu_base& mil)
 {
-  if (sub_type(Fluide_Dilatable_base,mil)) multiplier_diviser_rho(val,ref_cast(Fluide_Dilatable_base,mil), 0 /* multiplier */);
+  if (sub_type(Fluide_Dilatable_base, mil))
+    multiplier_diviser_rho(val, ref_cast(Fluide_Dilatable_base, mil), 0 /* multiplier */);
   else
     {
       Cerr << "What ?? The method multiplier_par_rho_si_dilatable should not be called since your fluid is not dilatable !!" << finl;
@@ -109,12 +115,37 @@ void multiplier_par_rho_si_dilatable(DoubleVect& val,const Milieu_base& mil)
 /*! @brief Idem que multiplier_par_rho_si_dilatable mais on divise par rho.
  *
  */
-void diviser_par_rho_si_dilatable(DoubleVect& val,const Milieu_base& mil)
+void diviser_par_rho_si_dilatable(DoubleVect& val, const Milieu_base& mil)
 {
-  if (sub_type(Fluide_Dilatable_base,mil)) multiplier_diviser_rho(val,ref_cast(Fluide_Dilatable_base,mil), 1 /* diviser */);
+  if (sub_type(Fluide_Dilatable_base, mil))
+    multiplier_diviser_rho(val, ref_cast(Fluide_Dilatable_base, mil), 1 /* diviser */);
   else
     {
       Cerr << "What ?? The method diviser_par_rho_si_dilatable should not be called since your fluid is not dilatable !!" << finl;
+      Process::exit();
+    }
+}
+
+void correction_nut_et_cisaillement_paroi_si_qc(Mod_turb_hyd_base& mod)
+{
+  // on recgarde si on a un fluide QC
+  if (sub_type(Fluide_Dilatable_base, mod.equation().probleme().milieu()))
+    {
+      const Fluide_Dilatable_base& le_fluide = ref_cast(Fluide_Dilatable_base, mod.equation().probleme().milieu());
+      // 1 on multiplie nu_t par rho
+
+      DoubleTab& nut = ref_cast_non_const(DoubleTab, mod.viscosite_turbulente().valeurs());
+      multiplier_diviser_rho(nut, le_fluide, 0 /* multiplier */);
+
+      // 2  On modifie le ciasaillement paroi
+      const DoubleTab& cisaillement_paroi = mod.loi_paroi().valeur().Cisaillement_paroi();
+      DoubleTab& cisaillement = ref_cast_non_const(DoubleTab, cisaillement_paroi);
+      multiplier_diviser_rho(cisaillement, le_fluide, 0 /* multiplier */);
+      cisaillement.echange_espace_virtuel();
+    }
+  else
+    {
+      Cerr << "What ?? The method Correction_nut_et_cisaillement_paroi_si_qc should not be called since your fluid is not dilatable !!" << finl;
       Process::exit();
     }
 }
--- src/ThHyd/Dilatable/Common/Modifier_pour_fluide_dilatable.h
+++ src/ThHyd/Dilatable/Common/Modifier_pour_fluide_dilatable.h
@@ -1,5 +1,5 @@
 /****************************************************************************
-* Copyright (c) 2022, CEA
+* Copyright (c) 2023, CEA
 * All rights reserved.
 *
 * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
@@ -18,19 +18,25 @@
 
 /*! @brief Fonctions destinees a multiplier ou diviser un tableau de valeurs par un vecteur.
  *
- * Ces fonctions sont utilisees ici pour multiplier ou diviser un tableau par rho
- *      pour le cas ou le milieu est un Fluide_Dilatable_base
+ * Ces fonctions sont utilisees ici pour multiplier ou diviser un tableau par rho pour le cas ou le milieu est un Fluide_Dilatable_base
  *
- *
+ * correction_nut_et_cisaillement_paroi_si_qc permet de faire la conversion de la viscosite cinematique turbulente nu_t en
+ *    viscosite dynamique turbulente mu_t dans le cas des equations compressibles.
+ *      - Cette conversion ne doit etre faite que dans le cas ou l'on utilise un modele sous-maille type LES puisque dans ce cas on renvoit un nu_t.
+ *      - Par contre en simulation RANS, puisque l'on rentre les bonnes grandeurs turbulentes multipliee par rho, on obtient bien au final un mu_t et
+ *         l'on n'a pas besoin de faire de conversion.
  *
  */
 
 #include <TRUSTTabs_forward.h>
+
 class Fluide_Dilatable_base;
+class Mod_turb_hyd_base;
 class Milieu_base;
 
-void multiplier_diviser_rho(DoubleVect& tab,const Fluide_Dilatable_base& le_fluide,int diviser=0);
-void diviser_par_rho_si_dilatable(DoubleVect& val,const Milieu_base& mil);
-void multiplier_par_rho_si_dilatable(DoubleVect& val,const Milieu_base& mil);
+void multiplier_diviser_rho(DoubleVect& tab, const Fluide_Dilatable_base& le_fluide, int diviser = 0);
+void diviser_par_rho_si_dilatable(DoubleVect& val, const Milieu_base& mil);
+void multiplier_par_rho_si_dilatable(DoubleVect& val, const Milieu_base& mil);
+void correction_nut_et_cisaillement_paroi_si_qc(Mod_turb_hyd_base& mod);
 
 #endif /* Modifier_pour_fluide_dilatable */
