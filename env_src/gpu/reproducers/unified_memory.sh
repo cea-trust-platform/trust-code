ef4cee0dc2f03cba110d092e5b4f7a12422c7370
Pierre LEDAC <pierre.ledac@cea.fr>
2023-08-31 13:54:22 +0200

    [GPU] Update reproducers

--- env_src/gpu/reproducers/unified_memory.sh
+++ env_src/gpu/reproducers/unified_memory.sh
@@ -1,3 +1,4 @@
+# Peut etre utiliser -gpu=managed ?
 $TRUST_CC_BASE -O3 -mp -target=gpu -I$CUDA_ROOT/include -L$CUDA_ROOT/lib64 -lcudart -o unified_memory unified_memory.cpp || exit -1
 echo "Build OK"
 rm -f *nsys-rep
