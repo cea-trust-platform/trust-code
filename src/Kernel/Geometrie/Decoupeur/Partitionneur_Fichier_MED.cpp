a8962f09a473e85b95f0aee04923b31b249fca95
Luc Lecointre <luc.lecointre@cea.fr>
2023-09-04 15:32:13 +0200

    [Partionneur_med_file] correction Partitionneur_fichier_med

--- src/Kernel/Geometrie/Decoupeur/Partitionneur_Fichier_MED.cpp
+++ src/Kernel/Geometrie/Decoupeur/Partitionneur_Fichier_MED.cpp
@@ -1,5 +1,5 @@
 /****************************************************************************
-* Copyright (c) 2022, CEA
+* Copyright (c) 2023, CEA
 * All rights reserved.
 *
 * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
@@ -102,7 +102,7 @@ void Partitionneur_Fichier_MED::construire_partition(IntVect& elem_part, int& nb
 #ifdef MEDCOUPLING_
   using namespace MEDCoupling;
 
-  MCAuto<MEDCouplingField> ffield = MEDCoupling::ReadField("Partition.med", "Thread");
+  MCAuto<MEDCouplingField> ffield = MEDCoupling::ReadField(filename_.getString(), fieldname_.getString());
   MEDCouplingFieldInt32 * field = dynamic_cast<MEDCouplingFieldInt32 *>((MEDCouplingField *)ffield);
 
   if(!field)
