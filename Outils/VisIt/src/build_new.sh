2cf1d14f337cc356a49cdf60fb44855c70fab306
triou <trust@cea.fr>
2023-09-22 16:34:56 +0200

    [VisIt] install python, cmake and qt with VisIt

--- Outils/VisIt/src/build_new.sh
+++ Outils/VisIt/src/build_new.sh
@@ -29,18 +29,26 @@ done
 ###############
 # Configuration
 ###############
-conda=$TRUST_ROOT/exec/python
+#conda=$TRUST_ROOT/exec/python
+#with_conda=0 && [ -f $TRUST_ROOT/exec/python/bin/conda ] && with_conda=1
 options="--tarball $src"					# Construction de VisIt depuis les sources
 options=$options" --prefix $TRUST_ROOT/exec/VisIt"		# Repertoire installation
 options=$options" --cc $TRUST_cc"				# Compilateur C
 options=$options" --cxx $TRUST_CC"				# Compilateur C++
-options=$options" --fc $TRUST_F77" 				# Compilateur Fortran
+options=$options" --fc $TRUST_F77"				# Compilateur Fortran
 options=$options" --makeflags -j$TRUST_NB_PROCS"		# Parallel build
-options=$options" --thirdparty-path $TRUST_TMP" 		# Pour partage par plusieurs installations TRUST des 3rd party
-options=$options" --system-cmake " #--alt-cmake-dir $conda" 	# Cmake de Miniconda; sauf pour topaze ou on prend cmake 3.22 module
-options=$options" --system-qt --alt-qt-dir $conda" 		# Qt5 de Miniconda
+options=$options" --thirdparty-path $TRUST_TMP"			# Pour partage par plusieurs installations TRUST des 3rd party
 options=$options" --no-sphinx"					# Disable pour eviter l'installation de Python3 et pas mal de modules...
-options=$options" --system-python --alt-python-dir $conda" 	# VisIt ne supporte pas Python3 systeme
+options=$options" --qt"                                         #
+#if [ "$with_conda" = "1" ]
+#then
+#   options=$options" --system-cmake"				# we use system cmake de Miniconda; sauf pour topaze ou on prend cmake 3.22 module
+#   options=$options" --system-qt --alt-qt-dir $conda" 		# Qt5 de Miniconda
+#   options=$options" --system-python --alt-python-dir $conda" 	# VisIt 3.3.3 supporte Python3.7
+#else
+    # VisIt 3.3.3 ne s'installe pas si python>=3.8 et si qt>=5.15, on demande a visit de les installer
+    options=$options" --python --cmake --qt"				# VisIt installe son python (3.7) et son qt (5.14).
+#fi
 if [ "$build_parallel" != "" ]
 then
    #export PAR_COMPILER=$TRUST_cc
@@ -56,12 +64,35 @@ fi
 # Hacks:
 ########
 unset OPT 												# Sinon numpy plante a la compilation car $OPT utilise...
-#pkg=xorg-libsm && [ "`conda list $pkg | grep 1.2.3`" = "" ] && conda install -c conda-forge $pkg -y	# Sinon /usr/bin/ld: /usr/lib64/libSM.so: undefined reference to `uuid_generate@UUID_1.0'
 VISIT_QT_DIR="VISIT_OPTION_DEFAULT(VISIT_QT_DIR \${QT_BIN_DIR}/..)"					# VISIT_QT_DIR non fixe lors du build de VisIt
 if [ "`grep "$VISIT_QT_DIR" $build`" = "" ]
 then
    sed -i "1,$ s?VISIT_OPTION_DEFAULT(VISIT_QT_BIN?$VISIT_QT_DIR\nVISIT_OPTION_DEFAULT(VISIT_QT_BIN?" $build	
 fi
+
+# List of supplementary patches applied for VisIt 3.3.3 :
+# 1. Python-3.7.7.tgz when compiled with mpicc runs into error (adds intel flag), in function apply_python_patch :
+#        # Apply patch for python if built with mpicc instead of gcc/icc
+#        sed -i 's/^case "$CC" in/case `basename "$CC"` in/' configure
+#        sed -i 's/^\*icc\*)/icc)/' configure
+# 2. in function build_mesagl, patch needed for gcc 10:
+#         sed -i "s/struct util_hash_table/static struct util_hash_table/" src/gallium/auxiliary/util/u_debug_symbol.c
+# 3. in function build_osmesa, patch needed for gcc 10:
+#         sed -i "s/struct util_hash_table/static struct util_hash_table/" src/gallium/auxiliary/util/u_debug_symbol.c
+# 
+# 4. apply_qt_5142_numeric_limits_patch and apply_qt_5142_cmath_patch fail on irene/topaze, we do not exit:
+#     elif [[ ${QT_VERSION} == 5.14.2 ]] ; then
+#        apply_qt_5142_numeric_limits_patch
+#     +  echo "skipped, patch KO on irene"
+#        if [[ $? != 0 ]] ; then
+#            return 1
+#        fi
+#        apply_qt_5142_cmath_patch
+#     +  echo "skipped, patch KO on irene"
+#        if [[ $? != 0 ]] ; then
+#            return 1
+#        fi
+ 
 mkdir -p $TRUST_ROOT/exec/VisIt  									# Important sinon current pas cree au premier make install
 
 ########
