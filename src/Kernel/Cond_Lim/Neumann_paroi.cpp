5783e25e1accf3136de5e1ac504093576dafc2ac
triou <trust@cea.fr>
2023-10-30 15:17:21 +0100

    [Source_canal_perio] create a folder for each test cases run in the form except for std

--- src/Kernel/Cond_Lim/Neumann_paroi.cpp
+++ src/Kernel/Cond_Lim/Neumann_paroi.cpp
@@ -1,5 +1,5 @@
 /****************************************************************************
-* Copyright (c) 2022, CEA
+* Copyright (c) 2023, CEA
 * All rights reserved.
 *
 * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
@@ -19,6 +19,7 @@
 #include <Milieu_base.h>
 
 Implemente_instanciable(Neumann_paroi, "Neumann_paroi", Neumann);
+// XD Neumann_paroi condlim_base Neumann_paroi -1 Neumann boundary condition for mass equation (multiphase problem)
 
 Sortie& Neumann_paroi::printOn(Sortie& s) const { return s << que_suis_je() << finl; }
 
