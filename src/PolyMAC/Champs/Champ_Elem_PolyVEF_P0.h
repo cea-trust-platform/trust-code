fdd30f10098fc665302e7575c05b43c9d7447b0f
Elie Saikali <elie.saikali@cea.fr>
2023-11-28 20:29:53 +0100

    [PolyVEF_P0] Add champs & geometrie

---
+++ src/PolyMAC/Champs/Champ_Elem_PolyVEF_P0.h
@@ -0,0 +1,54 @@
+/****************************************************************************
+* Copyright (c) 2024, CEA
+* All rights reserved.
+*
+* Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+* 1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+* 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+* 3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+*
+* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+* IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+* OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*
+*****************************************************************************/
+
+#ifndef Champ_Elem_PolyVEF_P0_included
+#define Champ_Elem_PolyVEF_P0_included
+
+#include <Champ_Elem_PolyVEF_P0P1NC.h>
+#include <Operateur.h>
+
+class Domaine_PolyVEF_P0;
+
+/*! @brief : class Champ_Elem_PolyVEF_P0
+ *
+ *  Champ correspondant a une inconnue scalaire (type temperature ou pression)
+ *  Degres de libertes : valeur aux elements + flux aux faces
+ *
+ */
+class Champ_Elem_PolyVEF_P0: public Champ_Elem_PolyVEF_P0P1NC
+{
+  Declare_instanciable(Champ_Elem_PolyVEF_P0);
+
+public:
+
+  const Domaine_PolyVEF_P0& domaine_PolyVEF_P0() const;
+
+  void init_auxiliary_variables() override;
+  Champ_base& affecter_(const Champ_base& ch) override { return Champ_Inc_P0_base::affecter_(ch); }
+
+  // Fonctions pour le calcul des coefficients du gradient
+  mutable IntTab fgrad_d, fgrad_e;             // Tables used in domaine_PolyVEF_P0::fgrad
+  mutable DoubleTab fgrad_w;
+  void init_grad(int full_stencil) const;      // Call to initialise the tables ; no updates necessary
+  void calc_grad(int full_stencil) const;      // Call to calculate the tables ; updates necessary
+  void mettre_a_jour(double tps) override;
+
+protected:
+
+  mutable int grad_a_jour = 0;
+  mutable double tps_last_calc_grad_ = -1.e8;
+};
+
+#endif /* Champ_Elem_PolyVEF_P0_included */
---
+++ src/PolyMAC/Geometrie/Domaine_PolyVEF_P0.h
@@ -0,0 +1,54 @@
+/****************************************************************************
+* Copyright (c) 2024, CEA
+* All rights reserved.
+*
+* Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+* 1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+* 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+* 3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+*
+* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+* IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+* OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*
+*****************************************************************************/
+
+#ifndef Domaine_PolyVEF_P0_included
+#define Domaine_PolyVEF_P0_included
+
+#include <Domaine_PolyVEF_P0P1NC.h>
+
+class Domaine_PolyVEF_P0 : public Domaine_PolyVEF_P0P1NC
+{
+  Declare_instanciable(Domaine_PolyVEF_P0);
+public :
+  void discretiser() override;
+
+  //stencil du gradient aux faces fgrad : fsten_eb([fsten_d(f), fsten_d(f + 1)[]) -> elements e, faces de bord ne_tot + f
+  void init_stencils() const;
+  mutable IntTab fsten_d, fsten_eb;
+
+  //pour u.n champ T aux elements, interpole [n_f.nu.grad T]_f
+  //en preservant exactement les champs verifiant [nu grad T]_e = cte.
+  //Entrees : N             : nombre de composantes
+  //          is_p          : 1 si on traite le champ de pression (inversion Neumann / Dirichlet)
+  //          vec           : 1 si on veut le vecteur complet a la face
+  //          cls           : conditions aux limites
+  //          fcl(f, 0/1/2) : donnes sur les CLs (type de CL, indice de CL, indice dans la CL) (cf. Champ_{P0,Face}_PolyVEF_P0)
+  //          nu(e, n, ..)  : diffusivite aux elements (optionnel)
+  //          som_ext       : liste de sommets a ne pas traiter (ex. : traitement direct des Echange_Contact dans Op_Diff_PolyVEF_P0_Elem)
+  //          virt          : 1 si on veut aussi le flux aux faces virtuelles
+  //          full_stencil  : 1 si on veut le stencil complet (pour dimensionner())
+  //Sorties : phif_d(f, 0/1)                       : indices dans phif_{e,c} / phif_{pe,pc} du flux a f dans [phif_d(f, 0/1), phif_d(f + 1, 0/1)[
+  //          phif_e(i), phif_c(i, n, c)           : indices/coefficients locaux (pas d'Echange_contact) et diagonaux (composantes independantes)
+  void fgrad(int N, int is_p, int vec, const Conds_lim& cls, const IntTab& fcl, const DoubleTab *nu, const IntTab *som_ext,
+             int virt, int full_stencil, IntTab& phif_d, IntTab& phif_e, DoubleTab& phif_c) const;
+
+  //MD_Vectors pour la pression (elems + faces de bord), pour Champ_Elem_PolyMAC (elems + faces) et pour Champ_Face_PolyMAC (faces + aretes)
+  mutable MD_Vector mdv_elems_fbord, mdv_elems_faces, mdv_faces_aretes;
+
+private:
+  mutable int first_fgrad_ = 1; //pour n'afficher le message "MPFA-O MPFA-O(h) VFSYM" qu'une seule fois par calcul
+};
+
+#endif /* Domaine_PolyVEF_P0_included */
