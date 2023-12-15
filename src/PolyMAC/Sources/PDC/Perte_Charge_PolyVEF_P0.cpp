9f9bff88d0ed34ae0129f6854390bdd992514b06
Elie Saikali <elie.saikali@cea.fr>
2023-12-15 21:31:36 +0100

    [PolyVEF_P0] Perte_Charge_PolyVEF_P0 has interface blocs ...

--- src/PolyMAC/Sources/PDC/Perte_Charge_PolyVEF_P0.cpp
+++ src/PolyMAC/Sources/PDC/Perte_Charge_PolyVEF_P0.cpp
@@ -39,7 +39,6 @@ Entree& Perte_Charge_PolyVEF_P0::readOn(Entree& is) { return Perte_Charge_PolyMA
 void Perte_Charge_PolyVEF_P0::ajouter_blocs(matrices_t matrices, DoubleTab& secmem, const tabs_t& semi_impl) const
 {
   assert(has_interface_blocs());
-
   const Domaine_Poly_base& domaine = ref_cast(Domaine_Poly_base, equation().domaine_dis());
   const Pb_Multiphase *pbm = sub_type(Pb_Multiphase, equation().probleme()) ? &ref_cast(Pb_Multiphase, equation().probleme()) : nullptr;
   const Champ_Face_PolyVEF_P0& ch = ref_cast(Champ_Face_PolyVEF_P0, equation().inconnue());
@@ -145,114 +144,6 @@ void Perte_Charge_PolyVEF_P0::ajouter_blocs(matrices_t matrices, DoubleTab& secm
                   for (d = 0; d < D; d++)
                     for (m = 0; m < N; m++)
                       (*mat)(N * (D * f + d) + n, N * (D * f + d) + m) += ((m == n) * fac_n + fac_m) * (alp ? (*alp)(e, m) : 1) * (pbm ? rho(!cR * e, m) : 1);
-              }
-          }
-    }
-}
-
-DoubleTab& Perte_Charge_PolyVEF_P0::ajouter(DoubleTab& resu) const
-{
-  if (has_interface_blocs()) return Source_base::ajouter(resu);
-
-  const Domaine_PolyMAC& domaine = le_dom_PolyMAC.valeur();
-  const Champ_Face_PolyVEF_P0& ch = ref_cast(Champ_Face_PolyVEF_P0, equation().inconnue());
-  const Champ_Don_base& nu = le_fluide->viscosite_cinematique(), &dh = diam_hydr;
-  const DoubleTab& xp = domaine.xp(), &xv = domaine.xv(), &vit = la_vitesse->valeurs();
-  const DoubleVect& pe = equation().milieu().porosite_elem(), &pf = equation().milieu().porosite_face(), &fs = domaine.face_surfaces();
-  const Sous_Domaine *pssz = sous_domaine ? &le_sous_domaine.valeur() : nullptr;
-  const IntTab& e_f = domaine.elem_faces(), &f_e = domaine.face_voisins();
-  int i, j, k, f, fb, r, C_nu = sub_type(Champ_Uniforme, nu), C_dh = sub_type(Champ_Uniforme, diam_hydr.valeur());
-  double t = equation().schema_temps().temps_courant();
-
-  DoubleVect pos(dimension), ve(dimension), ved(dimension), vep(dimension), dir(dimension);
-
-  /* contribution de chaque element ou on applique la perte de charge */
-  for (i = 0; i < (pssz ? pssz->nb_elem_tot() : domaine.nb_elem_tot()); i++)
-    {
-      int e = pssz ? (*pssz)[i] : i;
-      for (r = 0; r < dimension; r++)
-        pos(r) = xp(e, r);
-
-      /* valeurs evaluees en l'element : nu, Dh, vecteur vitesse, Re, coefficients de perte de charge isotrope et directionel + la direction */
-      double nu_e = C_nu ? nu.valeurs()(0, 0) : nu.valeur_a_compo(pos, 0), dh_e = C_dh ? dh.valeurs()(0, 0) : dh.valeur_a_compo(pos, 0);
-      for (j = domaine.vedeb(e), ve = 0; j < domaine.vedeb(e + 1); j++)
-        for (r = 0; r < dimension; r++)
-          fb = domaine.veji(j), ve(r) += domaine.veci(j, r) * vit(fb) * pf(fb) / pe(e);
-      double n_ve = sqrt(domaine.dot(ve.addr(), ve.addr())), Re = std::max(n_ve * dh_e / nu_e, 1e-10), C_iso, C_dir, v_dir;
-      coeffs_perte_charge(ve, pos, t, n_ve, dh_e, nu_e, Re, C_iso, C_dir, v_dir, dir);
-
-      /* contributions aux faces de e */
-      for (j = 0; j < e_f.dimension(1) && (f = e_f(e, j)) >= 0; j++)
-        if (f < domaine.nb_faces() && ch.fcl()(f, 0) < 2)
-          {
-            double m2vf = 0, contrib;
-            for (k = domaine.m2i(domaine.m2d(e) + j); k < domaine.m2i(domaine.m2d(e) + j + 1); k++)
-              fb = e_f(e, domaine.m2j(k)), m2vf += pf(f) * (e == f_e(f, 0) ? 1 : -1) * (e == f_e(fb, 0) ? 1 : -1) * domaine.volumes(e) * domaine.m2c(k) * vit(fb) * pf(fb) / pe(e);
-            contrib = C_iso * m2vf + fs(f) * pf(f) * (C_dir - C_iso) * domaine.dot(&ve(0), &dir(0)) * (e == f_e(f, 0) ? 1 : -1) * domaine.dot(&xv(f, 0), &dir(0), &xp(e, 0));
-            if (contrib <= std::min(C_dir, C_iso) * m2vf)
-              contrib = std::min(C_dir, C_iso) * m2vf; //pour garantir un frottement minimal
-            resu(f) -= contrib;
-          }
-    }
-  return resu;
-}
-
-void Perte_Charge_PolyVEF_P0::contribuer_a_avec(const DoubleTab& inco, Matrice_Morse& matrice) const
-{
-  if (has_interface_blocs())
-    {
-      Source_base::contribuer_a_avec(inco, matrice);
-      return;
-    }
-
-  const Domaine_PolyMAC& domaine = le_dom_PolyMAC.valeur();
-  const Champ_Face_PolyVEF_P0& ch = ref_cast(Champ_Face_PolyVEF_P0, equation().inconnue());
-  const Champ_Don_base& nu = le_fluide->viscosite_cinematique(), &dh = diam_hydr;
-  const DoubleTab& xp = domaine.xp(), &xv = domaine.xv(), &vit = inco;
-  const DoubleVect& pe = equation().milieu().porosite_elem(), &pf = equation().milieu().porosite_face(), &fs = domaine.face_surfaces();
-  const Sous_Domaine *pssz = sous_domaine ? &le_sous_domaine.valeur() : nullptr;
-  const IntTab& e_f = domaine.elem_faces(), &f_e = domaine.face_voisins();
-  int i, j, k, f, fb, r, C_nu = sub_type(Champ_Uniforme, nu), C_dh = sub_type(Champ_Uniforme, diam_hydr.valeur());
-  double t = equation().schema_temps().temps_courant();
-  DoubleVect pos(dimension), ve(dimension), vf(dimension), dir(dimension);
-
-  for (i = 0; i < (pssz ? pssz->nb_elem_tot() : domaine.nb_elem_tot()); i++)
-    {
-      int e = pssz ? (*pssz)[i] : i;
-      for (r = 0; r < dimension; r++)
-        pos(r) = xp(e, r);
-
-      /* valeurs evaluees en l'element : nu, Dh, vecteur vitesse, Re, coefficients de perte de charge isotrope et directionel + la direction */
-      double nu_e = C_nu ? nu.valeurs()(0, 0) : nu.valeur_a_compo(pos, 0), dh_e = C_dh ? dh.valeurs()(0, 0) : dh.valeur_a_compo(pos, 0);
-      for (j = domaine.vedeb(e), ve = 0; j < domaine.vedeb(e + 1); j++)
-        for (r = 0; r < dimension; r++)
-          fb = domaine.veji(j), ve(r) += domaine.veci(j, r) * vit(fb) * pf(fb) / pe(e);
-      double n_ve = sqrt(domaine.dot(ve.addr(), ve.addr())), Re = std::max(n_ve * dh_e / nu_e, 1e-10), C_iso, C_dir, v_dir;
-      coeffs_perte_charge(ve, pos, t, n_ve, dh_e, nu_e, Re, C_iso, C_dir, v_dir, dir);
-
-      /* contributions aux faces de e */
-      for (j = 0; j < e_f.dimension(1) && (f = e_f(e, j)) >= 0; j++)
-        if (f < domaine.nb_faces() && ch.fcl()(f, 0) < 2)
-          {
-            double m2vf = 0, contrib;
-            for (k = domaine.m2i(domaine.m2d(e) + j); k < domaine.m2i(domaine.m2d(e) + j + 1); k++)
-              fb = e_f(e, domaine.m2j(k)), m2vf += pf(f) * (e == f_e(f, 0) ? 1 : -1) * (e == f_e(fb, 0) ? 1 : -1) * domaine.volumes(e) * domaine.m2c(k) * vit(fb) * pf(fb) / pe(e);
-            contrib = C_iso * m2vf + fs(f) * pf(f) * (C_dir - C_iso) * domaine.dot(&ve(0), &dir(0)) * (e == f_e(f, 0) ? 1 : -1) * domaine.dot(&xv(f, 0), &dir(0), &xp(e, 0));
-            if (contrib >= std::min(C_dir, C_iso) * m2vf)
-              {
-                for (k = domaine.m2i(domaine.m2d(e) + j); k < domaine.m2i(domaine.m2d(e) + j + 1); k++)
-                  if (ch.fcl()(fb = e_f(e, domaine.m2j(k)), 0) < 2)
-                    matrice(f, fb) += C_iso * pf(f) * (e == f_e(f, 0) ? 1 : -1) * (e == f_e(fb, 0) ? 1 : -1) * domaine.volumes(e) * domaine.m2c(k) * pf(fb) / pe(e);
-                for (k = domaine.vedeb(e); k < domaine.vedeb(e + 1); k++)
-                  if (ch.fcl()(fb = domaine.veji(k), 0) < 2)
-                    matrice(f, fb) += fs(f) * pf(f) * (C_dir - C_iso) * domaine.dot(&domaine.veci(k, 0), &dir(0)) * pf(fb) / pe(e) * (e == f_e(f, 0) ? 1 : -1)
-                                      * domaine.dot(&xv(f, 0), &dir(0), &xp(e, 0));
-              }
-            else
-              {
-                for (k = domaine.m2i(domaine.m2d(e) + j); k < domaine.m2i(domaine.m2d(e) + j + 1); k++)
-                  if (ch.fcl()(fb = e_f(e, domaine.m2j(k)), 0) < 2)
-                    matrice(f, fb) += std::min(C_dir, C_iso) * pf(f) * (e == f_e(f, 0) ? 1 : -1) * (e == f_e(fb, 0) ? 1 : -1) * domaine.volumes(e) * domaine.m2c(k) * pf(fb) / pe(e);
               }
           }
     }
