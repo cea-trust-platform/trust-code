297f4e24a80afa5c5133aea0e9593c2f521aae9c
Elie Saikali <elie.saikali@cea.fr>
2023-12-15 23:15:41 +0100

    [PolyMAC] Change & re-write PDC inheritance !

--- src/PolyMAC/Sources/PDC/Perte_Charge_Isotrope_PolyMAC_Face.cpp
+++ src/PolyMAC/Sources/PDC/Perte_Charge_Isotrope_PolyMAC_Face.cpp
@@ -17,16 +17,41 @@
 #include <Motcle.h>
 
 Implemente_instanciable(Perte_Charge_Isotrope_PolyMAC_Face, "Perte_Charge_Isotrope_Face_PolyMAC", Perte_Charge_PolyMAC);
-Implemente_instanciable(Perte_Charge_Isotrope_PolyMAC_P0P1NC_Face, "Perte_Charge_Isotrope_Face_PolyMAC_P0P1NC|Perte_Charge_Isotrope_Face_PolyMAC_P0", Perte_Charge_Isotrope_PolyMAC_Face);
 
 Sortie& Perte_Charge_Isotrope_PolyMAC_Face::printOn(Sortie& s) const { return s << que_suis_je() << finl; }
 Entree& Perte_Charge_Isotrope_PolyMAC_Face::readOn(Entree& s) { return Perte_Charge_PolyMAC::readOn(s); }
-Sortie& Perte_Charge_Isotrope_PolyMAC_P0P1NC_Face::printOn(Sortie& s) const { return s << que_suis_je() << finl; }
-Entree& Perte_Charge_Isotrope_PolyMAC_P0P1NC_Face::readOn(Entree& s) { return Perte_Charge_Isotrope_PolyMAC_Face::readOn(s); }
 
 void Perte_Charge_Isotrope_PolyMAC_Face::coeffs_perte_charge(const DoubleVect& u, const DoubleVect& pos, double t, double norme_u,
                                                              double dh, double nu, double reynolds, double& coeff_ortho,
                                                              double& coeff_long, double& u_l, DoubleVect& v_valeur) const
+{
+  coeffs_perte_charge_impl(u, pos, t, norme_u, dh, nu, reynolds, coeff_ortho, coeff_long, u_l, v_valeur, lambda);
+}
+
+/////////////////////////////////////////////////
+
+Implemente_instanciable(Perte_Charge_Isotrope_PolyMAC_P0P1NC_Face, "Perte_Charge_Isotrope_Face_PolyMAC_P0P1NC|Perte_Charge_Isotrope_Face_PolyMAC_P0", Perte_Charge_PolyMAC_P0P1NC);
+
+Sortie& Perte_Charge_Isotrope_PolyMAC_P0P1NC_Face::printOn(Sortie& s) const { return s << que_suis_je() << finl; }
+Entree& Perte_Charge_Isotrope_PolyMAC_P0P1NC_Face::readOn(Entree& s) { return Perte_Charge_PolyMAC_P0P1NC::readOn(s); }
+
+void Perte_Charge_Isotrope_PolyMAC_P0P1NC_Face::coeffs_perte_charge(const DoubleVect& u, const DoubleVect& pos, double t, double norme_u,
+                                                                    double dh, double nu, double reynolds, double& coeff_ortho,
+                                                                    double& coeff_long, double& u_l, DoubleVect& v_valeur) const
+{
+  coeffs_perte_charge_impl(u, pos, t, norme_u, dh, nu, reynolds, coeff_ortho, coeff_long, u_l, v_valeur, lambda);
+}
+
+/////////////////////////////////////////////////
+
+Implemente_instanciable(Perte_Charge_Isotrope_PolyVEF_P0_Face, "Perte_Charge_Isotrope_Face_PolyVEF_P0", Perte_Charge_PolyVEF_P0);
+
+Sortie& Perte_Charge_Isotrope_PolyVEF_P0_Face::printOn(Sortie& s) const { return s << que_suis_je() << finl; }
+Entree& Perte_Charge_Isotrope_PolyVEF_P0_Face::readOn(Entree& s) { return Perte_Charge_PolyVEF_P0::readOn(s); }
+
+void Perte_Charge_Isotrope_PolyVEF_P0_Face::coeffs_perte_charge(const DoubleVect& u, const DoubleVect& pos, double t, double norme_u,
+                                                                double dh, double nu, double reynolds, double& coeff_ortho,
+                                                                double& coeff_long, double& u_l, DoubleVect& v_valeur) const
 {
   coeffs_perte_charge_impl(u, pos, t, norme_u, dh, nu, reynolds, coeff_ortho, coeff_long, u_l, v_valeur, lambda);
 }
