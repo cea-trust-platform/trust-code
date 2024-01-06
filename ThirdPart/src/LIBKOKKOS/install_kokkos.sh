a4c527b260eb4f12b1c0b49a20807b043c8bb0c3
Pierre LEDAC <pierre.ledac@cea.fr>
2024-01-06 16:23:02 +0100

    [Kokkos] Becomes a mandatory (TRUST_USE_KOKKOS=1) prerequisite of TRUST. Moved under ThirdPart directory

--- ThirdPart/src/LIBKOKKOS/install_kokkos.sh
+++ ThirdPart/src/LIBKOKKOS/install_kokkos.sh
@@ -1,15 +1,14 @@
 #!/bin/sh
 
 archive=$TRUST_ROOT/externalpackages/kokkos/kokkos-3.7.02.tgz
-# ToDo mv to LIBKOKKOS car Kokkos prerequis obligatoire
 # Attention 4.x C++17 pour TRUST GPU mais necessaire pour nvc++ -cuda
 [ "$TRUST_USE_OPENMP" = 1 ] && archive=$TRUST_ROOT/externalpackages/kokkos/kokkos-4.2.00.tgz
 build_dir=$TRUST_ROOT/build/kokkos
-KOKKOS_ROOT_DIR=$TRUST_ROOT/env/gpu/kokkos
+KOKKOS_ROOT_DIR=$TRUST_ROOT/lib/src/LIBKOKKOS
 # Log file of the process:
 log_file=$TRUST_ROOT/kokkos_compile.log
 
-if [ ! -f $KOKKOS_ROOT_DIR/bin/nvcc_wrapper ]; then
+if [ ! -f $KOKKOS_ROOT_DIR/lib64/libkokkos.a ]; then
     echo "# Installing `basename $archive` ..."
     # Sub shell to get back to correct dir:
     (  
@@ -21,12 +20,11 @@ if [ ! -f $KOKKOS_ROOT_DIR/bin/nvcc_wrapper ]; then
 
         mkdir -p BUILD
         cd BUILD
-
-        CMAKE_OPT="-DCMAKE_CXX_COMPILER=$src_dir/bin/nvcc_wrapper"   # For now take wrapper as in the sources of Kokkos - later will used installed one.
+        CMAKE_OPT="-DCMAKE_CXX_COMPILER=$TRUST_CC_BASE"
         # We try to use nvc++ for Cuda to mix Kokkos with OpenMP:
-        [ "$TRUST_USE_CUDA" = 1 ] && [ "$TRUST_USE_OPENMP" = 1 ] && CMAKE_OPT="-DCMAKE_CXX_COMPILER=nvc++ -DKokkos_ENABLE_IMPL_NVHPC_AS_DEVICE_COMPILER=ON -DKokkos_ENABLE_CUDA_RELOCATABLE_DEVICE_CODE=ON"
+        [ "$TRUST_USE_CUDA" = 1 ] && [ "$TRUST_USE_OPENMP" = 1 ] && CMAKE_OPT="$CMAKE_OPT -DKokkos_ENABLE_IMPL_NVHPC_AS_DEVICE_COMPILER=ON -DKokkos_ENABLE_CUDA_RELOCATABLE_DEVICE_CODE=ON"
         [ "$TRUST_USE_CUDA" = 1 ]                                && CMAKE_OPT="$CMAKE_OPT -DKokkos_ENABLE_CUDA=ON -DKokkos_ENABLE_CUDA_LAMBDA=ON"
-        [ "$TRUST_USE_ROCM" = 1 ] && CMAKE_OPT="$CMAKE_OPT -DKokkos_ENABLE_HIP=ON" && echo "ToDo and test" && exit -1
+        [ "$TRUST_USE_ROCM" = 1 ]                                && CMAKE_OPT="$CMAKE_OPT -DKokkos_ENABLE_HIP=ON" && echo "ToDo and test" && exit -1
         CMAKE_OPT="$CMAKE_OPT -DKokkos_ENABLE_EXAMPLES=ON"
         CMAKE_OPT="$CMAKE_OPT -DCMAKE_INSTALL_PREFIX=$KOKKOS_ROOT_DIR"
         # Autres options: See https://kokkos.github.io/kokkos-core-wiki/keywords.html#cmake-keywords
@@ -42,7 +40,7 @@ if [ ! -f $KOKKOS_ROOT_DIR/bin/nvcc_wrapper ]; then
         # Clean build:
         rm -rf $build_dir
     )   
-    echo "# Kokkos OK! Path to nvcc_wrapper is: $KOKKOS_ROOT_DIR/bin/nvcc_wrapper"
+    echo "# Kokkos OK! Path to Kokkos is: $KOKKOS_ROOT_DIR"
 else
     echo "# Kokkos: already installed. Doing nothing."
 fi
