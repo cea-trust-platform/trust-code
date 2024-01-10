efd7625d4cc2298e49c2ab69ac68c3dc93bf2426
Pierre LEDAC <pierre.ledac@cea.fr>
2024-01-10 14:25:18 +0100

    [GPU] Ok, memoire unfiee fonctionne bien, mais surcout sur cette exemple de 20% potentiellement amorti par la disparition de checkDataOnHost() des methodes TRUSTArray

--- env_src/gpu/reproducers/unified_memory.cpp
+++ env_src/gpu/reproducers/unified_memory.cpp
@@ -9,115 +9,68 @@ int main() {
     // Define the size of the array
     const int N = 1000000;
 
-    // Allocate Unified Memory – accessible from CPU or GPU
-    double *data;
-    cudaMallocManaged(&data, N*sizeof(double));
-
-    // Initialize data to 1 on the host
-    for (int i = 0; i < N; i++) {
-        data[i] = 1;
-    }
-    if (data[0]!=1) std::cerr << "Error: data[0]=" << data[0] << " should be 1." << std::endl;
-
-    // Perform the computation on the device to 2
-    // data is automatically copied to device
-    #pragma omp target teams distribute parallel for
-    for (int i = 0; i < N; i++)
     {
-        data[i] = data[i] * 2;
-    }
-    // Wait for GPU to finish before accessing on host
-    cudaDeviceSynchronize();
-    
-    // Normally thanks to UM, data is updated on the host:
-    if (data[0]!=2) std::cerr << "Error: data[0]=" << data[0] << " should be 2." << std::endl;
-    
-    // Free memory
-    cudaFree(data);
-     
-   /*
-    // Exemple: 1 millions mailles 100^3, CL 6xn^2, donc 60000 
-    const int N = 60000; // Gain interessant sur les petits envoi de donnees:
-// [Pageable memory] Mean time taken to copy data H->D: 0.000295352 s 1.51356 GB/s
-// [Pageable memory] Mean time taken to copy data D->H: 0.000196696 s 2.27272 GB/s
-// [Pinned memory]   Mean time taken to copy data H->D: 0.000163316 s 2.73724 GB/s
-// [Pinned memory]   Mean time taken to copy data D->H: 0.000150052 s 2.9792 GB/s
-
-    const int samples = 10;
-    // Initialize data on the host
-    double *host_data = new double[N]();
-
-    for (int i = 0; i < N; i++)
-        host_data[i] = 0;
-    #pragma omp target enter data map(alloc:host_data[0:N])
-    
-    double HtoD=0;
-    double DtoH=0; 
-    for (int sample=0;sample<samples;sample++)
-    {
+       double *data = new double[N];
+       // Initialize data to 1 on the host
+       for (int i = 0; i < N; i++) 
+          data[i] = 1;
+       if (data[0]!=1) std::cerr << "Error: data[0]=" << data[0] << " should be 1." << std::endl;
+       #pragma omp target enter data map(alloc:data[0:N])       
+       // Perform the computation on the device with explicite copy:
        auto start = std::chrono::high_resolution_clock::now();
-       #pragma omp target update to(host_data[0:N])
-       auto end = std::chrono::high_resolution_clock::now();
-       std::chrono::duration<double> diff = end - start;
-       HtoD += diff.count();       
-
+       #pragma omp target update to(data[0:N])
        #pragma omp target teams distribute parallel for
        for (int i = 0; i < N; i++)
-          host_data[i] = 1;
+           data[i] = data[i] * 2;       
+       
+       std::chrono::duration<double> diff = std::chrono::high_resolution_clock::now() - start;
+       std::cout << "[Non Unified Memory] H2D explicit copy then Device Kernel: " << 1000*diff.count() << " ms " << std::endl;   
 
-       // Copy data from device to host
+       // Loop on host with explicity copy: 
        start = std::chrono::high_resolution_clock::now();
-       #pragma omp target update from(host_data[0:N])
-       end = std::chrono::high_resolution_clock::now();
-       diff = end - start;
-       DtoH += diff.count();
-       if (host_data[10]!=1) abort();
+       int OK=1;
+       #pragma omp target update from(data[0:N])
+       for (int i = 0; i < N; i++)
+          if (data[i]!=2) OK=0;
+       diff = std::chrono::high_resolution_clock::now() - start;
+       std::cout << "[Non Unified Memory] D2H explicit copy then Host Kernel: " << 1000*diff.count() << " ms " << std::endl;   
+       std::cerr << (!OK ? "Error!" : "OK!") << std::endl;       
+       // Free memory
+       free(data);
     }
-    #pragma omp target exit data map(release:host_data[0:N])
-    HtoD/=samples;
-    DtoH/=samples;
-    std::cout << "[Pageable memory] Mean time taken to copy data H->D: " << HtoD << " s " << 8.0*N/(1024*1024*1024)/HtoD << " GB/s\n"; 
-    std::cout << "[Pageable memory] Mean time taken to copy data D->H: " << DtoH << " s " << 8.0*N/(1024*1024*1024)/DtoH << " GB/s\n"; 
 
-    // Pin memory on the host
-    cudaError_t err = cudaHostRegister(host_data, N * sizeof(double), cudaHostRegisterDefault);
-    double* pinned_mem = host_data;
-    
-    for (int i = 0; i < N; i++)
-        pinned_mem[i] = 0;
-    #pragma omp target enter data map(alloc:pinned_mem[0:N])
-
-    HtoD=0;
-    DtoH=0;
-    for (int sample=0;sample<samples;sample++)
     {
-       // Copy data from host to device using OpenMP target directives
+       // Allocate Unified Memory
+       double *data;
+       cudaMallocManaged(&data, N*sizeof(double));
+       
+       // Initialize data to 1 on the host
+       for (int i = 0; i < N; i++)
+          data[i] = 1;
+       if (data[0]!=1) std::cerr << "Error: data[0]=" << data[0] << " should be 1." << std::endl;
+
+       // Perform the computation on the device : data is automatically copied to device
        auto start = std::chrono::high_resolution_clock::now();
-       #pragma omp target update to(pinned_mem[0:N])
-       auto end = std::chrono::high_resolution_clock::now();
-       std::chrono::duration<double> diff = end - start;
-       HtoD += diff.count();
-        
-       // Compute data on device:
        #pragma omp target teams distribute parallel for
        for (int i = 0; i < N; i++)
-          pinned_mem[i] = 1;
-    
-       // Copy data from device to host
-       start = std::chrono::high_resolution_clock::now();
-       #pragma omp target update from(pinned_mem[0:N])
-       end = std::chrono::high_resolution_clock::now();
-       diff = end - start;
-       DtoH += diff.count();
-       if (pinned_mem[10]!=1) abort();
-    }
-    #pragma omp target exit data map(release:pinned_mem[0:N])
-    HtoD/=samples;
-    DtoH/=samples;
-    std::cout << "[Pinned memory]   Mean time taken to copy data H->D: " << HtoD << " s " << 8.0*N/(1024*1024*1024)/HtoD << " GB/s\n"; 
-    std::cout << "[Pinned memory]   Mean time taken to copy data D->H: " << DtoH << " s " << 8.0*N/(1024*1024*1024)/DtoH << " GB/s\n"; 
+           data[i] = data[i] * 2;
 
-    cudaHostUnregister(pinned_mem); */
+       // Wait for GPU to finish before accessing on host
+       cudaDeviceSynchronize();
+       std::chrono::duration<double> diff = std::chrono::high_resolution_clock::now() - start;
+       std::cout << "[Non Unified Memory] H2D automatic copy during Device Kernel: " << 1000*diff.count() << " ms " << std::endl;   
+ 
+       // Normally thanks to UM, data is updated on the host:
+       start = std::chrono::high_resolution_clock::now();
+       int OK=1;
+       for (int i = 0; i < N; i++)
+          if (data[i]!=2) OK=0;
+       diff = std::chrono::high_resolution_clock::now() - start;
+       std::cout << "[Non Unified Memory] D2H automatic copy during Host Kernel: " << 1000*diff.count() << " ms " << std::endl;   
+       std::cerr << (!OK ? "Error!" : "OK!") << std::endl;       
+       // Free memory
+       cudaFree(data);
+    }
     return 0;
 }
 
--- env_src/gpu/reproducers/unified_memory.sh
+++ env_src/gpu/reproducers/unified_memory.sh
@@ -1,6 +1,8 @@
-# Peut etre utiliser -gpu=managed ?
+# L'option -gpu=managed fait TOUTES les allocations dynamiques dans la memoire unifiee
+# 
+rm -f unified_memory
 $TRUST_CC_BASE -O3 -mp -target=gpu -I$CUDA_ROOT/include -L$CUDA_ROOT/lib64 -lcudart -o unified_memory unified_memory.cpp || exit -1
 echo "Build OK"
 rm -f *nsys-rep
-./unified_memory && [ "$1" = -nsys ] && nsys profile ./unified_memory && nsys-ui report1.nsys-rep
+./unified_memory && [ "$1" = -nsys ] && exec=`pwd`/unified_memory && trust -nsys dumb
 
