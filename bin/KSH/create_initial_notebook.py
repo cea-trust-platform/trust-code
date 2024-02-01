9e3e669e31574d9f805b85754f56c6704822813b
Alan BURLOT <alan.burlot@cea.fr>
2024-02-01 14:43:14 +0100

    change os.getlogin() to os.environ.get("USER") because it crashed
    I changed the string to the modern f-string format which is more readable.

--- bin/KSH/create_initial_notebook.py
+++ bin/KSH/create_initial_notebook.py
@@ -4,103 +4,94 @@ from datetime import datetime
 import os
 
 parser = argparse.ArgumentParser(description="Create jupyter notebook")
-parser.add_argument('-n','--namefile', required=True, metavar="File", help = "jupyter notebook name")
-parser.add_argument('-d','--datafile', nargs="+", required=True, metavar="File"   , help = "List of data files")
-parser.add_argument('-t','--tutorial', required=False, type=int, help = "print more examples")
+parser.add_argument('-n', '--namefile', required=True,
+                    metavar="File", help="jupyter notebook name")
+parser.add_argument('-d', '--datafile', nargs="+", required=True,
+                    metavar="File", help="List of data files")
+parser.add_argument('-t', '--tutorial', required=False, type=int,
+                    help="print more examples")
 args = parser.parse_args()
 
 Files = args.datafile
 
 nb = nbf.v4.new_notebook()
 
-title = "# %s test case\n" %args.namefile
+title = f"# {args.namefile} test case\n"
 
-intro = "## Introduction\n \n Validation made by: " + os.getlogin() + "\n\n"
+intro = "## Introduction\n \n Validation made by: " + os.environ.get("USER") + "\n\n"
 intro += " Report generated " + datetime.now().strftime("%d/%m/%Y")
 
 description = "### Description\n \n"
-
 description += "Sketch of a classic jupyter validation form with main example of validation.\n\n"
 description += "For additional information, please consult validation form documentation with <code>Run_fiche -doc</code> and report to the more detailed tutorial in $TRUST_ROOT/Validation/Rapports_automatiques/Verification/SampleFormJupyter."
 
-binary, version = os.environ.get("exec", "[UNKNOWN]"), os.environ.get("TRUST_VERSION", "[UNKNOWN]")
+binary = os.environ.get("exec", "[UNKNOWN]")
+version = os.environ.get("TRUST_VERSION", "[UNKNOWN]")
 
 origin = os.getcwd()
 path = os.path.join(origin, "build")
 
-parameters = "from trustutils import run \n \n"
-parameters += "run.TRUST_parameters(\"" + version + "\")"
+parameters = "from trustutils import run\n\n"
+parameters += f"run.TRUST_parameters(\"{version}\")"
 
-testCases = "from trustutils import run\n \n"
+testCases = "from trustutils import run\n\n"
 testCases += "run.reset()\n"
 
 for datafile in Files:
     dirname_, basename_ = os.path.split(datafile)
     if dirname_ == "":
         dirname_ = "."
-    testCases += "run.addCase(\"%s\",\"%s\",nbProcs=1) \n" % (dirname_,basename_)
+    testCases += f"run.addCase(\"{dirname_}\",\"{basename_}\",nbProcs=1)\n"
 testCases += "run.printCases()\n"
 testCases += "run.runCases()"
 
-if args.tutorial==1:
+if args.tutorial == 1:
     exampletest = Files[0].split('.data')[0]
-    
+
     graph_t = "## Visualization of the probes evolution (point and segment example)"
-    
     graph = "from trustutils import plot \n \n"
-    
     graph += "graph1=plot.Graph(\"Sonde point\",size=[15,8])\n"
-    graph += "graph1.addPoint(\"%s_SONDE_PRESSION.son\",compo=0,marker=\"+\")\n\n"%exampletest
-    
+    graph += f"graph1.addPoint(\"{exampletest}_SONDE_PRESSION.son\",compo=0,marker=\"+\")\n\n"
     graph += "graph2=plot.Graph(\"Sonde segment\",size=[15,8])\n"
-    graph += "graph2.addSegment(\"%s_SONDE_VITESSE.son\",compo=0,marker=\"-x\")\n"%exampletest
-    
-    
+    graph += f"graph2.addSegment(\"{exampletest}_SONDE_VITESSE.son\",compo=0,marker=\"-x\")\n"
+
     visit_t = "## Visualization via VisIt (Pressure field example)"
-    
-    visit = "from trustutils import visit\n \n"
-    visit += "fig=visit.Show(\"%s.lata\",\"Pseudocolor\",\"PRESSION_ELEM_dom\",nX=1,nY=1,mesh=\"dom\",title=\"\",time=-1)\n"%exampletest
+    visit = "from trustutils import visit\n\n"
+    visit += f"fig=visit.Show(\"{exampletest}.lata\",\"Pseudocolor\",\"PRESSION_ELEM_dom\",nX=1,nY=1,mesh=\"dom\",title=\"\",time=-1)\n"
     visit += "fig.plot()"
-    
-    tab_t = "## Table Creation" 
-    tab = "from trustutils import plot\n \n" 
+
+    tab_t = "## Table Creation"
+    tab = "from trustutils import plot\n\n"
     tab += "tab = plot.Table(['column1','column2'])\n"
     tab += "data = plot.loadText(\"file.dat\",transpose=False)\n"
     tab += "tab.addLigne([[data[0],data[1]]],\"label\")\n"
     tab += "display(tab)"
 
     data_t = "## Data"
-    data = "run.dumpDataset(\"%s.data\")"%exampletest
+    data = f"run.dumpDataset(\"{exampletest}.data\")"
 
 perf_t = "## Computer Performance"
 perf = "run.tablePerf()"
 
-
 nb['cells'] = [nbf.v4.new_markdown_cell(title),
-        nbf.v4.new_markdown_cell(intro),
-        nbf.v4.new_markdown_cell(description),
-        nbf.v4.new_code_cell(parameters),
-        nbf.v4.new_code_cell(testCases)]
+               nbf.v4.new_markdown_cell(intro),
+               nbf.v4.new_markdown_cell(description),
+               nbf.v4.new_code_cell(parameters),
+               nbf.v4.new_code_cell(testCases)]
 
-if args.tutorial==1:
-    nb['cells'] += [
-        nbf.v4.new_markdown_cell(graph_t),
-        nbf.v4.new_code_cell(graph),
-        nbf.v4.new_markdown_cell(visit_t),
-        nbf.v4.new_code_cell(visit),
-        nbf.v4.new_markdown_cell(tab_t),
-        nbf.v4.new_code_cell(tab)]
+if args.tutorial == 1:
+    nb['cells'] += [nbf.v4.new_markdown_cell(graph_t),
+                    nbf.v4.new_code_cell(graph),
+                    nbf.v4.new_markdown_cell(visit_t),
+                    nbf.v4.new_code_cell(visit),
+                    nbf.v4.new_markdown_cell(tab_t),
+                    nbf.v4.new_code_cell(tab)]
 
-nb['cells'] += [
-        nbf.v4.new_markdown_cell(perf_t),
-        nbf.v4.new_code_cell(perf)]
+nb['cells'] += [nbf.v4.new_markdown_cell(perf_t),
+                nbf.v4.new_code_cell(perf)]
 
-if args.tutorial==1:
-    nb['cells'] += [
-        nbf.v4.new_markdown_cell(data_t),
-        nbf.v4.new_code_cell(data)
-        ]
+if args.tutorial == 1:
+    nb['cells'] += [nbf.v4.new_markdown_cell(data_t),
+                    nbf.v4.new_code_cell(data)]
 
-nbf.write(nb, "%s.ipynb" % args.namefile)
-
-
+nbf.write(nb, f"{args.namefile}.ipynb")
