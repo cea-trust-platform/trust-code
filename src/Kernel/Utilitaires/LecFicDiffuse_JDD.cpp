752da7824f943214b490be5931c502366d557a12
triou <trust@cea.fr>
2024-02-16 15:32:56 +0100

    [jdd] Detect missing space near comma in the datafile and display a clear message

--- src/Kernel/Utilitaires/LecFicDiffuse_JDD.cpp
+++ src/Kernel/Utilitaires/LecFicDiffuse_JDD.cpp
@@ -1,5 +1,5 @@
 /****************************************************************************
-* Copyright (c) 2023, CEA
+* Copyright (c) 2024, CEA
 * All rights reserved.
 *
 * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
@@ -169,7 +169,7 @@ int LecFicDiffuse_JDD::ouvrir(const char* name,
               line+=jol;
             }
 
-          else if ((motlu.find("}")!=-1 && motlu != "}") || (motlu.find("{")!=-1 && motlu != "{") )
+          else if (((motlu.find("}")!=-1 && motlu != "}") || (motlu.find("{")!=-1 && motlu != "{") || (motlu.find(",")!=-1 && motlu != ",")) && ! fin_lu )
             {
               Nom msg = "Error while reading '" + motlu.getString() + "' from datafile " + name + " at line " + std::to_string(line) + ".\nCheck for missing space character.\n";
               msg+= "============================================\nExiting TRUST.";
