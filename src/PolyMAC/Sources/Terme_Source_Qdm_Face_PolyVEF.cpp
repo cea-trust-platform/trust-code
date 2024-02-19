a2e5a3d35ac684f31484d3d13a9fdbf407837595
Antoine Gerschenfeld <antoine.gerschenfeld@cea.fr>
2024-02-19 18:03:11 +0100

    Terme_Source_Qdm_PolyVEF: bugfix

--- src/PolyMAC/Sources/Terme_Source_Qdm_Face_PolyVEF.cpp
+++ src/PolyMAC/Sources/Terme_Source_Qdm_Face_PolyVEF.cpp
@@ -15,7 +15,7 @@
 
 #include <Terme_Source_Qdm_Face_PolyVEF.h>
 #include <Champ_Face_PolyVEF.h>
-#include <Domaine_PolyMAC_P0.h>
+#include <Domaine_PolyVEF.h>
 #include <Pb_Multiphase.h>
 #include <Equation_base.h>
 #include <Milieu_base.h>
@@ -32,21 +32,20 @@ void Terme_Source_Qdm_Face_PolyVEF::ajouter_blocs(matrices_t matrices, DoubleTab
 {
   const Domaine_Poly_base& dom = ref_cast(Domaine_Poly_base, equation().domaine_dis());
   const Champ_Face_PolyVEF& ch = ref_cast(Champ_Face_PolyVEF, equation().inconnue());
-  const DoubleTab& vals = la_source->valeurs(), &vfd = dom.volumes_entrelaces_dir(),
-                   &rho = equation().milieu().masse_volumique().passe(), &nf = dom.face_normales(),
-                    *alp = sub_type(Pb_Multiphase, equation().probleme()) ? &ref_cast(Pb_Multiphase, equation().probleme()).equation_masse().inconnue().passe() : nullptr;
-  const DoubleVect& pf = equation().milieu().porosite_face(), &fs = dom.face_surfaces();
+  const DoubleTab& vals = la_source->valeurs(), &vfd = dom.volumes_entrelaces_dir(), &rho = equation().milieu().masse_volumique().passe(),
+                   *alp = sub_type(Pb_Multiphase, equation().probleme()) ? &ref_cast(Pb_Multiphase, equation().probleme()).equation_masse().inconnue().passe() : nullptr;
+  const DoubleVect& pf = equation().milieu().porosite_face();
   const IntTab& f_e = dom.face_voisins(), &fcl = ch.fcl();
-  int e, f, i, cS = (vals.dimension_tot(0) == 1), cR = (rho.dimension_tot(0) == 1), p0 = sub_type(Domaine_PolyMAC_P0, dom),
-               d, D = dimension, n, N = equation().inconnue().valeurs().line_size() / (p0 ? D : 1);
+  int e, f, i, cS = (vals.dimension_tot(0) == 1), cR = (rho.dimension_tot(0) == 1), p0p1 = sub_type(Domaine_PolyVEF_P0P1, dom),
+               d, D = dimension, n, N = equation().inconnue().valeurs().line_size() / D;
 
   /* contributions aux faces (par chaque voisin), aux elems */
   DoubleTrav a_f(N), rho_f(N), val_f(N), rho_m(2);
 
   for (a_f = 1, f = 0; f < dom.nb_faces(); f++)
-    for (i = 0; i < 2 && (e = f_e(f, i)) >= 0; i++)
-      for (e = f_e(f, i), n = 0; n < N; n++)
-        for (d = 0; d < D; d++)
-          if (p0) secmem(f, N * d + n) += vfd(f, i) * pf(f) * (alp ? (*alp)(e, n) * rho(!cR * e, n) : 1) * vals(!cS * e, N * d + n);
-          else if (fcl(f, 0) < 2) secmem(f, n) += vfd(f, i) * pf(f) * (alp ? (*alp)(e, n) * rho(!cR * e, n) : 1) * nf(f, d) / fs(f) * vals(!cS * e, N * d + n);
+    if (!p0p1 || fcl(f, 0) < 2)
+      for (i = 0; i < 2 && (e = f_e(f, i)) >= 0; i++)
+        for (e = f_e(f, i), n = 0; n < N; n++)
+          for (d = 0; d < D; d++)
+            secmem(f, N * d + n) += vfd(f, i) * pf(f) * (alp ? (*alp)(e, n) * rho(!cR * e, n) : 1) * vals(!cS * e, N * d + n);
 }
