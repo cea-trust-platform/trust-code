bbf17f630131f6ffe08178c6186bec653334a152
abn <adrien.bruneton@cea.fr>
2024-02-27 17:33:57 +0100

    [MergeMeds] Now properly handle BC (face groups) and node merging

--- Outils/lata_tools/src/scripts/merge_meds.py
+++ Outils/lata_tools/src/scripts/merge_meds.py
@@ -1,4 +1,17 @@
-from __future__ import print_function
+"""
+Merge several MED files together
+
+Typically used to merge the several MED produced during a parallel computation into a single MED file.
+
+Usage:
+  python merge_meds.py MEDFilesToMerge.med NB_PROCS [MergedFileName.med]
+
+This script will:
+  - merge coincident nodes located on joints
+  - merge and recreate proper face groups for the boundaries
+  - merge the fields
+"""
+
 import medcoupling as mc
 from optparse import OptionParser
 import sys
@@ -7,17 +20,54 @@ import os
 
 def write_mesh():
     mn = mc.GetMeshNames(FilesList[0])
-    wfs = True
-    for n in mn:
+    new_m = mc.MEDFileUMesh()
+    for idx_m, n in enumerate(mn):
         print ("Mesh : %s, proc :" %format(n), end=" ")
-        meshes = []
+        mesh_top, mesh_faces = [], []
+        off_face = 0
+        grps = {}
         for i, f in enumerate(FilesList):
             print (i, end=" "),
-            mesh = mc.ReadUMeshFromFile(f, n, 0)
-            meshes.append(mesh)
-        mesh_m = mc.MEDCouplingUMesh.MergeUMeshes(meshes)
-        mesh_m.setName(meshes[0].getName())
-        mc.WriteUMesh(OutputFile, mesh_m, wfs); wfs = False
+            mesh = mc.MEDFileUMesh(f, n)
+            lvls = mesh.getNonEmptyLevels()
+            mesh_top.append(mesh.getMeshAtLevel(0))
+            if -1 in lvls: # There is a face mesh (for BC)
+                # Compute the offset that we will apply to the group identifiers:
+                if i > 0:
+                  off_face += mesh_faces[-1].getNumberOfCells()
+                fc = mesh.getMeshAtLevel(-1)
+                mesh_faces.append(fc)
+                grp_nams = mesh.getGroupsOnSpecifiedLev(-1)
+                for gn in grp_nams:
+                    arr = mesh.getGroupArr(-1, gn)
+                    arr += off_face  # shift
+                    grps.setdefault(gn, []).append(arr)
+        # Register highest dimension
+        top = mc.MEDCouplingUMesh.MergeUMeshes(mesh_top)
+        top.mergeNodes(1.0e-10)
+        new_m.setMeshAtLevel(0, top)
+
+        # Register face meshes with groups
+        if len(mesh_faces) != 0:
+            fc = mc.MEDCouplingUMesh.MergeUMeshes(mesh_faces)
+            fc.mergeNodes(1.0e-10)  # should merge exactly as above ... hmm not optimal
+            fc.setCoords(top.getCoords())
+            new_m.setMeshAtLevel(-1, fc)
+
+            #    Groups management
+            final_grps = []
+            for gnam, sub_g in grps.items():
+                da = mc.DataArrayInt.Aggregate(sub_g)
+                da.setName(gnam)
+                final_grps.append(da)
+            new_m.setGroupsAtLevel(-1, final_grps)
+        new_m.setName(mesh_top[0].getName())
+        if idx_m == 0:
+          write_opt = 2  # Overwrite
+        else:
+            write_opt = 1  # Append
+        new_m.write(OutputFile, write_opt)
+        new_m = mc.MEDFileUMesh() # reset object
         print ("done")
 
 def write_fields():
@@ -53,7 +103,7 @@ if __name__=="__main__":
     InputFile = sys.argv[1]
     if InputFile.endswith('.med'):
         InputFile = os.path.splitext(os.path.basename(InputFile))[0]
-    InputFile = InputFile[:-4] 
+    InputFile = InputFile[:-4]
 
     nb_procs = int(sys.argv[2])
 
