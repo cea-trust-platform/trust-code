b8166d305bdcb037dc822f2b3351d3ebe4fe41cb
triou <trust@cea.fr>
2024-02-29 22:48:29 +0100

    [Gnuplot] set pngcairo terminal if available; otherwise set terminal png

--- Validation/Outils/Genere_courbe/src/Figure.py
+++ Validation/Outils/Genere_courbe/src/Figure.py
@@ -260,7 +260,10 @@ class Figure:
                 #png
                 #ficPlot.write('set size 1.3,1.3\n')
                 #ficPlot.write('set terminal png large\n')
-                ficPlot.write('set terminal pngcairo size 800,600\n')
+                #ficPlot.write('set terminal pngcairo size 800,600\n')
+                # if pngcairo avail (FED38), it will be used; otherwise switch to png terminal
+                # see https://stackoverflow.com/questions/20657816/how-do-i-check-if-a-terminal-is-available-through-my-gnuplot-script
+                ficPlot.write("if (strstrt(GPVAL_TERMINALS, 'pngcairo') > 0) {\n    set terminal pngcairo size 800,600\n} else {\n    set terminal png large\n}\n")
                 self.fichierGraphique = 'fic%03d.png' % (indice)
 
             elif self.format=='epslatex':
