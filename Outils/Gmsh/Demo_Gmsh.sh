670126c540b2c5e6e358d0188af6325cfd1b0867
triou <trust@cea.fr>
2024-04-08 08:43:52 +0200

    [Makefile] Update makefile for MPI, Vc and tools

--- Outils/Gmsh/Demo_Gmsh.sh
+++ Outils/Gmsh/Demo_Gmsh.sh
@@ -10,7 +10,7 @@
 
 source $TRUST_ROOT/env_TRUST.sh 1>/dev/null 2>&1
 # On jean-zay, the Gmsh test is false since we build TRUST on compil partition
-[ $TRUST_WITHOUT_HOST = 0 ]  && echo "We do not check Gmsh on clusters" && exit 0
+[ $TRUST_WITHOUT_HOST = 0 ]  && [ "${HOST#cea-dm2s}" = "$HOST" ] && echo "We do not check Gmsh on clusters" && exit 0
 
 # Test Gmsh if installed
 [ -f ~/.gmshrc ] && mv ~/.gmshrc ~/.gmshrc.bak
