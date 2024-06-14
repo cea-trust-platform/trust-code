24c17238e25a4149112714673bbd02101215c412
Pierre LEDAC <pierre.ledac@cea.fr>
2024-06-14 13:34:22 +0200

    [Kokkos] Add sample for virtual function on device but crash ?

---
+++ env_src/gpu/reproducers/virtual_function.cpp
@@ -0,0 +1,90 @@
+#include <Kokkos_Core.hpp>
+#include <iostream>
+class Foo {
+ protected:
+  int val;
+
+ public:
+  KOKKOS_FUNCTION
+  Foo() { val = 0; };
+
+  KOKKOS_FUNCTION
+  virtual int value() { return 0; };
+
+  KOKKOS_FUNCTION
+  void set_value(double v) { val = v; }
+
+  KOKKOS_FUNCTION
+  virtual ~Foo() {}
+};
+
+class Foo_1 : public Foo {
+ public:
+  KOKKOS_FUNCTION
+  Foo_1() { val = 1; };
+
+  KOKKOS_FUNCTION
+  int value() { return val; };
+};
+
+class Foo_2 : public Foo {
+ public:
+  KOKKOS_FUNCTION
+  Foo_2() { val = 2; };
+  KOKKOS_FUNCTION
+  int value() { return val; };
+};
+
+int main(int argc, char* argv[]) {
+  Kokkos::initialize(argc, argv);
+
+  {
+    
+  Foo_1* deviceInstance = (Foo_1*)Kokkos::kokkos_malloc<Kokkos::CudaUVMSpace>(sizeof(Foo_1));
+  Kokkos::parallel_for( "CreateObjects", 1, KOKKOS_LAMBDA(const int&)
+  {
+    new((Foo_1*)deviceInstance) Foo_1(); // initialize an instance, and place the result in the pointer deviceInstance
+  });
+  deviceInstance->set_value(-1); //OK
+
+  Kokkos::parallel_for( "CreateObjects", 1, KOKKOS_LAMBDA(const int&)
+  {
+    Kokkos::printf("value=%d \n", deviceInstance->value());
+  });
+  //std::cerr << deviceInstance->value() << std::endl;
+  //deviceInstance->setAField(someHostValue); // set some field on the host
+
+  /*
+    Foo* f_1 = (Foo*)Kokkos::kokkos_malloc(sizeof(Foo_1));
+    Foo* f_2 = (Foo*)Kokkos::kokkos_malloc(sizeof(Foo_2));
+
+    Kokkos::parallel_for(
+        "CreateObjects", 1, KOKKOS_LAMBDA(const int&) {
+          new ((Foo_1*)f_1) Foo_1();
+          new ((Foo_2*)f_2) Foo_2();
+        });
+
+    int value_1, value_2;
+    Kokkos::parallel_reduce(
+        "CheckValues", 1,
+        KOKKOS_LAMBDA(const int&, int& lsum) { lsum = f_1->value(); }, value_1);
+
+    Kokkos::parallel_reduce(
+        "CheckValues", 1,
+        KOKKOS_LAMBDA(const int&, int& lsum) { lsum = f_2->value(); }, value_2);
+
+    printf("Values: %i %i\n", value_1, value_2);
+
+    Kokkos::parallel_for(
+        "DestroyObjects", 1, KOKKOS_LAMBDA(const int&) {
+          f_1->~Foo();
+          f_2->~Foo();
+        });
+
+    Kokkos::kokkos_free(f_1);
+    Kokkos::kokkos_free(f_2);
+    */
+  }
+
+  Kokkos::finalize();
+}
---
+++ env_src/gpu/reproducers/virtual_function.sh
@@ -0,0 +1,10 @@
+# Using virtual functio
+KOKKOS="-I$TRUST_KOKKOS_ROOT/linux/include -L$TRUST_KOKKOS_ROOT/linux/lib64 -lkokkoscontainers -lkokkoscore" # -expt-extended-lambda -Wext-lambda-captures-this 
+if [ "$ROCM_PATH" != "" ]
+then
+   OPENMP="-fopenmp -fopenmp-targets=amdgcn-amd-amdhsa -Xopenmp-target=amdgcn-amd-amdhsa -march=$ROCM_ARCH -Wno-openmp-mapping -ldl"
+else
+   OPENMP="-fopenmp -mp -target=gpu -gpu=managed -cuda"
+fi
+echo $OPENMP
+$TRUST_CC -std=c++17 $OPENMP -o virtual_function virtual_function.cpp $KOKKOS && touch dumb.data && exec=`pwd`/virtual_function trust dumb 1
