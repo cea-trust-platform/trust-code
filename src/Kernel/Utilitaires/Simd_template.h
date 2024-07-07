41acccfbdebfe01c3e42efee6e128a3237ba73ec
Anida Khizar <anida.khizar@cea.fr>
2024-07-07 12:16:52 +0200

    [Vecto] vectorization is triggered only if Kokkos simd has been compiled

--- src/Kernel/Utilitaires/Simd_template.h
+++ src/Kernel/Utilitaires/Simd_template.h
@@ -21,7 +21,10 @@
 #include <cstdlib>
 #include <cmath>
 
+#include <kokkos++.h>
+#ifdef _KOKKOS_AVX
 #include <Kokkos_SIMD.hpp>
+#endif
 
 /*! @brief This class provides a generic access to simd operations on x86, x86 AMD and ARM architectures.
  *
@@ -38,35 +41,76 @@ class Simd_template
 {
 public:
   using value_type = _TYPE_;
-  // is a vector if Kokkos is compiled with correct SIMD options (scalar otherwise)
+#ifdef _KOKKOS_AVX
   using simd_type = Kokkos::Experimental::native_simd<_TYPE_>;
-
+#endif
   Simd_template() { data_ = 0.; };
   // Commodity default constructor (provides implicit conversion)
-  Simd_template(simd_type x) : data_(x) {};
   Simd_template(_TYPE_ x) : data_(x) {};
-
+#ifdef _KOKKOS_AVX
+  Simd_template(simd_type x) : data_(x) {};
+#endif
   // Size of the vector
   static int size()
   {
+#ifdef _KOKKOS_AVX
     return int(simd_type::size());
+#else
+    return 1;
+#endif
   }
-
   void operator+=(Simd_template a) { data_ += a.data_; }
   void operator*=(Simd_template a) { data_ *= a.data_; }
   void operator-=(Simd_template a) { data_ -= a.data_; }
 
+  _TYPE_& operator[](int idx)
+  {
+    assert(idx>=0 && idx<size());
+#ifdef _KOKKOS_AVX
+    return data_[idx];
+#else
+    return data_;
+#endif
+  }
+
+  _TYPE_ operator[](int idx) const
+  {
+    assert(idx>=0 && idx<size());
+#ifdef _KOKKOS_AVX
+    return data_[idx];
+#else
+    return data_;
+#endif
+  }
+
+  _TYPE_ sumReduction()
+  {
+#ifdef _KOKKOS_AVX
+    _TYPE_ res(0.);
+    for(int ivec = 0; ivec < size(); ivec++)
+      res += data_[ivec];
+    return res;
+#else
+    return data_;
+#endif
+  }
+
+#ifdef _KOKKOS_AVX
   simd_type data_;
+#else
+  _TYPE_ data_;
+#endif
 };
 
 using Simd_float = Simd_template<float>;
 using Simd_double = Simd_template<double>;
 using Simd_int = Simd_template<int>;
+using RightSimdArrayView = Kokkos::View<Simd_double*,Kokkos::LayoutRight>;
+using RightSimdTabView = Kokkos::View<Simd_double**,Kokkos::LayoutRight>;
 
-using ArrOfSimdDouble = Kokkos::View<Kokkos::Experimental::native_simd<double>*,Kokkos::LayoutRight>;
-using SimdDoubleTab = Kokkos::View<Kokkos::Experimental::native_simd<double>**,Kokkos::LayoutRight>;
-
+#ifdef _KOKKOS_AVX
 using tag_type = Kokkos::Experimental::element_aligned_tag;
+#endif
 
 /*! @brief returns 1 if pointer is aligned on size bytes, 0 otherwise
  *
@@ -87,9 +131,13 @@ inline int aligned(const void *ptr, int size)
 template<typename _TYPE_>
 inline Simd_template<_TYPE_> SimdGet(const _TYPE_ *data)
 {
+#ifdef _KOKKOS_AVX
   Simd_template<_TYPE_> v;
   v.data_.copy_from(data,tag_type());
   return v;
+#else
+  return *data;
+#endif
 }
 
 /*! @brief Stores vector x at address data.
@@ -100,7 +148,11 @@ inline Simd_template<_TYPE_> SimdGet(const _TYPE_ *data)
 template<typename _TYPE_>
 inline void SimdPut(_TYPE_ *data, Simd_template<_TYPE_> x)
 {
+#ifdef _KOKKOS_AVX
   x.data_.copy_to(data, tag_type());
+#else
+  *data = x.data_;
+#endif
 }
 
 /*! @brief Returns the vector x starting at adress data+1, data must be aligned for the architecture
@@ -109,9 +161,13 @@ inline void SimdPut(_TYPE_ *data, Simd_template<_TYPE_> x)
 template<typename _TYPE_>
 inline Simd_template<_TYPE_> SimdGetAtRight(const _TYPE_ *data)
 {
+#ifdef _KOKKOS_AVX
   Simd_template<_TYPE_> v;
   v.data_.copy_from(data+1,tag_type());
   return v;
+#else
+  return data[1];
+#endif
 }
 
 /*! @brief Returns the vector x starting at adress data-1 data must be aligned for the architecture
@@ -120,9 +176,13 @@ inline Simd_template<_TYPE_> SimdGetAtRight(const _TYPE_ *data)
 template<typename _TYPE_>
 inline Simd_template<_TYPE_> SimdGetAtLeft(const _TYPE_ *data)
 {
+#ifdef _KOKKOS_AVX
   Simd_template<_TYPE_> v;
   v.data_.copy_from(data-1,tag_type());
   return v;
+#else
+  return data[-1];
+#endif
 }
 
 /*! @brief Returns the vector left and center starting at adress data-1 and data data must be aligned for the architecture
@@ -131,8 +191,13 @@ inline Simd_template<_TYPE_> SimdGetAtLeft(const _TYPE_ *data)
 template<typename _TYPE_>
 inline void SimdGetLeftCenter(const _TYPE_ *data, Simd_template<_TYPE_>& left, Simd_template<_TYPE_>& center)
 {
+#ifdef _KOKKOS_AVX
   left.data_.copy_from(data-1, tag_type());
   center.data_.copy_from(data, tag_type());
+#else
+  left = data[-1];
+  center = data[0];
+#endif
 }
 
 /*! @brief Returns the vector center and right starting at adress data and data+1 data must be aligned for the architecture
@@ -143,8 +208,13 @@ inline void SimdGetCenterRight(const _TYPE_ *data,
                                Simd_template<_TYPE_>& center,
                                Simd_template<_TYPE_>& right)
 {
+#ifdef _KOKKOS_AVX
   center.data_.copy_from(data, tag_type());
   right.data_.copy_from(data+1, tag_type());
+#else
+  center = data[0];
+  right = data[1];
+#endif
 }
 
 /*! @brief Returns the vectors left, center and right starting at adress data-1, data and data+1 data must be aligned for the architecture
@@ -156,9 +226,15 @@ inline void SimdGetLeftCenterRight(const _TYPE_ *data,
                                    Simd_template<_TYPE_>& center,
                                    Simd_template<_TYPE_>& right)
 {
+#ifdef _KOKKOS_AVX
   left.data_.copy_from(data-1, tag_type());
   center.data_.copy_from(data, tag_type());
   right.data_.copy_from(data+1, tag_type());
+#else
+  left = data[-1];
+  center = data[0];
+  right = data[1];
+#endif
 }
 
 /*! @brief Returns the vectors leftleft, left, center and right starting at adress data-2, data-1, data and data+1 data must be aligned for the architecture
@@ -171,10 +247,17 @@ inline void SimdGetLeftleftLeftCenterRight(const _TYPE_ *data,
                                            Simd_template<_TYPE_>& center,
                                            Simd_template<_TYPE_>& right)
 {
+#ifdef _KOKKOS_AVX
   leftleft.data_.copy_from(data-2, tag_type());
   left.data_.copy_from(data-1, tag_type());
   center.data_.copy_from(data, tag_type());
   right.data_.copy_from(data+1, tag_type());
+#else
+  leftleft = data[-2];
+  left = data[-1];
+  center = data[0];
+  right = data[1];
+#endif
 }
 
 /*! @brief returns a+b
@@ -182,7 +265,10 @@ inline void SimdGetLeftleftLeftCenterRight(const _TYPE_ *data,
  */
 template<typename _TYPE_>
 inline Simd_template<_TYPE_> operator+(Simd_template<_TYPE_> a, Simd_template<_TYPE_> b) { return a.data_ + b.data_; }
-
+template<typename _TYPE_>
+inline Simd_template<_TYPE_> operator+(Simd_template<_TYPE_> a, double b) { return a.data_ + b; }
+template<typename _TYPE_>
+inline Simd_template<_TYPE_> operator+(double a, Simd_template<_TYPE_> b) { return a + b.data_; }
 /*! @brief returns a-b
  *
  */
@@ -214,33 +300,53 @@ inline Simd_template<_TYPE_> operator/(double a, Simd_template<_TYPE_> b) { retu
 template<typename _TYPE_>
 inline Simd_template<_TYPE_> Simd_absolute_value(Simd_template<_TYPE_> a)
 {
+#ifdef _KOKKOS_AVX
   return Kokkos::abs(a.data_);
+#else
+  return std::abs(a.data_);
+#endif
 }
 
 template<typename _TYPE_>
 inline Simd_template<_TYPE_> Simd_exp(Simd_template<_TYPE_> a)
 {
+#ifdef _KOKKOS_AVX
   return Kokkos::exp(a.data_);
+#else
+  return std::exp(a.data_);
+#endif
 }
 
 template<typename _TYPE_>
 inline Simd_template<_TYPE_> Simd_sqrt(Simd_template<_TYPE_> a)
 {
+#ifdef _KOKKOS_AVX
   return Kokkos::sqrt(a.data_);
+#else
+  return std::sqrt(a.data_);
+#endif
 }
 
 // Returns a vector built with min(a[i],b[i]) (element wise)
 template<typename _TYPE_>
 inline Simd_template<_TYPE_> SimdMin(const Simd_template<_TYPE_>& a, const Simd_template<_TYPE_>&   b)
 {
+#ifdef _KOKKOS_AVX
   return Kokkos::min(a.data_, b.data_);
+#else
+  return (a.data_ < b.data_) ? a : b;
+#endif
 }
 
 // Returns a vector built with max(a[i],b[i]) (element wise)
 template<typename _TYPE_>
 inline Simd_template<_TYPE_> SimdMax(const Simd_template<_TYPE_>& a, const Simd_template<_TYPE_>&   b)
 {
+#ifdef _KOKKOS_AVX
   return Kokkos::max(a.data_, b.data_);
+#else
+  return (a.data_ > b.data_) ? a : b;
+#endif
 }
 
 // Approximation of a/b not available with Kokkos... classic division
@@ -272,9 +378,13 @@ inline Simd_template<_TYPE_> SimdSelect(Simd_template<_TYPE_> x1,
                                         Simd_template<_TYPE_> value_if_x1_lower_than_x2,
                                         Simd_template<_TYPE_> value_otherwise)
 {
+#ifdef _KOKKOS_AVX
   Simd_template<_TYPE_> res(value_otherwise.data_);
   where(x1.data_ < x2.data_, res.data_) = value_if_x1_lower_than_x2.data_;
   return res;
+#else
+  return (x1.data_ < x2.data_) ? value_if_x1_lower_than_x2.data_ : value_otherwise.data_;
+#endif
 }
 
 #endif
