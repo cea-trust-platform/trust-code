b54fda1b56869d78afd473d02c5e117c29911408
Pierre LEDAC <pierre.ledac@cea.fr>
2024-07-25 10:26:16 +0200

    [Op_Diff_VEF_Face] Remove OMPT completly + use __KERNEL_NAME__

--- src/VEF/Operateurs/Op_Diff_Dift/Op_Diff_VEF_Face.cpp
+++ src/VEF/Operateurs/Op_Diff_Dift/Op_Diff_VEF_Face.cpp
@@ -90,7 +90,6 @@ void Op_Diff_VEF_Face::ajouter_cas_scalaire(const DoubleTab& tab_inconnue,
   DoubleTabView flux_bords = tab_flux_bords.view_rw();
   DoubleTabView resu = tab_resu.view_rw();
   // On traite les faces bord
-  start_gpu_timer();
   for (int n_bord=0; n_bord<nb_bords; n_bord++)
     {
       const Cond_lim& la_cl = domaine_Cl_VEF.les_conditions_limites(n_bord);
@@ -103,7 +102,7 @@ void Op_Diff_VEF_Face::ajouter_cas_scalaire(const DoubleTab& tab_inconnue,
         {
           const Periodique& la_cl_perio = ref_cast(Periodique,la_cl.valeur());
           CIntArrView face_associee = la_cl_perio.face_associee().view_ro();
-          Kokkos::parallel_for("Op_Diff_VEF_Face::ajouter_cas_scalaire bord periodique",
+          Kokkos::parallel_for(start_gpu_timer(__KERNEL_NAME__),
                                Kokkos::RangePolicy<>(num1, nb_faces_bord_reel), KOKKOS_LAMBDA(
                                  const int ind_face)
           {
@@ -127,13 +126,14 @@ void Op_Diff_VEF_Face::ajouter_cas_scalaire(const DoubleTab& tab_inconnue,
                   }
               }
           });
+          end_gpu_timer(Objet_U::computeOnDevice, __KERNEL_NAME__);
         }
       else   // Il n'y a qu'une seule composante, donc on traite
         // une equation scalaire (pas la vitesse) on a pas a utiliser
         // le tau tangentiel (les lois de paroi thermiques ne calculent pas
         // d'echange turbulent a la paroi pour l'instant
         {
-          Kokkos::parallel_for("Op_Diff_VEF_Face::ajouter_cas_scalaire bord quelconque",
+          Kokkos::parallel_for(start_gpu_timer(__KERNEL_NAME__),
                                Kokkos::RangePolicy<>(num1, num2), KOKKOS_LAMBDA(
                                  const int ind_face)
           {
@@ -160,13 +160,12 @@ void Op_Diff_VEF_Face::ajouter_cas_scalaire(const DoubleTab& tab_inconnue,
                   }
               }
           });
+          end_gpu_timer(Objet_U::computeOnDevice, __KERNEL_NAME__);
         }
     }
-  end_gpu_timer(Objet_U::computeOnDevice, "[KOKKOS]Op_Diff_VEF_Face::ajouter_cas_scalaire bords");
 
   // Faces internes :
-  start_gpu_timer();
-  Kokkos::parallel_for(Kokkos::RangePolicy<>(premiere_face_int, nb_faces), KOKKOS_LAMBDA (const int num_face)
+  Kokkos::parallel_for(start_gpu_timer(__KERNEL_NAME__), Kokkos::RangePolicy<>(premiere_face_int, nb_faces), KOKKOS_LAMBDA (const int num_face)
   {
     for (int k = 0; k < 2; k++)
       {
@@ -202,7 +201,7 @@ void Op_Diff_VEF_Face::ajouter_cas_scalaire(const DoubleTab& tab_inconnue,
           }
       }
   });
-  end_gpu_timer(Objet_U::computeOnDevice, "Face loop in Op_Diff_VEF_Face::ajouter");
+  end_gpu_timer(Objet_U::computeOnDevice, __KERNEL_NAME__);
   /* ToDo OpenMP use grad as calculer_cas_vectoriel
   for (int num_face=premiere_face_int; num_face<nb_faces; num_face++)
   {
@@ -224,8 +223,8 @@ void Op_Diff_VEF_Face::ajouter_cas_scalaire(const DoubleTab& tab_inconnue,
   } // Fin de la boucle sur les faces */
 
   // Neumann :
-  // copyPartialFromDevice(tab_resu, 0, premiere_face_int, "resu on boundary");
-  // copyPartialFromDevice(tab_inconnue, 0, premiere_face_int, "inconnue on boundary");
+  copyPartialFromDevice(tab_resu, 0, premiere_face_int, "resu on boundary");
+  copyPartialFromDevice(tab_inconnue, 0, premiere_face_int, "inconnue on boundary");
   start_gpu_timer();
   for (int n_bord=0; n_bord<nb_bords; n_bord++)
     {
@@ -274,7 +273,7 @@ void Op_Diff_VEF_Face::ajouter_cas_scalaire(const DoubleTab& tab_inconnue,
             tab_flux_bords(face,0) = 0.;
         }
     }
-  end_gpu_timer(0, "Boundary condition on resu in Op_Diff_VEF_Face::ajouter_cas_scalaire\n");
+  end_gpu_timer(0, __KERNEL_NAME__);
   copyPartialToDevice(tab_resu, 0, premiere_face_int, "resu on boundary");
   copyPartialToDevice(tab_inconnue, 0, premiere_face_int, "inconnue on boundary");
 }
@@ -330,100 +329,37 @@ void Op_Diff_VEF_Face::ajouter_cas_vectoriel(const DoubleTab& inconnue,
 
   int nb_faces = domaine_VEF.nb_faces();
   int nb_faces_bord = domaine_VEF.premiere_face_int();
+  CIntTabView face_voisins_v = domaine_VEF.face_voisins().view_ro();
+  CDoubleTabView face_normales_v = domaine_VEF.face_normales().view_ro();
+  CDoubleTabView nu_v = nu.view_ro();
+  CDoubleTabView3 grad_v = grad_.view3_ro();
+  DoubleTabView resu_v = resu.view_rw();
+  DoubleTabView tab_flux_bords_v = tab_flux_bords.view_rw();
 
-  if (getenv("TRUST_DISABLE_KOKKOS") != nullptr)
-    {
-      const int *face_voisins_addr = mapToDevice(domaine_VEF.face_voisins());
-      const double *face_normales_addr = mapToDevice(domaine_VEF.face_normales());
-      const double *nu_addr = mapToDevice(nu, "nu");
-      const double *grad_addr = mapToDevice(grad_, "grad");
-      //DoubleTab resu_cop = resu;
-      //DoubleTab flux_cop = tab_flux_bords;
-      double *resu_addr = computeOnTheDevice(resu, "resu");
-      double *tab_flux_bords_addr = computeOnTheDevice(tab_flux_bords, "tab_flux_bords");
-      start_gpu_timer();
-      #pragma omp target teams distribute parallel for if (computeOnDevice)
-      for (int num_face = 0; num_face < nb_faces; num_face++)
-        {
-          for (int k = 0; k < 2; k++)
-            {
-              int elem = face_voisins_addr[2 * num_face + k];
-              if (elem >= 0)
-                {
-                  int ori = 1 - 2 * k;
-                  for (int i = 0; i < nb_comp; i++)
-                    for (int j = 0; j < nb_comp; j++)
-                      {
-                        double flux = ori * face_normales_addr[num_face * nb_comp + j]
-                                      * (nu_addr[elem] * grad_addr[elem * nb_comp * nb_comp + i * nb_comp +
-                                                                   j] /* + Re(elem, i, j) */ );
-                        #pragma omp atomic
-                        resu_addr[num_face * nb_comp + i] -= flux;
-                        if (num_face < nb_faces_bord)
-                          #pragma omp atomic
-                          tab_flux_bords_addr[num_face * nb_comp + i] -= flux;
-                      }
-                }
-            } // Fin de la boucle sur les 2 elements comnuns a la face
-        } // Fin de la boucle sur les faces
-      end_gpu_timer(Objet_U::computeOnDevice, "Face loop in Op_Diff_VEF_Face::ajouter");
-    }
-  else
-    {
-      CIntTabView face_voisins_v = domaine_VEF.face_voisins().view_ro();
-      CDoubleTabView face_normales_v = domaine_VEF.face_normales().view_ro();
-      CDoubleTabView nu_v = nu.view_ro();
-      CDoubleTabView3 grad_v = grad_.view3_ro();
-      DoubleTabView resu_v = resu.view_rw();
-      DoubleTabView tab_flux_bords_v = tab_flux_bords.view_rw();
-
-      auto kern_ajouter = KOKKOS_LAMBDA(int
-                                        num_face)
+  auto kern_ajouter = KOKKOS_LAMBDA(int
+                                    num_face)
+  {
+    for (int k = 0; k < 2; k++)
       {
-        for (int k = 0; k < 2; k++)
+        int elem = face_voisins_v(num_face, k);
+        if (elem >= 0)
           {
-            int elem = face_voisins_v(num_face, k);
-            if (elem >= 0)
-              {
-                int ori = 1 - 2 * k;
-                for (int i = 0; i < nb_comp; i++)
-                  for (int j = 0; j < nb_comp; j++)
-                    {
-                      double flux = ori * face_normales_v(num_face, j)
-                                    * (nu_v(elem, 0) * grad_v(elem, i, j)  /* + Re(elem, i, j) */ );
-                      Kokkos::atomic_sub(&resu_v(num_face, i), flux);
-                      if (num_face < nb_faces_bord)
-                        Kokkos::atomic_sub(&tab_flux_bords_v(num_face, i), flux);
-                    }
-              }
+            int ori = 1 - 2 * k;
+            for (int i = 0; i < nb_comp; i++)
+              for (int j = 0; j < nb_comp; j++)
+                {
+                  double flux = ori * face_normales_v(num_face, j)
+                                * (nu_v(elem, 0) * grad_v(elem, i, j)  /* + Re(elem, i, j) */ );
+                  Kokkos::atomic_sub(&resu_v(num_face, i), flux);
+                  if (num_face < nb_faces_bord)
+                    Kokkos::atomic_sub(&tab_flux_bords_v(num_face, i), flux);
+                }
           }
-      };
+      }
+  };
 
-//  auto kern_ajouter2 = KOKKOS_LAMBDA(int num_face)
-//  {
-//    for (int k=0; k<2; k++)
-//      {
-//        int elem = face_voisins_v(num_face, k);
-//        if (elem>=0)
-//          {
-//            int ori = 1 - 2 * k;
-//            for (int i = 0; i < nb_comp; i++)
-//              for (int j = 0; j < nb_comp; j++)
-//                {
-//                  double flux = ori * face_normales_v(num_face,j)
-//                                * (nu_v(elem, 0) * grad_v(elem, i,j)  /* + Re(elem, i, j) */ );
-//                  Kokkos::atomic_sub(&resu_v(num_face, i), flux);
-//                }
-//          }
-//      }
-//  };
-
-      start_gpu_timer();
-      Kokkos::parallel_for("[KOKKOS] Face loop in Op_Diff_VEF_Face::ajouter", nb_faces, kern_ajouter);
-//  Kokkos::parallel_for("[KOKKOS] Face loop in Op_Diff_VEF_Face::ajouter 2",
-//                       Kokkos::RangePolicy<>(nb_faces_bord, nb_faces), kern_ajouter2);
-      end_gpu_timer(Objet_U::computeOnDevice, "[KOKKOS] Face loop in Op_Diff_VEF_Face::ajouter");
-    }
+  Kokkos::parallel_for(start_gpu_timer(__KERNEL_NAME__), nb_faces, kern_ajouter);
+  end_gpu_timer(Objet_U::computeOnDevice, __KERNEL_NAME__);
 
   // Update flux_bords on symmetry:
   const int nb_bords=domaine_VEF.nb_front_Cl();
@@ -886,10 +822,9 @@ void Op_Diff_VEF_Face::ajouter_contribution(const DoubleTab& tab_transporte, Mat
       }
   };
 
-  start_gpu_timer();
-  Kokkos::parallel_for("Op_Diff_VEF_Face::ajouter_contribution",
-                       domaine_VEF.nb_faces(), ajouter_contrib);
-  end_gpu_timer(Objet_U::computeOnDevice, "[KOKKOS] Op_Diff_VEF_Face::ajouter_contribution");
+
+  Kokkos::parallel_for(start_gpu_timer(__KERNEL_NAME__),domaine_VEF.nb_faces(), ajouter_contrib);
+  end_gpu_timer(Objet_U::computeOnDevice, __KERNEL_NAME__);
 
   int premiere_face_int = domaine_VEF.premiere_face_int();
   DoubleVect tab_h_impose(premiere_face_int);
@@ -956,9 +891,8 @@ void Op_Diff_VEF_Face::ajouter_contribution(const DoubleTab& tab_transporte, Mat
     matrice(face,face) += (h + dphi_dT) * face_surfaces(face);
   };
 
-  start_gpu_timer();
-  Kokkos::parallel_for("Op_Diff_VEF_Face::ajouter_contribution (Neumann)", premiere_face_int, neumann);
-  end_gpu_timer(Objet_U::computeOnDevice, "[KOKKOS] Op_Diff_VEF_Face::ajouter_contribution (Neumann)");
+  Kokkos::parallel_for(start_gpu_timer(__KERNEL_NAME__), premiere_face_int, neumann);
+  end_gpu_timer(Objet_U::computeOnDevice, __KERNEL_NAME__);
 
   modifier_matrice_pour_periodique_apres_contribuer(tab_matrice,equation());
 }
@@ -1162,9 +1096,8 @@ void Op_Diff_VEF_Face::ajouter_contribution_multi_scalaire(const DoubleTab& tab_
       }
   };
 
-  start_gpu_timer();
-  Kokkos::parallel_for("Op_Diff_VEF_Face::ajouter_contribution_multi_scalaire", nb_faces_tot, kern_elem_faces);
-  end_gpu_timer(Objet_U::computeOnDevice, "[KOKKOS]Op_Diff_VEF_Face::ajouter_contribution_multi_scalaire");
+  Kokkos::parallel_for(start_gpu_timer(__KERNEL_NAME__), nb_faces_tot, kern_elem_faces);
+  end_gpu_timer(Objet_U::computeOnDevice, __KERNEL_NAME__);
 
   modifier_matrice_pour_periodique_apres_contribuer(tab_matrice, equation());
 }
