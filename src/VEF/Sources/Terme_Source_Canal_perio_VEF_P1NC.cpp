dccd7a4f0b8d992a2c4d646379bcf1a58416df6a
Pierre LEDAC <pierre.ledac@cea.fr>
2024-08-11 16:28:57 +0200

    [Kokkos] Complete port of Canal_perio term source in VEF but weird bug with Kokkos reduction...

--- src/VEF/Sources/Terme_Source_Canal_perio_VEF_P1NC.cpp
+++ src/VEF/Sources/Terme_Source_Canal_perio_VEF_P1NC.cpp
@@ -70,60 +70,56 @@ void Terme_Source_Canal_perio_VEF_P1NC::associer_domaines(const Domaine_dis& dom
   le_dom_Cl_VEF = ref_cast(Domaine_Cl_VEF, domaine_Cl_dis.valeur());
 }
 
-DoubleTab& Terme_Source_Canal_perio_VEF_P1NC::ajouter(DoubleTab& resu) const
+DoubleTab& Terme_Source_Canal_perio_VEF_P1NC::ajouter(DoubleTab& tab_resu) const
 {
   const Domaine_VF& domaine_VF = le_dom_VEF.valeur();
-  const DoubleVect& volumes_entrelaces = domaine_VF.volumes_entrelaces();
-  const DoubleVect& volumes_entrelaces_Cl = le_dom_Cl_VEF->volumes_entrelaces_Cl();
-  int premiere_face_std=le_dom_VEF->premiere_face_std() ;
-  const DoubleVect& porosite_face=equation().milieu().porosite_face();
-  ArrOfDouble s;
-  s = source();
+  int premiere_face_std = le_dom_VEF->premiere_face_std() ;
+  ArrOfDouble tab_s;
+  tab_s = source();
   int nb_faces = domaine_VF.nb_faces();
-  if (s.size_array()==dimension)
+  int dim = Objet_U::dimension;
+  CDoubleArrView volumes_entrelaces_Cl = le_dom_Cl_VEF->volumes_entrelaces_Cl().view_ro();
+  CDoubleArrView volumes_entrelaces = domaine_VF.volumes_entrelaces().view_ro();
+  CDoubleArrView porosite_face = equation().milieu().porosite_face().view_ro();
+  CDoubleArrView s = tab_s.view_ro();
+  if (tab_s.size_array()==dim)
     {
       // Case Navier Stokes, s is uniform
-      const double* s_addr = mapToDevice(s);
-      const double* volumes_entrelaces_Cl_addr = mapToDevice(volumes_entrelaces_Cl);
-      const double* volumes_entrelaces_addr = mapToDevice(volumes_entrelaces);
-      const double* porosite_face_addr = mapToDevice(porosite_face);
-      double* resu_addr = computeOnTheDevice(resu);
-      start_gpu_timer();
-      #pragma omp target teams distribute parallel for if (computeOnDevice)
-      for (int num_face = 0; num_face<nb_faces; num_face++)
-        {
-          double vol = (num_face<premiere_face_std ? volumes_entrelaces_Cl_addr[num_face] : volumes_entrelaces_addr[num_face]) * porosite_face_addr[num_face];
-          for (int i=0; i<dimension; i++)
-            resu_addr[num_face*dimension+i]+= s_addr[i]*vol;
-        }
-      end_gpu_timer(Objet_U::computeOnDevice, "Face loop in Terme_Source_Canal_perio_VEF_P1NC::ajouter");
+      DoubleTabView resu = tab_resu.view_rw();
+      Kokkos::parallel_for(start_gpu_timer(__KERNEL_NAME__), nb_faces, KOKKOS_LAMBDA(
+                             const int num_face)
+      {
+        double vol = (num_face<premiere_face_std ? volumes_entrelaces_Cl(num_face) : volumes_entrelaces(num_face))*porosite_face(num_face);
+        for (int i=0; i<dim; i++)
+          resu(num_face,i) += s(i)*vol;
+      });
+      end_gpu_timer(Objet_U::computeOnDevice, __KERNEL_NAME__);
     }
   else
     {
       // Case Energy, s is non uniform
-      ToDo_Kokkos("");
       bilan_=0;
-      const ArrOfInt& fd=domaine_VF.faces_doubles();
-      for (int num_face = 0; num_face<nb_faces; num_face++)
-        {
-          double vol = (num_face < premiere_face_std ? volumes_entrelaces_Cl(num_face) : volumes_entrelaces(num_face))*porosite_face(num_face);
-          double contrib = s[num_face]*vol;
-          resu(num_face)+= contrib;
-          bilan_(0)+= contrib*(1-0.5*fd[num_face]);
-        }
+      CIntArrView fd = domaine_VF.faces_doubles().view_ro();
+      DoubleArrView bilan = bilan_.view_rw();
+      DoubleArrView resu = static_cast<DoubleVect&>(tab_resu).view_rw();
+      Kokkos::parallel_for(start_gpu_timer(__KERNEL_NAME__), nb_faces, KOKKOS_LAMBDA(
+                             const int num_face)
+      {
+        double vol = (num_face < premiere_face_std ? volumes_entrelaces_Cl(num_face) : volumes_entrelaces(num_face))*porosite_face(num_face);
+        double contrib = s[num_face]*vol;
+        resu(num_face)+= contrib;
+        Kokkos::atomic_add(&bilan(0), contrib*(1-0.5*fd[num_face]));
+      });
+      end_gpu_timer(Objet_U::computeOnDevice, __KERNEL_NAME__);
     }
-  return resu;
+  return tab_resu;
 }
 
 void Terme_Source_Canal_perio_VEF_P1NC::calculer_debit(double& debit_e) const
 {
+  debit_e = 0.;
   const Domaine_VF& domaine_VF = le_dom_VEF.valeur();
   const Domaine_Cl_dis_base& domaine_Cl_dis = le_dom_Cl_VEF.valeur();
-  const Champ_Inc_base& velocity = (sub_type(Convection_Diffusion_std,equation()) ? ref_cast(Convection_Diffusion_std,equation()).vitesse_transportante() : equation().inconnue());
-  const DoubleVect& porosite_face=equation().milieu().porosite_face();
-  // Check we have really velocity:
-  assert(velocity.le_nom()=="vitesse");
-  const DoubleTab& vitesse = velocity.valeurs();
   int nb_bords = domaine_VF.nb_front_Cl();
   for (int n_bord=0; n_bord<nb_bords; n_bord++)
     {
@@ -133,51 +129,74 @@ void Terme_Source_Canal_perio_VEF_P1NC::calculer_debit(double& debit_e) const
           const Front_VF& le_bord = ref_cast(Front_VF,la_cl.frontiere_dis());
           if (bord_periodique_ == le_bord.le_nom())
             {
-              debit_e=0.;
-              //const Front_VF& le_bord = ref_cast(Front_VF,la_cl.frontiere_dis());
+              const Champ_Inc_base& velocity = (sub_type(Convection_Diffusion_std,equation()) ? ref_cast(Convection_Diffusion_std,equation()).vitesse_transportante() : equation().inconnue());
+              // Check we have really velocity:
+              assert(velocity.le_nom()=="vitesse");
               int ndeb = le_bord.num_premiere_face();
               int nfin = ndeb + le_bord.nb_faces()/2;
               int axe = direction_ecoulement_;
-              if (equation().probleme().is_dilatable()==1)
+              int dim = Objet_U::dimension;
+              bool is_dilatable = equation().probleme().is_dilatable();
+              // Si l'on est en Quasi Compressible, il faut conserver le debit massique et non pas le debit volumique.
+              // C'est pour cela que dans le cas QC, on multiplie les vecteurs vitesse
+              // par la masse volumique discretisee aux faces pour que lorsqu'on integre sur la surface,
+              // on obtienne bien un debit massique et non pas un debit volumique.
+              const DoubleTab& tab_rho_face = is_dilatable ? ref_cast(Fluide_Dilatable_base,equation().milieu()).rho_discvit() : velocity.valeurs() /* tableau de meme format mais non utilise */;
+              if (getenv("TRUST_KOKKOS_REDUCTION_WEIRD"))
                 {
-                  // Si l'on est en Quasi Compressible, il faut conserver
-                  // le debit massique et non pas le debit volumique.
-                  // C'est pour cela que dans le cas QC, on multiplie les vecteurs vitesse
-                  // par la masse volumique discretisee aux faces pour que lorsqu'on integre sur la surface,
-                  // on obtienne bien un debit massique et non pas un debit volumique.
-                  const DoubleTab& tab_rho_face = ref_cast(Fluide_Dilatable_base,equation().milieu()).rho_discvit();
-
-                  if (axe>=0)
-                    for (int num_face=ndeb; num_face<nfin; num_face++)
+                  ToDo_Kokkos("critical reduction mais ecarts en optimise pas en debug, pourquoi?");
+                  CDoubleArrView rho_face = static_cast<const DoubleVect&>(tab_rho_face).view_ro();
+                  CDoubleTabView face_normales = domaine_VF.face_normales().view_ro();
+                  CDoubleArrView porosite_face = equation().milieu().porosite_face().view_ro();
+                  CDoubleTabView vitesse = velocity.valeurs().view_ro();
+                  Kokkos::parallel_reduce(start_gpu_timer(__KERNEL_NAME__),
+                                          Kokkos::RangePolicy<>(ndeb, nfin), KOKKOS_LAMBDA(
+                                            const int num_face, double& debit)
+                  {
+                    double debit_face = 0;
+                    if (axe >= 0)
+                      debit_face += porosite_face(num_face) * vitesse(num_face, axe) *
+                                    std::fabs(face_normales(num_face, axe));
+                    else
                       {
-                        double debit_face = porosite_face(num_face)*vitesse(num_face,axe) * std::fabs(domaine_VF.face_normales(num_face,axe));
-                        debit_e += tab_rho_face[num_face] * debit_face;
+                        for (int i = 0; i < dim; i++)
+                          debit_face += porosite_face(num_face) * vitesse(num_face, i) *
+                                        face_normales(num_face, i);
                       }
-                  else
-                    for (int num_face=ndeb; num_face<nfin; num_face++)
-                      {
-                        double debit_face=0;
-                        for (int i=0; i<dimension; i++)
-                          debit_face += porosite_face(num_face)*vitesse(num_face,i) * domaine_VF.face_normales(num_face,i);
-                        debit_e += tab_rho_face[num_face] * debit_face;
-                      }
+                    debit += (is_dilatable ? rho_face(num_face) : 1) * debit_face;
+                    //printf("Provisoire %d %f \n",num_face,debit_face);
+                  }, Kokkos::Sum<double>(debit_e));
+                  end_gpu_timer(Objet_U::computeOnDevice, __KERNEL_NAME__);
+                  //Cerr << "Provisoire GPU debit_e=" << debit_e << finl;
                 }
               else
                 {
-                  if (axe>=0)
-                    for (int num_face=ndeb; num_face<nfin; num_face++)
-                      {
-                        double debit_face = porosite_face(num_face)*vitesse(num_face,axe) * std::fabs(domaine_VF.face_normales(num_face,axe));
-                        debit_e += debit_face;
-                      }
-                  else
-                    for (int num_face=ndeb; num_face<nfin; num_face++)
-                      {
-                        double debit_face=0;
-                        for (int i=0; i<dimension; i++)
-                          debit_face += porosite_face(num_face)*vitesse(num_face,i) * domaine_VF.face_normales(num_face,i);
-                        debit_e += debit_face;
-                      }
+                  // PL: On utilise des copies partielles en attendant...
+                  const DoubleTab& face_normales = domaine_VF.face_normales();
+                  const DoubleVect& porosite_face = equation().milieu().porosite_face();
+                  const DoubleTab& vitesse = velocity.valeurs();
+                  copyPartialFromDevice(face_normales, ndeb*dimension, nfin*dimension, "face_normales on boundary");
+                  copyPartialFromDevice(porosite_face, ndeb, nfin, "porosite_face on boundary");
+                  copyPartialFromDevice(tab_rho_face, ndeb, nfin, "tab_rho_face on boundary");
+                  copyPartialFromDevice(vitesse,       ndeb*dimension, nfin*dimension, "vitesse on boundary");
+                  for (int num_face = ndeb; num_face < nfin; num_face++)
+                    {
+                      double debit_face = 0;
+                      if (axe >= 0)
+                        debit_face += porosite_face(num_face) * vitesse(num_face, axe) *
+                                      std::fabs(face_normales(num_face, axe));
+                      else
+                        {
+                          for (int i = 0; i < dim; i++)
+                            debit_face += porosite_face(num_face) * vitesse(num_face, i) * face_normales(num_face, i);
+                        }
+                      debit_e += (is_dilatable ? tab_rho_face(num_face) : 1) * debit_face;
+                    }
+                  copyPartialToDevice(face_normales, ndeb*dimension, nfin*dimension, "face_normales on boundary");
+                  copyPartialToDevice(porosite_face, ndeb, nfin, "porosite_face on boundary");
+                  copyPartialToDevice(tab_rho_face, ndeb, nfin, "tab_rho_face on boundary");
+                  copyPartialToDevice(vitesse,       ndeb*dimension, nfin*dimension, "vitesse on boundary");
+                  //Cerr << "Provisoire CPU debit_e=" << debit_e << finl;
                 }
             }
         }
