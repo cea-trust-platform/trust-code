d17d86e93b47b9a83e354e55b2a011704e99b165
KRAEMER Valentin 278269 <vk278269@is245203.intra.cea.fr>
2024-08-21 14:01:24 +0200

    modification des Robin cl pour la lecture du jeu de donnees

--- src/Kernel/Cond_Lim/Robin_VEF.cpp
+++ src/Kernel/Cond_Lim/Robin_VEF.cpp
@@ -15,22 +15,48 @@
 /////////////////////////////////////////////////////////////////////////////
 //
 // File      : Robin_VEF.cpp
-// Directory : $NEW_DEV_ROOT/src/my_module
+// Directory : $TRUST_ROOT/src/Kernel/Cond_Lim
 //
 /////////////////////////////////////////////////////////////////////////////
 
-#include <Robin_VEF.h>
 
-Implemente_instanciable( Robin_VEF, "Robin_VEF", Objet_U ) ;
+#include <Robin_VEF.h>
+#include <Param.h>
+
+Implemente_instanciable( Robin_VEF, "Robin_VEF", Cond_lim_base ) ;
 
 Sortie& Robin_VEF::printOn( Sortie& os ) const
 {
-  Objet_U::printOn( os );
+  Cond_lim_base::printOn( os );
   return os;
 }
 
 Entree& Robin_VEF::readOn( Entree& is )
 {
-  Objet_U::readOn( is );
+  Param param(que_suis_je());
+  // reading Robin border conditions parameters
+
+  param.ajouter("alpha", 		&alpha_robin_cl_, 		Param::REQUIRED);
+  param.ajouter("beta" , 		&beta_robin_cl_ , 		Param::REQUIRED);
+  param.ajouter("champ_front_normal_et_tangentiel_robin",&le_champ_front,Param::REQUIRED); // Champ_front_tangentiel_robin::readOn(is);
+  param.lire_avec_accolades_depuis(is);
+
+
+  Cerr << "Reading Robin boundary condition, the value of the normal Robin coefficient is  "
+       << alpha_robin_cl_ << " and the tangential Robin coefficient is  " << beta_robin_cl_ << finl;
+
+  if (alpha_robin_cl_==0 || beta_robin_cl_==0)
+    {
+      Cerr << "Error of Robin boundary conditions : Alpha and Beta parameters should not be zero  " << finl;
+      exit() ;
+    }
+
+
   return is;
+}
+
+int Robin_VEF::compatible_avec_eqn( const Equation_base& eqn) const
+{
+  // TOD : if we need to do some verification ?
+  return 1;
 }
--- src/Kernel/Cond_Lim/Robin_VEF.h
+++ src/Kernel/Cond_Lim/Robin_VEF.h
@@ -15,32 +15,65 @@
 /////////////////////////////////////////////////////////////////////////////
 //
 // File      : Robin_VEF.h
-// Directory : $NEW_DEV_ROOT/src/my_module
+// Directory : $TRUST_ROOT/src/Kernel/Cond_Lim
 //
 /////////////////////////////////////////////////////////////////////////////
-
-#ifndef Robin_VEF_included
-#define Robin_VEF_included
-
-#include <Objet_U.h>
-
 /////////////////////////////////////////////////////////////////////////////
 //
 // .DESCRIPTION : class Robin_VEF
 //
 // <Description of class Robin_VEF>
 //
+// g = \alpha( \nabla(u)n.n - p ) + u.n
+// xi = \beta(\nabla(u)n\times n) + u \times n
 /////////////////////////////////////////////////////////////////////////////
 
-class Robin_VEF : public Objet_U
+#ifndef Robin_VEF_included
+#define Robin_VEF_included
+
+#include <Cond_lim_base.h>
+
+/*! @brief Class Robin_VEF for Robin boundary condititions
+ *
+ *    Robin boundary conditions functions are set as it follows :
+ *      g = \alpha( \nabla(u)n.n - p ) + u.n
+ *      xi = \beta(\nabla(u)n\times n) + u \times n
+ *
+ *      The parameters \alpha, \beta, and the functions g and xi comes from the data file e.g.
+ *
+ *      Conditions_limites {
+ *                          Robin_boudary_name {
+ *                                                  alpha val
+ *                                                  beta val
+ *                                                  champ_front_normal_et_tangentiel_robin champ_front_fonc_xyz 4 (in 3D, 2 in 2D) normal_field1D, tangential_field_X, tangential_field_Y, tangential_field_Z (tangential_fiel_1D for 2D)
+ *                                              }
+ *                          }
+ *
+ * @sa Cond_lim_base Robin_VEF
+ */
+
+class Robin_VEF : public Cond_lim_base
 {
 
   Declare_instanciable( Robin_VEF ) ;
 
 public :
+  int compatible_avec_eqn(const Equation_base&) const override ;
+  inline  double get_alpha_cl() const { return alpha_robin_cl_; } ;
+  inline double get_beta_cl() const { return beta_robin_cl_; };
+// void test() const {Cerr<<"Mdr"<<finl;};
+  // double val_imp(int i) const;
+  // double val_imp(int i, int j) const ;
+  // double val_imp_au_temps(double temps, int i) const ;
+  // double val_imp_au_temps(double temps, int i, int j ) const;
 
 protected :
 
+
+  double alpha_robin_cl_  ;
+  double beta_robin_cl_   ;
+  // Champ_front_normal_robin champ_normal_robin_cl_;
+  // Champ_front_tangentiel_robin champ_tangent_robin_cl_;
 };
 
 #endif /* Robin_VEF_included */
