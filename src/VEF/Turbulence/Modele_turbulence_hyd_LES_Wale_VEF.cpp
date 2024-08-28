905ad480116ca00efad4231fe77b61fb97661ff3
Victor Rodriguez <victor.rodriguez@cea.fr>
2024-08-28 13:26:27 +0200

    [Modele_turbulence_hyd_LES_Wale_VEF] calculer_viscosite_turbulente(): rewrite OpenMP kernel in Kokkos

--- src/VEF/Turbulence/Modele_turbulence_hyd_LES_Wale_VEF.cpp
+++ src/VEF/Turbulence/Modele_turbulence_hyd_LES_Wale_VEF.cpp
@@ -44,90 +44,93 @@ Champ_Fonc_base& Modele_turbulence_hyd_LES_Wale_VEF::calculer_viscosite_turbulen
 {
   const Domaine_VEF& domaine_VEF = ref_cast(Domaine_VEF, le_dom_VF_.valeur());
   double temps = mon_equation_->inconnue().temps();
-  DoubleTab& visco_turb = la_viscosite_turbulente_->valeurs();
+  DoubleTab& tab_visco_turb = la_viscosite_turbulente_->valeurs();
   const int nb_elem = domaine_VEF.nb_elem();
   const DoubleTab& la_vitesse = mon_equation_->inconnue().valeurs();
   const Domaine_Cl_VEF& domaine_Cl_VEF = ref_cast(Domaine_Cl_VEF, le_dom_Cl_.valeur());
 
-  if (visco_turb.size() != nb_elem)
+  if (tab_visco_turb.size() != nb_elem)
     {
       Cerr << "Size error for the array containing the values of the turbulent viscosity." << finl;
       exit();
     }
-  Debog::verifier("Modele_turbulence_hyd_LES_Wale_VEF::calculer_viscosite_turbulente visco_turb 0", visco_turb);
+  Debog::verifier("Modele_turbulence_hyd_LES_Wale_VEF::calculer_viscosite_turbulente visco_turb 0", tab_visco_turb);
 
   const int nb_elem_tot = domaine_VEF.nb_elem_tot();
-  DoubleTrav duidxj(nb_elem_tot,dimension,dimension);
+  DoubleTrav tab_duidxj(nb_elem_tot,dimension,dimension);
   // Patrick : on travaille sur le champ filtre.
   //const Champ_P1NC& ch=(const Champ_P1NC&) mon_equation->inconnue();
   //DoubleTab ubar(la_vitesse);
   //ch.filtrer_L2(ubar);
   //Champ_P1NC::calcul_gradient(ubar,duidxj,domaine_Cl_VEF);
-  Champ_P1NC::calcul_gradient(la_vitesse,duidxj,domaine_Cl_VEF);
+  Champ_P1NC::calcul_gradient(la_vitesse,tab_duidxj,domaine_Cl_VEF);
 
-  // ToDo_Kokkos
-  const double* l_addr = mapToDevice(l_);
-  const double* duidxj_addr = mapToDevice(duidxj);
-  double* visco_turb_addr = computeOnTheDevice(visco_turb);
-  start_gpu_timer();
-  #pragma omp target teams
+  double cw = cw_; // Necessary to avoid kernel crash
+  int dimension = Objet_U::dimension;
+
+  CDoubleArrView l = l_.view_ro();
+  CDoubleTabView3 duidxj = tab_duidxj.view3_ro();
+  CDoubleArrView visco_turb = static_cast<DoubleVect&>(tab_visco_turb).view_rw();
+
+  start_gpu_timer(__KERNEL_NAME__);
+  Kokkos::parallel_for(
+    __KERNEL_NAME__,
+    nb_elem,
+    KOKKOS_LAMBDA (int elem)
   {
-    double gij2[3][3] { };
-    double sd[3][3] { };
-    #pragma omp distribute parallel for private(gij2, sd)
-    for (int elem = 0; elem < nb_elem; elem++)
-      {
-        // Calcul du terme gij2.
-        for (int i = 0; i < dimension; i++)
-          for (int j = 0; j < dimension; j++)
-            {
-              gij2[i][j] = 0;
-              for (int k = 0; k < dimension; k++)
-                gij2[i][j] += duidxj_addr[elem * dimension * dimension + i * dimension + k] * duidxj_addr[elem * dimension * dimension + k * dimension + j];
-            }
+    double gij2[3][3] {};
+    double sd[3][3] {};
 
-        // Calcul du terme gkk2.
-        double gkk2 = 0;
-        for (int k = 0; k < dimension; k++)
-          gkk2 += gij2[k][k];
+    // Calcul du terme gij2.
+    for (int i = 0; i < dimension; i++)
+      for (int j = 0; j < dimension; j++)
+        {
+          gij2[i][j] = 0;
+          for (int k = 0; k < dimension; k++)
+            gij2[i][j] += duidxj(elem,i,k) * duidxj(elem,k,j);
+        }
 
-        // Calcul de sd.
-        for (int i = 0; i < dimension; i++)
-          for (int j = 0; j < dimension; j++)
-            {
-              sd[i][j] = 0.5 * (gij2[i][j] + gij2[j][i]);
-              if (i == j)
-                sd[i][j] -= gkk2 / 3.; // Terme derriere le tenseur de Kronecker.
-            }
+    // Calcul du terme gkk2.
+    double gkk2 = 0;
+    for (int k = 0; k < dimension; k++)
+      gkk2 += gij2[k][k];
 
-        // Calcul de sd2 et Sij2.
-        double sd2 = 0.;
-        double Sij2 = 0.;
-        for (int i = 0; i < dimension; i++)
-          for (int j = 0; j < dimension; j++)
-            {
-              sd2 += sd[i][j] * sd[i][j];
-              //Deplacement du calcul de Sij
-              double Sij = 0.5 * (duidxj_addr[elem * dimension * dimension + i * dimension + j] + duidxj_addr[elem * dimension * dimension + j * dimension + i]);
-              Sij2 += Sij * Sij;
-            }
+    // Calcul de sd.
+    for (int i = 0; i < dimension; i++)
+      for (int j = 0; j < dimension; j++)
+        {
+          sd[i][j] = 0.5 * (gij2[i][j] + gij2[j][i]);
+          if (i == j)
+            sd[i][j] -= gkk2 / 3.; // Terme derriere le tenseur de Kronecker.
+        }
 
-        // Calcul de OP1 et OP2.
-        // Replace pow by sqrt and multiply, faster
-        //OP1=pow(sd2,1.5);
-        double OP1 = sd2 * sqrt(sd2);
-        //OP2=pow(Sij2,2.5)+pow(sd2,1.25);
-        double OP2 = Sij2 * Sij2 * sqrt(Sij2) + sd2 * sqrt(sqrt(sd2));
+    // Calcul de sd2 et Sij2.
+    double sd2 = 0.;
+    double Sij2 = 0.;
+    for (int i = 0; i < dimension; i++)
+      for (int j = 0; j < dimension; j++)
+        {
+          sd2 += sd[i][j] * sd[i][j];
+          //Deplacement du calcul de Sij
+          double Sij = 0.5 * (duidxj(elem,i,j) + duidxj(elem,j,i));
+          Sij2 += Sij * Sij;
+        }
 
-        if (OP1 != 0.) // donc sd2 et OP2 par voie de consequence sont differents de zero
-          visco_turb_addr[elem] = cw_ * cw_ * l_addr[elem] * l_addr[elem] * OP1 / OP2;
-        else
-          visco_turb_addr[elem] = 0;
-      } // fin de la boucle sur les elements
-  }
+    // Calcul de OP1 et OP2.
+    // Replace pow by sqrt and multiply, faster
+    //OP1=pow(sd2,1.5);
+    double OP1 = sd2 * sqrt(sd2);
+    //OP2=pow(Sij2,2.5)+pow(sd2,1.25);
+    double OP2 = Sij2 * Sij2 * sqrt(Sij2) + sd2 * sqrt(sqrt(sd2));
+
+    if (OP1 != 0.) // donc sd2 et OP2 par voie de consequence sont differents de zero
+      visco_turb(elem) = cw * cw * l(elem) * l(elem) * OP1 / OP2;
+    else
+      visco_turb(elem) = 0;
+  }); // fin de la boucle sur les elements
   end_gpu_timer(Objet_U::computeOnDevice, __KERNEL_NAME__);
 
-  Debog::verifier("Modele_turbulence_hyd_LES_Wale_VEF::calculer_viscosite_turbulente visco_turb 1", visco_turb);
+  Debog::verifier("Modele_turbulence_hyd_LES_Wale_VEF::calculer_viscosite_turbulente visco_turb 1", tab_visco_turb);
 
   la_viscosite_turbulente_->changer_temps(temps);
   return la_viscosite_turbulente_;
