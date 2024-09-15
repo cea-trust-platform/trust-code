8ac65dba4d5b6744b02eb6aaec466469bbdaff44
Pierre LEDAC <pierre.ledac@cea.fr>
2024-09-15 16:49:12 +0200

    [MPI_GPU_AWARE] Improve reproducer

--- env_src/gpu/reproducers/gpu_direct.cpp
+++ env_src/gpu/reproducers/gpu_direct.cpp
@@ -5,8 +5,11 @@
 #include <cuda_runtime.h>
 
 int main(int argc, char** argv) {
+    int world_size, world_rank;
+    
     if (getenv("SLURM_LOCALID")!=NULL)
     {
+       // Cluster multi-gpu
        char* local_rank_env = getenv("SLURM_LOCALID");
        int rank = atoi(local_rank_env);
        cudaError_t cudaRet = cudaSetDevice(rank);
@@ -16,30 +19,53 @@ int main(int argc, char** argv) {
           abort();
          }
        else
-        std::cout << "Set MPI rank " << rank << " to device " << rank << std::endl;
+          std::cout << "Set MPI rank " << rank << " to device " << rank << std::endl;
+	  
+       MPI_Init(NULL, NULL);
+       MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+       MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);  
     }
     else
     {
-       std::cout << "No binding between MPI ranks and device(s)" << std::endl;
+       // PC mono-gpu
+
+       MPI_Init(NULL, NULL);
+       MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+       MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+
+       char* local_rank_env = getenv("OMPI_COMM_WORLD_RANK");
+       int rank = atoi(local_rank_env);
+       int device = 0;
+       cudaError_t cudaRet = cudaSetDevice(device);
+       if(cudaRet != cudaSuccess)
+         {
+          printf("Error: cudaSetDevice failed\n");
+          abort();
+         }
+       else
+          std::cout << "Set MPI rank " << rank << " to device " << device << std::endl;    
     }
-    MPI_Init(NULL, NULL);
-
-    int world_size;
-    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
-
-    int world_rank;
-    MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
-
+    
     const int N = 1000000;  // Adjust this based on the size of data you're sending
-    std::vector<double> data(N, 1.0f);  // Initialize with some data
+    std::vector<double> array(N, 1.0f);  // Initialize with some data
+    double* host_data = array.data();
+    // Initialize data on device:
+    double* device_data;
+    cudaMalloc((void**)&device_data, N * sizeof(double));
+    cudaMemcpy(device_data, host_data, N * sizeof(double), cudaMemcpyHostToDevice);
+    cudaDeviceSynchronize();
+
 
     MPI_Barrier(MPI_COMM_WORLD); // Make sure all processes are ready before starting the clock
     auto start = std::chrono::high_resolution_clock::now();
     if (world_rank == 0) {
-        MPI_Send(data.data(), N, MPI_DOUBLE, 1, 0, MPI_COMM_WORLD);
+        cudaMemcpy(host_data, device_data, N, cudaMemcpyDeviceToHost);
+        MPI_Send(host_data, N, MPI_DOUBLE, 1, 0, MPI_COMM_WORLD);
     } else if (world_rank == 1) {
-        MPI_Recv(data.data(), N, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        MPI_Recv(host_data, N, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+	cudaMemcpy(device_data, host_data, N, cudaMemcpyHostToDevice);
     }
+    MPI_Barrier(MPI_COMM_WORLD);
     auto end = std::chrono::high_resolution_clock::now();
     std::chrono::duration<double> diff = end - start;
 
@@ -49,27 +75,16 @@ int main(int argc, char** argv) {
     }
 
 
-    double* device_data;
-    cudaMalloc((void**)&device_data, N * sizeof(double));
-
-    if (world_rank == 0) {
-        // Initialize data on device in rank 0 process
-        // This could be done with a CUDA kernel
-        double* host_data = new double[N];
-        for (int i = 0; i < N; i++) {
-            host_data[i] = 1.0f;
-        }
-        cudaMemcpy(device_data, host_data, N * sizeof(double), cudaMemcpyHostToDevice);
-        delete[] host_data;
-    }
-    
     MPI_Barrier(MPI_COMM_WORLD); // Make sure all processes are ready before starting the clock
+    
     start = std::chrono::high_resolution_clock::now();
     if (world_rank == 0) {
         MPI_Send(device_data, N, MPI_DOUBLE, 1, 0, MPI_COMM_WORLD);
     } else if (world_rank == 1) {
         MPI_Recv(device_data, N, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
     }
+        
+    MPI_Barrier(MPI_COMM_WORLD);
     end = std::chrono::high_resolution_clock::now();
     diff = end - start;
 
--- env_src/gpu/reproducers/gpu_direct.sh
+++ env_src/gpu/reproducers/gpu_direct.sh
@@ -1,2 +1,2 @@
-$TRUST_CC -mp -target=gpu -O3 -I$CUDA_ROOT/include -L$CUDA_ROOT/lib64 -lcudart -o gpu_direct gpu_direct.cpp && touch dumb.data && exec=`pwd`/gpu_direct trust dumb 2
+$TRUST_CC -mp -target=gpu -O3 -I$CUDA_ROOT/include -L$CUDA_ROOT/lib64 -lcudart -o gpu_direct gpu_direct.cpp && touch dumb.data && exec=`pwd`/gpu_direct trust $* dumb 2
 
