f907bc3081a872b0226527b889fc1476e04317a8
Yannick Gorsse <yannick.gorsse@stellaria-energy.com>
2024-10-02 09:13:34 +0200

    avec diffusion

--- src/PolyMAC/Operateurs/Op_Diff_Dift/Op_Diff_PolyVEF_P0P1_Elem.h
+++ src/PolyMAC/Operateurs/Op_Diff_Dift/Op_Diff_PolyVEF_P0P1_Elem.h
@@ -13,20 +13,37 @@
 *
 *****************************************************************************/
 
-#include <Temperature_imposee_paroi.h>
+#ifndef Op_Diff_PolyVEF_P0P1_Elem_included
+#define Op_Diff_PolyVEF_P0P1_Elem_included
 
-Implemente_instanciable(Temperature_imposee_paroi, "Temperature_imposee_paroi|Enthalpie_imposee_paroi", Scalaire_impose_paroi);
-// XD temperature_imposee_paroi paroi_temperature_imposee temperature_imposee_paroi 0 Imposed temperature condition at the wall called bord (edge).
+#include <Op_Diff_PolyMAC_P0P1NC_base.h>
+#include <Matrice_Morse.h>
 
-Sortie& Temperature_imposee_paroi::printOn(Sortie& s) const { return s << que_suis_je() << finl; }
+/*! @brief : class Op_Diff_PolyVEF_P0P1_Elem
+ *
+ *  <Description of class Op_Diff_PolyVEF_P0P1_Elem>
+ *
+ *
+ *
+ */
 
-Entree& Temperature_imposee_paroi::readOn(Entree& s)
+class Op_Diff_PolyVEF_P0P1_Elem : public Op_Diff_PolyMAC_P0P1NC_base
 {
-  if (app_domains.size() == 0) app_domains = { Motcle("Thermique"), Motcle("indetermine") };
-  if (supp_discs.size() == 0) supp_discs = { Nom("VEF"), Nom("EF"), Nom("EF_axi"), Nom("VEF_P1_P1"), Nom("VEFPreP1B"),
-                                               Nom("PolyMAC"), Nom("PolyMAC_P0P1NC"), Nom("PolyMAC_P0"),
-                                               Nom("DG"), Nom("VDF"), Nom("PolyVEF_P0"), Nom("PolyVEF_P0P1")
-                                             };
 
-  return Dirichlet::readOn(s);
-}
+  Declare_instanciable_sans_constructeur( Op_Diff_PolyVEF_P0P1_Elem ) ;
+
+public :
+  Op_Diff_PolyVEF_P0P1_Elem();
+  void completer() override;
+  void init_op_ext() const override;
+  // virtual void calculer_flux_bord(const DoubleTab& inco) const { abort(); };
+
+  /* interface {dimensionner,ajouter}_blocs */
+  double calculer_dt_stab() const override;
+  void dimensionner_blocs_ext(int aux_only, matrices_t matrices, const tabs_t& semi_impl = {}) const override;
+  void ajouter_blocs_ext(int aux_only, matrices_t matrices, DoubleTab& secmem, const tabs_t& semi_impl = {}) const override;
+
+  void modifier_pour_Cl(Matrice_Morse& la_matrice, DoubleTab& secmem) const override { };
+};
+
+#endif /* Op_Diff_PolyVEF_P0P1_Elem_included */
