0a4ac6f91f81f3f1e494e6489213bdd900e2bbef
Pierre LEDAC <pierre.ledac@cea.fr>
2024-11-12 12:43:13 +0100

    [Kokkos] Champ_Fonc_tabule_P0_VEF for 1 param and 1 component (ex: temperature dependent fields)

--- src/VEF/Champs/Champ_Fonc_Tabule_P0_VEF.cpp
+++ src/VEF/Champs/Champ_Fonc_Tabule_P0_VEF.cpp
@@ -14,9 +14,10 @@
 *****************************************************************************/
 
 #include <Champ_Fonc_Tabule_P0_VEF.h>
-
+#include <TRUSTTrav.h>
 #include <Domaine_VF.h>
 #include <Table.h>
+#include <ParserView.h>
 
 Implemente_instanciable(Champ_Fonc_Tabule_P0_VEF, "Champ_Fonc_Tabule_P0_VEF", Champ_Fonc_P0_VEF);
 
@@ -36,37 +37,68 @@ void Champ_Fonc_Tabule_P0_VEF::mettre_a_jour(double t)
   const Table& table = la_table.valeur();
   DoubleTab& mes_valeurs = valeurs();
   const int nb_elem = domaine_VF.nb_elem(), nb_elem_tot = domaine_VF.nb_elem_tot(), nb_param = les_ch_param.size();
-  DoubleTabs val_params_aux_elems;
-  for (int i = 0; i < nb_param; i++)
-    {
-      DoubleTab vp(nb_elem_tot, mes_valeurs.dimension(1));
-      val_params_aux_elems.add(vp);
-    }
+  const int nbcomp = mes_valeurs.dimension(1);
   const DoubleTab& centres_de_gravites = domaine_VF.xp();
-  IntVect les_polys(nb_elem_tot);
-  ToDo_Kokkos("");
-  for (int elem = 0; elem < nb_elem_tot; elem++)
-    les_polys(elem) = elem;
 
-  // Estimate the field parameter on cells:
-  for (int i = 0; i < nb_param; i++)
-    les_ch_param[i]->valeur_aux_elems(centres_de_gravites, les_polys, val_params_aux_elems[i]);
-  // Compute the field according to the parameter field
-  if (table.isfonction() != 2)
+  // ToDo Kokkos: factorize somewhere this array or rewrite valeur_aux_elems !
+  IntTrav les_polys(nb_elem_tot);
+  IntArrView les_polys_v = static_cast<IntVect&>(les_polys).view_wo();
+  Kokkos::parallel_for(start_gpu_timer(__KERNEL_NAME__), nb_elem_tot, KOKKOS_LAMBDA(const int num_elem)
+  {
+    les_polys_v(num_elem) = num_elem;
+  });
+  end_gpu_timer(Objet_U::computeOnDevice, __KERNEL_NAME__);
+
+  if (nb_param==1 && nbcomp==1 && table.isfonction()==1)
     {
-      const int nbcomp = mes_valeurs.dimension(1);
-      for (int num_elem = 0; num_elem < nb_elem; num_elem++)
+      // Ported on GPU. ToDo Kokkos, extend to more than one param or more than one nbcomp
+      DoubleTrav val_param_aux_elems(nb_elem_tot, nbcomp);
+      les_ch_param[0]->valeur_aux_elems(centres_de_gravites, les_polys, val_param_aux_elems);
+
+      ParserView parser(table.parser(0).getString(), 1, nb_elem);
+      parser.addVar("val");
+      parser.parseString();
+      CDoubleTabView val_params_aux_elems_v = val_param_aux_elems.view_ro();
+      DoubleTabView mes_valeurs_v = mes_valeurs.view_wo();
+      Kokkos::parallel_for(start_gpu_timer(__KERNEL_NAME__), nb_elem, KOKKOS_LAMBDA(
+                             const int num_elem)
+      {
         for (int ncomp = 0; ncomp < nbcomp; ncomp++)
           {
-            std::vector<double> vals;
-            for (int n = 0; n < nb_param; n++)
-              vals.push_back(val_params_aux_elems[n](num_elem, ncomp));
-            mes_valeurs(num_elem, ncomp) = table.val(vals, ncomp);
+            double val = val_params_aux_elems_v(num_elem, ncomp);
+            parser.setVar(0, val, num_elem);
+            mes_valeurs_v(num_elem, ncomp) = parser.eval(num_elem);
           }
+      });
+      end_gpu_timer(Objet_U::computeOnDevice, __KERNEL_NAME__);
     }
   else
     {
-      table.valeurs(val_params_aux_elems[0], centres_de_gravites, t, mes_valeurs);
+      ToDo_Kokkos("critical");
+      DoubleTabs val_params_aux_elems;
+      for (int i = 0; i < nb_param; i++)
+        {
+          DoubleTab vp(nb_elem_tot, mes_valeurs.dimension(1));
+          val_params_aux_elems.add(vp);
+        }
+      for (int i = 0; i < nb_param; i++)
+        les_ch_param[i]->valeur_aux_elems(centres_de_gravites, les_polys, val_params_aux_elems[i]);
+
+      if (table.isfonction() != 2)
+        {
+          for (int num_elem = 0; num_elem < nb_elem; num_elem++)
+            for (int ncomp = 0; ncomp < nbcomp; ncomp++)
+              {
+                std::vector<double> vals;
+                for (int n = 0; n < nb_param; n++)
+                  vals.push_back(val_params_aux_elems[n](num_elem, ncomp));
+                mes_valeurs(num_elem, ncomp) = table.val(vals, ncomp);
+              }
+        }
+      else
+        {
+          table.valeurs(val_params_aux_elems[0], centres_de_gravites, t, mes_valeurs);
+        }
     }
   Champ_Fonc_base::mettre_a_jour(t);
 }
