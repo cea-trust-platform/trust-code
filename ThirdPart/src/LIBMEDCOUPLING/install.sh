4e5081e80cc16a79dbb622a98373e3ed4e748fd2
Yannick Gorsse <yannick.gorsse@stellaria-energy.com>
2024-11-13 16:20:41 +0100

    [mac] Add medcoupling compilation flags to fix ParaMEDMED use in python

--- ThirdPart/src/LIBMEDCOUPLING/install.sh
+++ ThirdPart/src/LIBMEDCOUPLING/install.sh
@@ -55,8 +55,8 @@ build_and_test_mc()
     export CXXFLAGS="-Wno-narrowing -Wno-sign-conversion"
     if [[ $use_python == ON && $(uname -s) == "Darwin" ]]
     then
-       export CXXFLAGS="$CXXFLAGS -I${TRUST_ROOT}/exec/python/include/python3.8"
-       export LDFLAGS="$LDFLAGS -Wl,-undefined,dynamic_lookup"
+       export CXXFLAGS="$CXXFLAGS -I${TRUST_ROOT}/exec/python/include/python3.12"
+       export LDFLAGS="$LDFLAGS -Wl,-undefined,error -Wl,-flat_namespace -L${TRUST_ROOT}/exec/python/lib -lpython3.12"
     fi
     echo "Applying patch for const pointer in IKDEC ..."
     (cd $src_dir; patch -p1 -f < $TRUST_ROOT/ThirdPart/src/LIBMEDCOUPLING/const_ptr.patch )
