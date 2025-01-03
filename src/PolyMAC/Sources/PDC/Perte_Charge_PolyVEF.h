6eb36091b234c0064cd70a5d5a532dc0639efe9a
Antoine Gerschenfeld <antoine.gerschenfeld@cea.fr>
2025-01-03 14:23:07 +0100

    [PolyVEF] Perte_Charge_PolyVEF: fix anisotropy

--- src/PolyMAC/Sources/PDC/Perte_Charge_PolyVEF.h
+++ src/PolyMAC/Sources/PDC/Perte_Charge_PolyVEF.h
@@ -36,7 +36,7 @@ class Perte_Charge_PolyVEF : public Perte_Charge_PolyMAC_P0P1NC
 public:
   int has_interface_blocs() const override { return 1; }
   void check_multiphase_compatibility() const override { }
-  void dimensionner_blocs(matrices_t matrices, const tabs_t& semi_impl = {}) const override { } //rien
+  void dimensionner_blocs(matrices_t matrices, const tabs_t& semi_impl = {}) const override;
   void ajouter_blocs(matrices_t matrices, DoubleTab& secmem, const tabs_t& semi_impl = {}) const override;
 };
 
