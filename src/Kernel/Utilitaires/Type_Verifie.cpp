8b1c1f7a209c6cf62892a5c16d5c6325c90db9df
triou <trust@cea.fr>
2025-01-29 14:30:22 +0100

    [PolyMAC] Option_PolyMAC_P0 keyword in datafiles should be replaced by Option_PolyMAC

--- src/Kernel/Utilitaires/Type_Verifie.cpp
+++ src/Kernel/Utilitaires/Type_Verifie.cpp
@@ -1,5 +1,5 @@
 /****************************************************************************
-* Copyright (c) 2024, CEA
+* Copyright (c) 2025, CEA
 * All rights reserved.
 *
 * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
@@ -21,7 +21,7 @@ static Motcles motcle_obsolete;
 // Parcours du jeu de donnees pour verification des mots cles obsoletes
 void verifie(const Nom& type)
 {
-  int n=73;
+  int n=74;
   if( motcle_obsolete.size()!=n)
     {
       int i=0;
@@ -100,6 +100,7 @@ void verifie(const Nom& type)
       motcle_obsolete[i++] = "seuil_statio_relatif_deconseille";
       motcle_obsolete[i++] = "CoviMAC";
       motcle_obsolete[i++] = "ecrire_fichier_xyz_valeur_bin";
+      motcle_obsolete[i++] = "option_polymac_p0";
       assert(i==n);
     }
   // GF on ne constuit les noms de la nouvelle syntaxe que si necessaire
@@ -184,6 +185,7 @@ void verifie(const Nom& type)
       nouvelle_syntaxe[i++] = "1.9.2, you should use Residuals { relative 1 } instead.";
       nouvelle_syntaxe[i++] = "1.9.3, you should use PolyMAC_P0 instead.";
       nouvelle_syntaxe[i++] = "1.9.4, you should use Ecrire_fichier_xyz_valeur { binary_file dt 1 fields 2 field1 field2 boundaries 1 boundary1 } instead.";
+      nouvelle_syntaxe[i++] = "1.9.6, you should use Option_PolyMAC instead.";
       assert(i==n);
       Cerr << "The keyword " << motcle_obsolete[rang] << " is now obsolete," << finl;
       Cerr << "since the version " << nouvelle_syntaxe[rang] << finl << finl;
