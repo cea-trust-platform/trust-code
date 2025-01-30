6d381507c889875320583772c57d7b5a20999162
Yannick Gorsse <yannick.gorsse@stellaria-energy.com>
2025-01-30 10:44:25 +0100

    [PolyVEF_P0] Add sources terms

---
+++ src/PolyMAC/Sources/PDC/Perte_Charge_PolyVEF_P0.h
@@ -0,0 +1,41 @@
+/****************************************************************************
+* Copyright (c) 2024, CEA
+* All rights reserved.
+*
+* Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+* 1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+* 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+* 3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+*
+* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+* IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+* OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*
+*****************************************************************************/
+
+#ifndef Perte_Charge_PolyVEF_P0_included
+#define Perte_Charge_PolyVEF_P0_included
+
+#include <Perte_Charge_PolyMAC.h>
+
+//! Factorise les fonctionnalites de plusieurs pertes de charge en VEF, vitesse aux faces
+/**
+   Perte_Charge_Isotrope, Perte_Charge_Directionnelle et
+   Perte_Charge_Anisotrope heritent de Perte_Charge_PolyVEF_P0. Elles
+   doivent surcharger essentiellement readOn() et perte_charge().
+   readOn() est suppose lire au moins diam_hydr et sous_domaine.
+
+   Ces classes sont censees remplacer Perte_Charge_PolyVEF_P0_Face
+   et Perte_Charge_PolyVEF_P0_P1NC.
+*/
+
+class Perte_Charge_PolyVEF_P0 : public Perte_Charge_PolyMAC
+{
+  Declare_base(Perte_Charge_PolyVEF_P0);
+public:
+  DoubleTab& ajouter(DoubleTab& ) const override; //!< Appelle perte_charge pour chaque face ou cela est necessaire
+  void contribuer_a_avec(const DoubleTab&, Matrice_Morse&) const override ;
+  void ajouter_blocs(matrices_t matrices, DoubleTab& secmem, const tabs_t& semi_impl = {}) const override;
+};
+
+#endif /* Perte_Charge_PolyVEF_P0_included */
