65a081b08ee7afd75eb94edfb67b0d7b691cc39b
Yannick Gorsse <yannick.gorsse@stellaria-energy.com>
2025-01-30 10:47:17 +0100

    [PolyVEF_P0] Change interitance geometrie

--- src/PolyMAC/Geometrie/Domaine_PolyVEF_P0.cpp
+++ src/PolyMAC/Geometrie/Domaine_PolyVEF_P0.cpp
@@ -21,10 +21,10 @@
 #include <Connectivite_som_elem.h>
 #include <MD_Vector_composite.h>
 #include <Dirichlet_homogene.h>
-#include <Option_PolyVEF_P0.h>
-#include <MD_Vector_tools.h>
 #include <Domaine_PolyVEF_P0.h>
 #include <Domaine_Cl_PolyMAC.h>
+#include <Option_PolyVEF_P0.h>
+#include <MD_Vector_tools.h>
 #include <TRUSTTab_parts.h>
 #include <Comm_Group_MPI.h>
 #include <Quadrangle_VEF.h>
@@ -76,37 +76,6 @@ void Domaine_PolyVEF_P0::discretiser()
   mdc_elems_fbord.add_part(domaine().md_vector_elements());
   mdc_elems_fbord.add_part(md_vector_faces_bord());
   mdv_elems_fbord.copy(mdc_elems_fbord);
-}
-
-
-//stencil face/face : fsten_f([fsten_d(f, 0), fsten_d(f + 1, 0)[)
-void Domaine_PolyVEF_P0::init_stencils() const
-{
-  if (fsten_d.size()) return;
-  const IntTab& f_s = face_sommets(), &f_e = face_voisins(), &e_s = domaine().les_elems();
-  int i, e, f, s, ne_tot = nb_elem_tot(), ns_tot = domaine().nb_som_tot();
-
-  /* connectivite sommets -> elems / faces de bord */
-  std::vector<std::set<int>> som_eb(ns_tot);
-  for (e = 0; e < nb_elem_tot(); e++)
-    for (i = 0; i < e_s.dimension(1) && (s = e_s(e, i)) >= 0; i++) som_eb[s].insert(e);
-  for (f = 0; f < nb_faces_tot(); f++)
-    if (fbord(f) >= 0)
-      for (i = 0; i < f_s.dimension(1) && (s = f_s(f, i)) >= 0; i++) som_eb[s].insert(ne_tot + f);
-
-  std::set<int> f_eb; //sommets de la face f, elems connectes a e par soms, sommets / faces connectes par une face commune
-  for (f = 0; f < nb_faces_tot(); fsten_d.append_line(fsten_eb.size()), f++)
-    if (f_e(f, 0) >= 0 && (fbord(f) >= 0 || f_e(f, 1) >= 0))
-      {
-        /* connectivite par un sommet de f */
-        for (f_eb.clear(), i = 0; i < f_s.dimension(1) && (s = f_s(f, i)) >= 0; i++)
-          for (auto &&el : som_eb[s]) f_eb.insert(el);
-
-        /* remplissage */
-        for (auto && el : f_eb) fsten_eb.append_line(el);
-      }
-
-  CRIMP(fsten_d), CRIMP(fsten_eb);
 }
 
 //pour u.n champ T aux elements, interpole [n_f.grad T]_f (si nu = nullptr) ou [n_f.nu.grad T]_f ou (grad p)_f (si is_p = 1)
