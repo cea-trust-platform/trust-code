c4122533a7d02d74170ebdf3509e4cda6e41284b
Adrien BRUNETON <adrien.bruneton@cea.fr>
2025-02-03 14:04:32 +0100

    [gtest] Fix for sequential test

--- tests/UnitTests/main_gtest.cpp
+++ tests/UnitTests/main_gtest.cpp
@@ -1,8 +1,14 @@
 #include <gtest/gtest.h>
 #include <TRUSTArray.h>
+#include <Comm_Group_Noparallel.h>
+#include <PE_Groups.h>
+
 
 int main(int argc, char **argv) {
     Kokkos::initialize();
+
+    Comm_Group_Noparallel groupe_trio;
+    PE_Groups::initialize(groupe_trio);
 
     ::testing::InitGoogleTest(&argc, argv);
     int result = RUN_ALL_TESTS();
