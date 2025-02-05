683bc3165782e8b4d1024e8f5b504fff749d0346
triou <trust@cea.fr>
2025-02-05 14:37:42 +0100

    [gtest] run unit_gtest* tests when doing make ctest_debug

--- bin/KSH/Createcmakefile.py
+++ bin/KSH/Createcmakefile.py
@@ -401,15 +401,19 @@ if(NOT ATELIER) # Not a Baltik, TRUST itself
     #
     # Unit tests - only compiled with micro kernel, and in Debug, but test added for all Kernel configuration
     #
-    if(${kernel} STREQUAL "micro" AND "${OPT}" STREQUAL "" AND NOT "$ENV{TRUST_DISABLE_GTEST}" STREQUAL "1" )
-        add_subdirectory(${CMAKE_SOURCE_DIR}/../tests/UnitTests ${CMAKE_BINARY_DIR}/UnitTests) # Must specify binary dir cause out of tree source
+    if("${OPT}" STREQUAL "" AND "$ENV{TRUST_DISABLE_GTEST}" STREQUAL "0" )
+        if(${kernel} STREQUAL "micro")
+            add_subdirectory(${CMAKE_SOURCE_DIR}/../tests/UnitTests ${CMAKE_BINARY_DIR}/UnitTests) # Must specify binary dir cause out of tree source
+        endif()
         add_test(NAME Unit_GTest COMMAND "$ENV{TRUST_ROOT}/exec/unit_gtest")
-        
-        # Parallel tests
-        ##
-        ## 2 procs
-        ##
-        add_test(NAME Unit_GTest_MPI_2procs COMMAND "$ENV{TRUST_ROOT}/bin/mpirun.sh" "-np 2" "$ENV{TRUST_ROOT}/exec/unit_gtest_mpi -unit")
+
+        if ("$ENV{TRUST_DISABLE_MPI}" STREQUAL "0")
+            # Parallel tests
+            ##
+            ## 2 procs
+            ##
+            add_test(NAME Unit_GTest_MPI_2procs COMMAND "$ENV{TRUST_ROOT}/bin/mpirun.sh" "-np 2" "$ENV{TRUST_ROOT}/exec/unit_gtest_mpi -unit")
+        endif()
     endif()
 
     #
