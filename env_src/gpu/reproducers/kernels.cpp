374a3128559b0626265011ef706ab75ea6e85307
Pierre LEDAC <pierre.ledac@cea.fr>
2025-02-18 08:33:59 +0100

    [Kokkos] Give example of how to use constness and Kokkos::RandomAccess carefully

--- env_src/gpu/reproducers/kernels.cpp
+++ env_src/gpu/reproducers/kernels.cpp
@@ -36,18 +36,7 @@ int main() {
 
     Kokkos::initialize();
     {  
-    
-    Kokkos::View<double*> a_view("a_v", nb_elem);
-    Kokkos::View<int*> ef_view("ef_v", 4*nb_elem);
-    Kokkos::View<double*> f_view("f_v", nb_faces);
-    Kokkos::View<double*> resu_view("resu_v", nb_faces);
-
-    // Copy data to Kokkos views
-    Kokkos::deep_copy(a_view, Kokkos::View<double*, Kokkos::HostSpace>(a, nb_elem));
-    Kokkos::deep_copy(ef_view, Kokkos::View<int*, Kokkos::HostSpace>(ef, 4*nb_elem));
-    Kokkos::deep_copy(f_view, Kokkos::View<double*, Kokkos::HostSpace>(f, nb_faces));
-    Kokkos::deep_copy(resu_view, Kokkos::View<double*, Kokkos::HostSpace>(resu, nb_faces));
-    
+        
     int FLOPS=0;
     double SEC=0;
     for (int sample=0;sample<samples;sample++)
@@ -71,6 +60,48 @@ int main() {
     SEC/=samples;
     std::cout << "[Kernel OpDiv] CPU    Mean time: " << 1000*SEC << " ms " << 0.1*int(0.01*0.001*0.001*FLOPS/SEC) << " GFLOPS\n";
 
+    // Kokkos views
+    Kokkos::View<double*> a_device("a_device", nb_elem);
+    Kokkos::View<int*> ef_device("ef_device", 4*nb_elem);
+    Kokkos::View<double*> f_device("f_device", nb_faces);
+    Kokkos::View<double*> resu_device("resu_device", nb_faces);
+
+    // Copy data to Kokkos views
+    Kokkos::deep_copy(a_device, Kokkos::View<double*, Kokkos::HostSpace>(a, nb_elem));
+    Kokkos::deep_copy(ef_device, Kokkos::View<int*, Kokkos::HostSpace>(ef, 4*nb_elem));
+    Kokkos::deep_copy(f_device, Kokkos::View<double*, Kokkos::HostSpace>(f, nb_faces));
+    Kokkos::deep_copy(resu_device, Kokkos::View<double*, Kokkos::HostSpace>(resu, nb_faces));
+
+    FLOPS=0;
+    SEC=0;
+    for (int sample=0;sample<samples;sample++)
+    {
+          auto start = std::chrono::high_resolution_clock::now();
+          Kokkos::parallel_for(nb_elem, KOKKOS_LAMBDA(const int elem) {
+              double pre = a_device(elem);
+              for (int j=0; j<4; j++)
+              {
+                  int face = ef_device(4*elem+j);
+                  Kokkos::atomic_add(&resu_device(face), pre * f_device(face));
+              }
+          });
+          Kokkos::fence();
+          auto end = std::chrono::high_resolution_clock::now();
+          std::chrono::duration<double> diff = end - start;
+          FLOPS += 8 * nb_elem;
+          SEC += diff.count();
+    }
+    FLOPS/=samples;
+    SEC/=samples;
+    std::cout << "[Kernel OpDiv] Kokkos Mean time: " << 1000*SEC << " ms " << 0.1*int(0.01*0.001*0.001*FLOPS/SEC) << " GFLOPS\n";
+
+    // Tuned views:
+    using RandomAccess = Kokkos::MemoryTraits<Kokkos::RandomAccess>;
+    Kokkos::View<const double*> a_view  = a_device;
+    Kokkos::View<const int*, RandomAccess> ef_view    = ef_device;
+    Kokkos::View<const double*, RandomAccess> f_view  = f_device;
+    Kokkos::View<double*> resu_view     = resu_device;
+
     FLOPS=0;
     SEC=0;
     for (int sample=0;sample<samples;sample++)
@@ -92,7 +123,7 @@ int main() {
     }
     FLOPS/=samples;
     SEC/=samples;
-    std::cout << "[Kernel OpDiv] Kokkos Mean time: " << 1000*SEC << " ms " << 0.1*int(0.01*0.001*0.001*FLOPS/SEC) << " GFLOPS\n";
+    std::cout << "[Kernel OpDiv] Kokkos Mean time: " << 1000*SEC << " ms " << 0.1*int(0.01*0.001*0.001*FLOPS/SEC) << " GFLOPS (Tuned kernel)\n";
     }
     Kokkos::finalize();
     return 0;
