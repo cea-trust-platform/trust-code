7bf8f195398aebca3e7cd355f0bceddb20165a93
triou <trust@cea.fr>
2025-02-19 15:31:32 +0100

    Update extp.commit

--- env_src/test.py
+++ env_src/test.py
@@ -8,10 +8,15 @@ import matplotlib
 print((matplotlib.__file__))
 import numpy
 print((numpy.__file__))
+import numpy.core._multiarray_umath
+print (numpy.core._multiarray_umath.__file__)
 import scipy
 print((scipy.__file__))
 import sympy
 print((sympy.__file__))
 import pandas
 print((pandas.__file__))
+import IPython
+print((IPython.__file__))
 print("Python is OK")
+
