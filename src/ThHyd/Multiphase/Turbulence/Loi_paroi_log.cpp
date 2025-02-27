bd6838bd0d985f14bfb711b2700520882316fc7b
Corentin Reiss <corentin.reiss@cea.fr>
2025-02-27 15:42:00 +0100

    [Turbulence] bugfix log-law

--- src/ThHyd/Multiphase/Turbulence/Loi_paroi_log.cpp
+++ src/ThHyd/Multiphase/Turbulence/Loi_paroi_log.cpp
@@ -1,5 +1,5 @@
 /****************************************************************************
-* Copyright (c) 2024, CEA
+* Copyright (c) 2025, CEA
 * All rights reserved.
 *
 * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
@@ -13,20 +13,21 @@
 *
 *****************************************************************************/
 
-#include <Loi_paroi_log.h>
+#include <Discretisation_base.h>
 #include <Navier_Stokes_std.h>
 #include <Correlation_base.h>
+#include <Champ_Face_base.h>
 #include <QDM_Multiphase.h>
 #include <TRUSTTab_parts.h>
 #include <Cond_lim_base.h>
 #include <Pb_Multiphase.h>
+#include <Loi_paroi_log.h>
 #include <Domaine_VF.h>
 #include <TRUSTTrav.h>
 #include <Motcle.h>
 #include <Param.h>
 #include <math.h>
 #include <Nom.h>
-#include <Champ_Face_base.h>
 
 Implemente_instanciable(Loi_paroi_log, "Loi_paroi_log", Loi_paroi_base);
 
@@ -42,85 +43,95 @@ Entree& Loi_paroi_log::readOn(Entree& is)
 
 void Loi_paroi_log::calc_y_plus(const DoubleTab& vit, const DoubleTab& nu_visc)
 {
+  const int cnu = nu_visc.dimension(0) == 1;
   Domaine_VF& domaine = ref_cast(Domaine_VF, pb_->domaine_dis());
-  DoubleTab& u_t = valeurs_loi_paroi_["u_tau"], &y_p = valeurs_loi_paroi_["y_plus"];
+  DoubleTab& u_t = valeurs_loi_paroi_["u_tau"], &y_p = valeurs_loi_paroi_["y_plus"], &y_loc = valeurs_loi_paroi_["y"];
   const DoubleTab& n_f = domaine.face_normales();
   const DoubleVect& fs = domaine.face_surfaces();
   const IntTab& f_e = domaine.face_voisins();
 
+  const bool is_VDF = pb_->discretisation().is_vdf();
+
   int nf_tot = domaine.nb_faces_tot(), D = dimension, N = vit.line_size();
 
-  DoubleTab pvit_elem(0, N * dimension);
-  if (nf_tot == vit.dimension_tot(0))
+  DoubleTab pvit_elem(0, N * D);
+  if (is_VDF)
     {
       const Champ_Face_base& ch = ref_cast(Champ_Face_base, pb_->equation(0).inconnue());
       domaine.domaine().creer_tableau_elements(pvit_elem);
       ch.get_elem_vector_field(pvit_elem, true);
     }
 
-  int n=0; // pour l'instant, turbulence dans seulement une phase
+  int n = 0; // pour l'instant, turbulence dans seulement une phase
 
-  for (int f = 0 ; f < nf_tot ; f ++)
-    if (Faces_a_calculer_(f,0)==1)
+  for (int f = 0; f < nf_tot; f++)
+    if (Faces_a_calculer_(f, 0) == 1)
       {
-        int c = (f_e(f,0)>=0) ? 0 : 1 ;
-        if (f_e(f, (c==0) ? 1 : 0 ) >= 0) Process::exit("Error in the definition of the boundary conditions for wall laws");
-        int e = f_e(f,c);
+        int c = (f_e(f, 0) >= 0) ? 0 : 1;
+        if (f_e(f, (c == 0) ? 1 : 0) >= 0)
+          Process::exit("Error in the definition of the boundary conditions for wall laws");
+        int e = f_e(f, c);
 
-        double u_orth = 0 ;
+        double u_orth = 0.;
+        const double yloc = y_loc(f, n);
         DoubleTrav u_parallel(D);
-        if (nf_tot == vit.dimension_tot(0)) // VDF case
+        if (is_VDF) // VDF case : vitesse au centre de l'element
           {
-            for (int d = 0; d <D ; d++) u_orth -= pvit_elem(e, N*d+n)*n_f(f,d)/fs(f); // ! n_f pointe vers la face 1 donc vers l'exterieur de l'element, d'ou le -
-            for (int d = 0 ; d < D ; d++) u_parallel(d) = pvit_elem(e, N*d+n) - u_orth*(-n_f(f,d))/fs(f) ; // ! n_f pointe vers la face 1 donc vers l'exterieur de l'element, d'ou le -
+            for (int d = 0; d < D; d++)
+              u_orth -= pvit_elem(e, N * d + n) * n_f(f, d) / fs(f); // ! n_f pointe vers la face 1 donc vers l'exterieur de l'element, d'ou le -
+            for (int d = 0; d < D; d++)
+              u_parallel(d) = pvit_elem(e, N * d + n) - u_orth * (-n_f(f, d)) / fs(f); // ! n_f pointe vers la face 1 donc vers l'exterieur de l'element, d'ou le -
           }
         else // PolyMAC case
           {
-            for (int d = 0; d <D ; d++) u_orth -= vit(nf_tot + e * D+d, n)*n_f(f,d)/fs(f); // ! n_f pointe vers la face 1 donc vers l'exterieur de l'element, d'ou le -
-            for (int d = 0 ; d < D ; d++) u_parallel(d) = vit(nf_tot + e * D + d, n) - u_orth*(-n_f(f,d))/fs(f) ; // ! n_f pointe vers la face 1 donc vers l'exterieur de l'element, d'ou le -
+            for (int d = 0; d < D; d++)
+              u_orth -= vit(nf_tot + e * D + d, n) * n_f(f, d) / fs(f); // ! n_f pointe vers la face 1 donc vers l'exterieur de l'element, d'ou le -
+            for (int d = 0; d < D; d++)
+              u_parallel(d) = vit(nf_tot + e * D + d, n) - u_orth * (-n_f(f, d)) / fs(f); // ! n_f pointe vers la face 1 donc vers l'exterieur de l'element, d'ou le -
           }
 
-        double residu = 0 ;
-        for (int d = 0; d <D ; d++) residu += u_parallel(d)*n_f(f,d)/fs(f);
-        if (residu > 1e-8) Process::exit("Loi_paroi_adaptative : Error in the calculation of the parallel velocity for wall laws");
         double norm_u_parallel = std::sqrt(domaine.dot(&u_parallel(0), &u_parallel(0)));
 
-        double y_loc = (c==0) ? domaine.dist_face_elem0(f,e) : domaine.dist_face_elem1(f,e) ;
-        y_p(f, n) = std::max(y_p_min_, calc_y_plus_loc(norm_u_parallel, nu_visc(e, n), y_loc, y_p(f, n)));
-        u_t(f, n) = y_p(f, n)*nu_visc(e, n)/y_loc;
+        // double residu = 0 ;
+        // for (int d = 0; d <D ; d++) residu += u_parallel(d)*n_f(f,d)/fs(f);
+        // if (residu > 1e-8) Process::exit("Loi_paroi_adaptative : Error in the calculation of the parallel velocity for wall laws");
+
+        y_p(f, n) = std::max(y_p_min_, calc_y_plus_loc(norm_u_parallel, nu_visc(!cnu * e, n), yloc, y_p(f, n)));
+        u_t(f, n) = y_p(f, n) * nu_visc(!cnu * e, n) / yloc;
       }
 }
 
 double Loi_paroi_log::calc_y_plus_loc(double u_par, double nu, double y, double y_p_0)
 {
-  if (u_par*y/nu < limiteur_y_p) return limiteur_y_p;
+  if (u_par * y / nu < limiteur_y_p)
+    return limiteur_y_p;
 
   double eps = eps_y_p_;
   int step = 1, iter_max = 30;
 
-  double y_p = y_p_0 ;
-  double u_tau = nu*y_p/y;
+  double y_p = y_p_0;
+  double u_tau = nu * y_p / y;
 
   do
     {
-      y_p = std::max(limiteur_y_p, y_p - (u_plus_de_y_plus(y_p) - u_par/u_tau)/(deriv_u_plus_de_y_plus(y_p) + u_par/(u_tau*y_p) ));
-      step = step+1;
-      u_tau = nu*y_p/y;
+      y_p = std::max(limiteur_y_p, y_p - (u_plus_de_y_plus(y_p) - u_par / u_tau) / (deriv_u_plus_de_y_plus(y_p) + u_par / (u_tau * y_p)));
+      step = step + 1;
+      u_tau = nu * y_p / y;
     }
-  while( (std::fabs(u_plus_de_y_plus(y_p) - u_par/u_tau) > eps) and (step < iter_max));
+  while ((std::fabs(u_plus_de_y_plus(y_p) - u_par / u_tau) > eps) and (step < iter_max));
 
-  assert ( (std::fabs(u_par/u_tau - u_plus_de_y_plus(y_p)) < eps_y_p_*10) and (step < iter_max));
+  assert((std::fabs(u_par / u_tau - u_plus_de_y_plus(y_p)) < eps_y_p_ * 10) and (step < iter_max));
 
   return y_p;
 }
 
 double Loi_paroi_log::u_plus_de_y_plus(double y_p)  // Blended Reichardt model
 {
-  return std::log(y_p+limiteur_y_p)/von_karman_ + 5.1;
+  return std::log(y_p + limiteur_y_p) / von_karman_ + 5.1;
 }
 
 double Loi_paroi_log::deriv_u_plus_de_y_plus(double y_p)
 {
-  return 1./((y_p+limiteur_y_p)*von_karman_);
+  return 1. / ((y_p + limiteur_y_p) * von_karman_);
 }
 
