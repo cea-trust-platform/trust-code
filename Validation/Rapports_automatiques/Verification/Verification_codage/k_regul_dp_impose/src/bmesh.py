a60f4a7c8e236adfaba1708ec31b55f196f2b5a6
Yannick Gorsse <yannick.gorsse@stellaria-energy.com>
2025-06-13 09:59:55 +0200

    [Verification] Add regulated regular pressure drop to k_regul_dp_impose notebook, add a description of the notebook, and improve graphs

--- Validation/Rapports_automatiques/Verification/Verification_codage/k_regul_dp_impose/src/bmesh.py
+++ Validation/Rapports_automatiques/Verification/Verification_codage/k_regul_dp_impose/src/bmesh.py
@@ -15,23 +15,29 @@ def build_mesh(tets, original_med, target_med):
     mf, _, _, F_E, F_Ei = mesh.buildDescendingConnectivity()
     bf = mf.computeIsoBarycenterOfNodesPerCell()
 
-    surf1 = []
-    surf2 = []
-    wall = []
-    iwall = []
+    surf1, surf2, wall, iwall = [], [], [], []
     for i, b in enumerate(bf):
-        if F_Ei[i + 1] == F_Ei[i] + 1: wall.append(i)
-        else:
-            if abs(b[2] - 0.5) < 1e-6:
-                if b[0] < 0.2: surf1.append(i)
-                else: surf2.append(i)
-            elif abs(b[0] - 0.2) < 1e-6 and b[2] > 0.25 and b[2] < 0.75:
-                iwall.append(i)
+        if F_Ei[i + 1] == F_Ei[i] + 1:
+            wall.append(i)
+        elif abs(b[2] - 0.5) < 1e-6:
+            (surf1 if b[0] < 0.2 else surf2).append(i)
+        elif abs(b[0] - 0.2) < 1e-6 and 0.25 < b[2] < 0.75:
+            iwall.append(i)
+
+    bc = mesh.computeIsoBarycenterOfNodesPerCell()
+    vol_pump = []
+    for i, b in enumerate(bc):
+        if (b[0] < 0.2) and 0.25 < b[2] < 0.75:
+            vol_pump.append(i)
+    vol_pump = mc.DataArrayInt.New(vol_pump)
+    vol_pump.setName("vol_pump")
+
 
     mm = mc.MEDFileUMesh.New()
     mm.setMeshAtLevel(0, mesh)
+    mm.addGroup(0, vol_pump)
     mm.setMeshAtLevel(-1, mf)
-    for name, g in [("surf1", surf1), ("surf2", surf2), ("wall", wall), ("iwall", iwall)]:
+    for name, g in zip(["surf1", "surf2", "wall", "iwall"], [surf1, surf2, wall, iwall]):
         grp = mc.DataArrayInt.New(g)
         grp.setName(name)
         mm.addGroup(-1, grp)
